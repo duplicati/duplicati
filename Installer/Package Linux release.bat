ce9c6fcb403ba065b7beb78fe72cbb09f7ee341e
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2009-04-18 15:37:24 +0000

    Added a wrapper for starting Duplicati with the newer SQLite binaries. Fixed a typo in the linux build script.
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@159 59da171f-624f-0410-aa54-27559c288bec

--- Installer/Package Linux release.bat
+++ Installer/Package Linux release.bat
@@ -1,7 +1,7 @@
 @echo off
 cd bin\Release
-mkdir Duplicity
-cd Duplicity
+mkdir Duplicati
+cd Duplicati
 cd
 del * /Q
 xcopy /I /Y ..\..\..\..\Duplicati\GUI\Bin\Release\* .
@@ -11,4 +11,5 @@ move sqlite-3.6.12.so libsqlite3.so.0
 xcopy /I /Y "..\..\..\..\thirdparty\SQLite\Bin\linux howto.txt" .
 move "linux howto.txt" linux-sqlite-readme.txt
 xcopy /I /Y "..\..\..\..\thirdparty\SQLite\Dll for .Net\ManagedOnly\System.Data.SQLite.dll" .
+xcopy /I /Y ..\..\..\..\Duplicati\GUI\StartDuplicati.sh .
 pause
