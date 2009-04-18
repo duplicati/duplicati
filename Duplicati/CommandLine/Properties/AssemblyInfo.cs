d2aa3725b00d69f65fc6591ae174bb98131c545d
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2009-04-18 10:12:55 +0000

    Updated version numbers on assemblies.
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@153 59da171f-624f-0410-aa54-27559c288bec

--- Duplicati/CommandLine/Properties/AssemblyInfo.cs
+++ Duplicati/CommandLine/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.146")]
+[assembly: AssemblyFileVersion("1.0.0.146")]
--- Duplicati/GUI/Properties/AssemblyInfo.cs
+++ Duplicati/GUI/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("1.0.0.146")]
+[assembly: AssemblyFileVersion("1.0.0.146")]
