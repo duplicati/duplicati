9301fc3d5436309d5b0a4b6f7fab392a21cfbc01
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2009-04-18 10:17:46 +0000

    Re-updated file version numbers for the RC1 release.
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@154 59da171f-624f-0410-aa54-27559c288bec

--- Duplicati/Library/Logging/Properties/AssemblyInfo.cs
+++ Duplicati/Library/Logging/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.146")]
-[assembly: AssemblyFileVersion("1.0.0.146")]
+[assembly: AssemblyVersion("1.0.0.154")]
+[assembly: AssemblyFileVersion("1.0.0.154")]
