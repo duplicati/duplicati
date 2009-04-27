5d95f0597169c012a59e0a570d303e25a8e4161e
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2009-04-27 17:42:35 +0000

    Fix for a Linux packaging issue.
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@170 59da171f-624f-0410-aa54-27559c288bec

--- Installer/Package Linux release.bat
+++ Installer/Package Linux release.bat
@@ -10,6 +10,7 @@ xcopy /I /Y ..\..\..\..\thirdparty\SQLite\Bin\sqlite-3.6.12.so .
 move sqlite-3.6.12.so libsqlite3.so.0
 xcopy /I /Y "..\..\..\..\thirdparty\SQLite\Bin\linux howto.txt" .
 move "linux howto.txt" linux-sqlite-readme.txt
+del "System.Data.SQLite.dll" /Q
 xcopy /I /Y "..\..\..\..\thirdparty\SQLite\Dll for .Net\ManagedOnly\System.Data.SQLite.dll" .
 xcopy /I /Y ..\..\..\..\Duplicati\GUI\StartDuplicati.sh .
 pause
