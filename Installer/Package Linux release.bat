8904d8f3ea2231375b94ecb9a3a6b59941d34ee1
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2009-05-16 11:24:24 +0000

    Minor updates to the build script for Linux.
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@177 59da171f-624f-0410-aa54-27559c288bec

--- Installer/Package Linux release.bat
+++ Installer/Package Linux release.bat
@@ -8,9 +8,11 @@ xcopy /I /Y ..\..\..\..\Duplicati\GUI\Bin\Release\* .
 del *.pdb /Q
 xcopy /I /Y ..\..\..\..\thirdparty\SQLite\Bin\sqlite-3.6.12.so .
 move sqlite-3.6.12.so libsqlite3.so.0
-xcopy /I /Y "..\..\..\..\thirdparty\SQLite\Bin\linux howto.txt" .
-move "linux howto.txt" linux-sqlite-readme.txt
+xcopy /I /Y "..\..\..\linux help\*" .
+move "linux-sqlite-readme.txt" "sqlite-readme.txt"
+move "linux-readme.txt" "README.txt"
 del "System.Data.SQLite.dll" /Q
+del "*.vshost.*" /Q
 xcopy /I /Y "..\..\..\..\thirdparty\SQLite\Dll for .Net\ManagedOnly\System.Data.SQLite.dll" .
 xcopy /I /Y ..\..\..\..\Duplicati\GUI\StartDuplicati.sh .
 pause
---
+++ Installer/linux help/linux-sqlite-readme.txt
@@ -0,0 +1,6 @@
+If Duplicati complains about the version of SQLite being too old,
+use the StartDuplicati.sh script to launch Duplicati with the bundled
+SQLite library.
+
+The StartDuplicati.sh script can be made executable with this command:
+chmod +x StartDuplicati.sh
