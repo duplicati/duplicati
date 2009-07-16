8588ff4cf20035a8a69033d5917527e9c4e51343
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2009-07-16 20:54:36 +0000

    Added copyright to WixProjBuilder
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@226 59da171f-624f-0410-aa54-27559c288bec

--- WixProjBuilder/Program.cs
+++ WixProjBuilder/Program.cs
@@ -1,4 +1,23 @@
-﻿using System;
+#region Disclaimer / License
+// Copyright (C) 2009, Kenneth Skovhede
+// http://www.hexad.dk, opensource@hexad.dk
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+#endregion
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -160,7 +179,7 @@ namespace WixProjBuilder
             psi.UseShellExecute = false;
 
             System.Diagnostics.Process p = System.Diagnostics.Process.Start(psi);
-            p.WaitForExit(60000); //Wait up to one minute
+            p.WaitForExit(5 * 60 * 1000); //Wait up to five minutes
             if (!p.HasExited)
             {
                 try { p.Kill(); }
