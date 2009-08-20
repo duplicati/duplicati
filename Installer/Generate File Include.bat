a0c6b91a9b7ab880a637ad70b5b0ec3beff22928
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2009-08-20 20:39:07 +0000

    Greatly simplified the build procedure, now that there is just one build
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@260 59da171f-624f-0410-aa54-27559c288bec

--- Installer/Generate File Include.bat
+++
@@ -1,2 +0,0 @@
-paraffin -dir ..\Duplicati\GUI\bin\Release\ -custom DUPLICATIBIN -dirref INSTALLLOCATION -ext .pdb -alias ..\Duplicati\GUI\bin\Release\ -guids incBinFiles.wxs 
-pause          
--- Installer/VS Express build.bat
+++ Installer/VS Express build.bat
@@ -1,9 +1,25 @@
-xcopy /Y "..\thirdparty\SQLite\Dll for .Net\System.Data.SQLite.dll" "..\Duplicati\GUI\bin\Release\System.Data.SQLite.dll"
-WixProjBuilder.exe WixInstaller.wixproj
-move /Y bin\Release\Duplicati.msi "bin\Release\Duplicati x86.msi"
+@echo off
+cd bin\Release
+mkdir Duplicati
+cd Duplicati
+del * /Q
+xcopy /I /Y /E ..\..\..\..\Duplicati\GUI\Bin\Release\* .
+del *.pdb /Q
+xcopy /I /Y ..\..\..\..\thirdparty\SQLite\Bin\sqlite-3.6.12.so .
+move sqlite-3.6.12.so libsqlite3.so.0
+xcopy /I /Y "..\..\..\linux help\*" .
+del "*.vshost.*" /Q
+xcopy /I /Y ..\..\..\..\Duplicati\GUI\StartDuplicati.sh .
 
-xcopy /Y "..\thirdparty\SQLite\Dll for .Net\x64\System.Data.SQLite.dll" "..\Duplicati\GUI\bin\Release\System.Data.SQLite.dll"
-WixProjBuilder.exe WixInstaller.wixproj
-move /Y bin\Release\Duplicati.msi "bin\Release\Duplicati x64.msi"
+cd ..
+del ..\Duplicati.msi
+del ..\Duplicati.zip
 
+"%PROGRAMFILES%\7-zip\7z.exe" a -r Duplicati.zip Duplicati
+
+cd ..\..
+
+paraffin -dir bin\Release\Duplicati -custom DUPLICATIBIN -dirref ProgramFilesFolder -ext .pdb -alias bin\Release\Duplicati -guids incBinFiles.wxs 
+
+WixProjBuilder.exe WixInstaller.wixproj
 pause
