36ff6e736d2a22f09a1ca936bb690cf1475f23e2
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2009-08-20 20:54:43 +0000

    Updated license texts and bumped version numbers
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@261 59da171f-624f-0410-aa54-27559c288bec

--- Duplicati/CommandLine/BackendTester/Properties/AssemblyInfo.cs
+++ Duplicati/CommandLine/BackendTester/Properties/AssemblyInfo.cs
@@ -1,3 +1,22 @@
+#region Disclaimer / License
+// Copyright (C) 2009, Kenneth Skovhede
+// http://www.hexad.dk, opensource@hexad.dk
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+#endregion
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -32,5 +51,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.235")]
-[assembly: AssemblyFileVersion("1.0.1.235")]
+[assembly: AssemblyVersion("1.0.1.261")]
+[assembly: AssemblyFileVersion("1.0.1.261")]
