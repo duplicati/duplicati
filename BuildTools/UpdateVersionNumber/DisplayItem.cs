c3fd3d1737788f22bae9587de091beed84452817
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2009-09-14 20:24:29 +0000

    Moved build tools to seperate folder
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@277 59da171f-624f-0410-aa54-27559c288bec

---
+++ BuildTools/UpdateVersionNumber/DisplayItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateVersionNumber
+{
+    public class DisplayItem
+    {
+        public DisplayItem(string file, Version version)
+        {
+            m_file = file;
+            m_version = version;
+        }
+
+        private string m_file;
+
+        public string File
+        {
+            get { return m_file; }
+            set { m_file = value; }
+        }
+        private Version m_version;
+
+        public Version Version
+        {
+            get { return m_version; }
+            set { m_version = value; }
+        }
+
+        public override string ToString()
+        {
+            return m_file + " (" + m_version.ToString() + ")";
+        }
+    }
+}
