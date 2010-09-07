7224cf432664a0e8edde50f119303c1638be7a8b
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2010-09-07 16:53:33 +0000

    Added a license tab to the options dialog to ensure compliance with thirdparty licenses.
    git-svn-id: https://duplicati.googlecode.com/svn/trunk/Duplicati/License@496 59da171f-624f-0410-aa54-27559c288bec

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Duplicati.License")]
+[assembly: AssemblyDescription("A license text reader for Duplicati")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HexaD")]
+[assembly: AssemblyProduct("Duplicati")]
+[assembly: AssemblyCopyright("LGPL, Copyright © HexaD 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2ab7f791-2317-41cf-966d-7451d9e63ae9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
