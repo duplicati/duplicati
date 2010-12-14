90aaa56a07645caf959f553a35667e94891a243f
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2010-12-14 07:36:39 +0000

    Updated the build script to also build any localization files.
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@631 59da171f-624f-0410-aa54-27559c288bec

--- Installer/VS Express build.bat
+++ Installer/VS Express build.bat
@@ -1,11 +1,18 @@
 @echo off
+SET PARAFFIN_EXE=%CD%\paraffin.exe
+SET ORIGIN_PATH=%CD%
+
+rmdir /S /Q "bin\Release\Duplicati"
+
 mkdir bin
 cd bin
 mkdir Release
 cd Release
+
+
 mkdir Duplicati
 cd Duplicati
-del * /Q
+
 xcopy /I /Y /E ..\..\..\..\Duplicati\GUI\Bin\Release\* .
 del *.pdb /Q
 xcopy /I /Y ..\..\..\..\thirdparty\SQLite\Bin\sqlite-3.6.12.so .
@@ -25,37 +32,59 @@ echo "" > "Duplicati.exe.config"
 xcopy /Y ..\..\..\AssemblyRedirects.xml "Duplicati.exe.config"
 xcopy /Y ..\..\..\AssemblyRedirects.xml "Duplicati.CommandLine.exe.config"
 
-REM Build translations : *TODO* Make this pick up output from the "compiled" folder automatically
-REM ..\..\..\..\Duplicati\Localization\LocalizationTool.exe CLEAN
-del "..\..\..\..\Duplicati\Localization\compiled\*" /Q
-..\..\..\..\Duplicati\Localization\LocalizationTool.exe UPDATE
-..\..\..\..\Duplicati\Localization\LocalizationTool.exe BUILD
-xcopy /I /Y /E ..\..\..\..\Duplicati\Localization\Compiled\fr-FR fr-FR
-xcopy /I /Y /E ..\..\..\..\Duplicati\Localization\Compiled\pt-BR pt-BR
-
 cd ..
-del ..\Duplicati.msi /Q
-del ..\Duplicati.x86.msi /Q
-del ..\Duplicati.x64.msi /Q
-del ..\Duplicati.zip /Q
-
-
-"%PROGRAMFILES%\7-zip\7z.exe" a -r Duplicati.zip Duplicati
+del .\Duplicati.msi /Q
+del .\Duplicati.x86.msi /Q
+del .\Duplicati.x64.msi /Q
+del .\Duplicati.zip /Q
 
 cd ..\..
 
+REM Build translations
+cd "..\Duplicati\Localization"
+rmdir /S /Q compiled
+
+LocalizationTool.exe update
+LocalizationTool.exe build
+for /D %%d in ("compiled\*") do call :langbuild %%d
+
+cd "..\..\Installer"
+
 REM Create incBinFiles.wxs if required
 REM Does NOT set the FILE_DUPLICATI_MAIN_EXE id on the main file, which is required to build the MSI
-if not exist incBinFiles.wxs paraffin -dir bin\Release\Duplicati -groupname DUPLICATIBIN -dirref INSTALLLOCATION -ext .pdb -ext .0 -alias bin\Release\Duplicati -norootdirectory -multiple -Win64var "$(var.Win64)" incBinFiles.wxs 
+if not exist incBinFiles.wxs "%PARAFFIN_EXE%" -dir bin\Release\Duplicati -groupname DUPLICATIBIN -dirref INSTALLLOCATION -ext .pdb -ext .0 -alias bin\Release\Duplicati -norootdirectory -multiple -Win64var "$(var.Win64)" incBinFiles.wxs 
 
 REM Update version
 if exist incBinFiles.PARAFFIN del incBinFiles.PARAFFIN
-paraffin -update incBinFiles.wxs
+"%PARAFFIN_EXE%" -update incBinFiles.wxs
 if exist incBinFiles.PARAFFIN xcopy /I /Y incBinFiles.PARAFFIN incBinFiles.wxs
 if exist incBinFiles.PARAFFIN del incBinFiles.PARAFFIN
+
+xcopy /I /Y /E "..\Duplicati\Localization\compiled\*" "bin\Release\Duplicati"
+"%PROGRAMFILES%\7-zip\7z.exe" a -r "bin\Release\Duplicati\Duplicati.zip" bin\Release\Duplicati
 
 WixProjBuilder.exe --wixpath="C:\Program Files (x86)\Windows Installer XML v3\bin" WixInstaller.wixproj
 move "bin\Release\Duplicati.msi" "bin\Release\Duplicati.x86.msi"
 WixProjBuilder.exe --wixpath="C:\Program Files (x86)\Windows Installer XML v3\bin" --platform=x64 WixInstaller.wixproj
 move "bin\Release\Duplicati.msi" "bin\Release\Duplicati.x64.msi"
 pause
+
+goto end_of_program
+
+:langbuild
+set FOLDERNAME=
+
+for /f "tokens=*" %%a in ("%1") do set FOLDERNAME=%%~na
+
+SET LANG_WXS_NAME=%ORIGIN_PATH%\incLocFiles.%FOLDERNAME%.wxs
+SET LANG_PRF_NAME=%ORIGIN_PATH%\incLocFiles.%FOLDERNAME%.PARAFFIN
+
+if not exist "%LANG_WXS_NAME%" "%PARAFFIN_EXE%" -dir %1 -alias ..\Duplicati\Localization\%1 -groupname DUPLICATI_LANG_%FOLDERNAME% -dirref INSTALLLOCATION "%LANG_WXS_NAME%" -Win64var "$(var.Win64)" -multiple -ext .pdb -direXclude .svn
+if exist "%LANG_PRF_NAME%" del "%LANG_PRF_NAME%"
+"%PARAFFIN_EXE%" -update "%ORIGIN_PATH%\incLocFiles.%FOLDERNAME%.wxs"
+if exist "%LANG_PRF_NAME%" xcopy /I /Y "%LANG_PRF_NAME%" "%LANG_WXS_NAME%"
+if exist "%LANG_PRF_NAME%" del "%LANG_PRF_NAME%"
+
+goto end_of_program
+
+:end_of_program
