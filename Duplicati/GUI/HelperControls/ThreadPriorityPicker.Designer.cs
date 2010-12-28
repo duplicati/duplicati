75000e64eb415fa54e6d85efa45ce6ff3bdf0377
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2010-12-28 14:27:34 +0000

    Update issue #321 Status: Fixed
    There were some issues with namespaces and signing of the files,
    which caused the translations to not load.
    
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@649 59da171f-624f-0410-aa54-27559c288bec

--- Duplicati/GUI/HelperControls/ThreadPriorityPicker.Designer.cs
+++ Duplicati/GUI/HelperControls/ThreadPriorityPicker.Designer.cs
@@ -28,48 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ThreadPriorityPicker));
             this.ThreadPriority = new System.Windows.Forms.ComboBox();
             this.ThreadPriorityEnabled = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // ThreadPriority
             // 
-            this.ThreadPriority.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.ThreadPriority, "ThreadPriority");
             this.ThreadPriority.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.ThreadPriority.Enabled = false;
             this.ThreadPriority.FormattingEnabled = true;
             this.ThreadPriority.Items.AddRange(new object[] {
-            "High (Not recommended)",
-            "Above normal",
-            "Normal",
-            "Below normal",
-            "Low"});
-            this.ThreadPriority.Location = new System.Drawing.Point(152, 0);
+            resources.GetString("ThreadPriority.Items"),
+            resources.GetString("ThreadPriority.Items1"),
+            resources.GetString("ThreadPriority.Items2"),
+            resources.GetString("ThreadPriority.Items3"),
+            resources.GetString("ThreadPriority.Items4")});
             this.ThreadPriority.Name = "ThreadPriority";
-            this.ThreadPriority.Size = new System.Drawing.Size(168, 21);
-            this.ThreadPriority.TabIndex = 20;
             this.ThreadPriority.SelectedIndexChanged += new System.EventHandler(this.ThreadPriority_SelectedIndexChanged);
             // 
             // ThreadPriorityEnabled
             // 
-            this.ThreadPriorityEnabled.AutoSize = true;
-            this.ThreadPriorityEnabled.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.ThreadPriorityEnabled.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.ThreadPriorityEnabled, "ThreadPriorityEnabled");
             this.ThreadPriorityEnabled.Name = "ThreadPriorityEnabled";
-            this.ThreadPriorityEnabled.Size = new System.Drawing.Size(108, 17);
-            this.ThreadPriorityEnabled.TabIndex = 19;
-            this.ThreadPriorityEnabled.Text = "Set thread priority";
             this.ThreadPriorityEnabled.UseVisualStyleBackColor = true;
             this.ThreadPriorityEnabled.CheckedChanged += new System.EventHandler(this.ThreadPriorityEnabled_CheckedChanged);
             // 
             // ThreadPriorityPicker
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.ThreadPriority);
             this.Controls.Add(this.ThreadPriorityEnabled);
             this.Name = "ThreadPriorityPicker";
-            this.Size = new System.Drawing.Size(322, 23);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Duplicati/Library/Backend/SSH/Properties/AssemblyInfo.cs
+++ Duplicati/Library/Backend/SSH/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.1.299")]
-[assembly: AssemblyFileVersion("1.0.1.299")]
+[assembly: AssemblyVersion("1.0.1.276")]
+[assembly: AssemblyFileVersion("1.0.1.276")]
