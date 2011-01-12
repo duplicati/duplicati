29ae917de278095ce37a313b240b698c859c6673
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2011-01-12 18:59:01 +0000

    Update issue #347 Status: Fixed I have now removed the code required to activate the deprecated AES crypt encryption, but kept the code required to decode it,
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@670 59da171f-624f-0410-aa54-27559c288bec

--- Duplicati/Library/Encryption/AESEncryption.cs
+++ Duplicati/Library/Encryption/AESEncryption.cs
@@ -33,19 +33,11 @@ namespace Duplicati.Library.Encryption
     {
         #region Commandline option constants
         /// <summary>
-        /// The commandline switch used to specify that AES Crypt is not allowed
-        /// </summary>
-        private const string COMMAND_LINE_NO_AES_CRYPT = "aes-encryption-dont-use-aescrypt";
-        /// <summary>
         /// The commandline switch used to specify that fallback decryption is not allowed
         /// </summary>
         private const string COMMAND_LINE_NO_FALLBACK = "aes-encryption-dont-allow-fallback";
         #endregion
 
-        /// <summary>
-        /// True if AESCrypt is disabled, false otherwise
-        /// </summary>
-        private bool m_disableAESCrypt = false;
         /// <summary>
         /// True if fallback is allowed, false otherwise
         /// </summary>
@@ -68,61 +60,16 @@ namespace Duplicati.Library.Encryption
         /// <param name="key">The key used for encryption. The key gets stretched through SHA hashing to fit the key size requirements</param>
         public AESEncryption(string passphrase, Dictionary<string, string> options)
         {
-            if (options.ContainsKey(COMMAND_LINE_NO_AES_CRYPT))
-                m_disableAESCrypt = Utility.Utility.ParseBool(options[COMMAND_LINE_NO_AES_CRYPT], true);
             if (options.ContainsKey(COMMAND_LINE_NO_FALLBACK))
                 m_allowFallback = !Utility.Utility.ParseBool(options[COMMAND_LINE_NO_FALLBACK], true);
 
-            if (m_disableAESCrypt && !m_allowFallback)
-                throw new ArgumentException(string.Format(Strings.AESEncryption.OptionsAreMutuallyExclusiveError, COMMAND_LINE_NO_AES_CRYPT, COMMAND_LINE_NO_FALLBACK));
-
             m_key = passphrase;
-        }
-
-        private System.Security.Cryptography.SymmetricAlgorithm GenerateAESEncryptor(string key)
-        {
-            SymmetricAlgorithm crypto = Rijndael.Create();
-            SHA256 sha = SHA256.Create();
-            int len = crypto.IV.Length + crypto.Key.Length;
-            System.IO.MemoryStream ms = new System.IO.MemoryStream();
-
-            //We stretch the key material by issuing cascading hash operations.
-            //This somewhat alters the characteristics of the key, but as long
-            //as the hashing is cryptographically safe and does not induce aliasing
-            //with the encryption, it does not reduce the strength of the encryption
-            byte[] tmp = Encoding.UTF8.GetBytes(key);
-            while (ms.Length < len)
-            {
-                if (!sha.CanReuseTransform)
-                    sha = SHA256.Create();
-                sha.Initialize();
-
-                tmp = sha.ComputeHash(tmp);
-                ms.Write(tmp, 0, (int)Math.Min(tmp.Length, len - ms.Length));
-            }
-
-            //TODO: Is it better to change the IV for each compressed file?
-            byte[] realkey = new byte[crypto.Key.Length];
-            byte[] iv = new byte[crypto.IV.Length];
-            ms.Position = 0;
-            if (ms.Read(iv, 0, iv.Length) != iv.Length)
-                throw new Exception(Strings.AESEncryption.BadKeyStretchError);
-            if (ms.Read(realkey, 0, realkey.Length) != realkey.Length)
-                throw new Exception(Strings.AESEncryption.BadKeyStretchError);
-            ms.Dispose();
-
-            crypto.IV = iv;
-            crypto.Key = realkey;
-            crypto.Mode = CipherMode.CBC;
-            crypto.Padding = PaddingMode.PKCS7;
-
-            return crypto;
         }
 
         #region IEncryption Members
 
         public override string FilenameExtension { get { return "aes"; } }
-        public override string Description { get { return string.Format(Strings.AESEncryption.Description, COMMAND_LINE_NO_AES_CRYPT); } }
+        public override string Description { get { return string.Format(Strings.AESEncryption.Description_v2); } }
         public override string DisplayName { get { return Strings.AESEncryption.DisplayName; } }
         protected override void Dispose(bool disposing) { m_key = null; }
 
@@ -137,18 +84,11 @@ namespace Duplicati.Library.Encryption
 
         public override Stream Encrypt(Stream input)
         {
-            if (m_disableAESCrypt)
-                return new CryptoStream(input, GenerateAESEncryptor(m_key).CreateEncryptor(), System.Security.Cryptography.CryptoStreamMode.Write);
-            else
-                return new SharpAESCrypt.SharpAESCrypt(m_key, input, SharpAESCrypt.OperationMode.Encrypt);
-
+            return new SharpAESCrypt.SharpAESCrypt(m_key, input, SharpAESCrypt.OperationMode.Encrypt);
         }
 
         public override Stream Decrypt(Stream input)
         {
-            if (m_disableAESCrypt)
-                return new System.Security.Cryptography.CryptoStream(input, GenerateAESEncryptor(m_key).CreateDecryptor(), System.Security.Cryptography.CryptoStreamMode.Read);
-
             try
             {
                 return new SharpAESCrypt.SharpAESCrypt(m_key, input, SharpAESCrypt.OperationMode.Decrypt);
@@ -158,7 +98,7 @@ namespace Duplicati.Library.Encryption
                 if (m_allowFallback)
                 {
                     input.Seek(0, SeekOrigin.Begin);
-                    return new System.Security.Cryptography.CryptoStream(input, GenerateAESEncryptor(m_key).CreateDecryptor(), System.Security.Cryptography.CryptoStreamMode.Read);
+                    return new System.Security.Cryptography.CryptoStream(input, GenerateOldAESDecryptor(m_key), System.Security.Cryptography.CryptoStreamMode.Read);
                 }
                 else
                     throw;
@@ -170,8 +110,7 @@ namespace Duplicati.Library.Encryption
             get 
             {
                 return new List<ICommandLineArgument>(new ICommandLineArgument[] {
-                    new CommandLineArgument(COMMAND_LINE_NO_AES_CRYPT, CommandLineArgument.ArgumentType.Boolean, Strings.AESEncryption.AesencryptiondontallowfallbackShort, string.Format(Strings.AESEncryption.AesencryptiondontallowfallbackLong, COMMAND_LINE_NO_FALLBACK), "false"),
-                    new CommandLineArgument(COMMAND_LINE_NO_FALLBACK, CommandLineArgument.ArgumentType.Boolean, Strings.AESEncryption.AesencryptiondontuseaescryptShort, string.Format(Strings.AESEncryption.AesencryptiondontuseaescryptLong, COMMAND_LINE_NO_AES_CRYPT), "false")
+                    new CommandLineArgument(COMMAND_LINE_NO_FALLBACK, CommandLineArgument.ArgumentType.Boolean, Strings.AESEncryption.AesencryptiondontallowfallbackShort, Strings.AESEncryption.AesencryptiondontallowfallbackLong_v2, "false")
                 });
             }
         }
@@ -207,6 +146,54 @@ namespace Duplicati.Library.Encryption
         public string GetConfiguration(IDictionary<string, string> applicationSettings, IDictionary<string, string> guiOptions, IDictionary<string, string> commandlineOptions)
         {
             return null;
+        }
+
+        #endregion
+
+        #region Previous decryption scheme support
+
+        private ICryptoTransform GenerateOldAESDecryptor(string key)
+        {
+            //Note this encryption scheme is not supported.
+            //It is still present because it allows Duplicati to
+            // read files that were created before the AESCrypt format
+            // was used.
+            SymmetricAlgorithm crypto = Rijndael.Create();
+            SHA256 sha = SHA256.Create();
+            int len = crypto.IV.Length + crypto.Key.Length;
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+
+            //We stretch the key material by issuing cascading hash operations.
+            //This somewhat alters the characteristics of the key, but as long
+            //as the hashing is cryptographically safe and does not induce aliasing
+            //with the encryption, it does not reduce the strength of the encryption
+            byte[] tmp = Encoding.UTF8.GetBytes(key);
+            while (ms.Length < len)
+            {
+                if (!sha.CanReuseTransform)
+                    sha = SHA256.Create();
+                sha.Initialize();
+
+                tmp = sha.ComputeHash(tmp);
+                ms.Write(tmp, 0, (int)Math.Min(tmp.Length, len - ms.Length));
+            }
+
+            //Note this code is deprecated as it encrypts all files with the same key
+            byte[] realkey = new byte[crypto.Key.Length];
+            byte[] iv = new byte[crypto.IV.Length];
+            ms.Position = 0;
+            if (ms.Read(iv, 0, iv.Length) != iv.Length)
+                throw new Exception(Strings.AESEncryption.BadKeyStretchError);
+            if (ms.Read(realkey, 0, realkey.Length) != realkey.Length)
+                throw new Exception(Strings.AESEncryption.BadKeyStretchError);
+            ms.Dispose();
+
+            crypto.IV = iv;
+            crypto.Key = realkey;
+            crypto.Mode = CipherMode.CBC;
+            crypto.Padding = PaddingMode.PKCS7;
+
+            return crypto.CreateDecryptor();
         }
 
         #endregion
