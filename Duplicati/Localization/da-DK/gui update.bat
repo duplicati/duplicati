407ab5017dd3d0444cedefcf40e96daddc7f0025
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2011-03-27 15:25:59 +0000

    Update issue #381 Status: Fixed
    Removed all the extra translation files,
    so the system is now solely based on CSV files
    
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@737 59da171f-624f-0410-aa54-27559c288bec

--- Duplicati/Localization/da-DK/gui update.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Processing updates, please wait ...
-
-set FOLDERNAME=
-for /f "tokens=*" %%a in ("%CD%") do set FOLDERNAME=%%~na
-
-..\LocalizationTool.exe update %FOLDERNAME%
-..\LocalizationTool.exe guiupdate %FOLDERNAME%
