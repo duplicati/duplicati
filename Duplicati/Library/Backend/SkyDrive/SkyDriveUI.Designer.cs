992f696b2939e9c2d3641cfc5c01523ae2f0c1f8
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2011-06-29 06:30:02 +0000

    Update issue #119 Status: Fixed
    There is now a SkyDrive backend.
    
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@820 59da171f-624f-0410-aa54-27559c288bec

---
+++ Duplicati/Library/Backend/SkyDrive/SkyDriveUI.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Duplicati.Library.Backend
+{
+    partial class SkyDriveUI
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateFolderButton = new System.Windows.Forms.Button();
+            this.TestConnection = new System.Windows.Forms.Button();
+            this.SignUpLink = new System.Windows.Forms.LinkLabel();
+            this.Path = new System.Windows.Forms.TextBox();
+            this.Password = new Duplicati.Winforms.Controls.PasswordControl();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CreateFolderButton
+            // 
+            this.CreateFolderButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.CreateFolderButton.Location = new System.Drawing.Point(362, 79);
+            this.CreateFolderButton.Name = "CreateFolderButton";
+            this.CreateFolderButton.Size = new System.Drawing.Size(112, 24);
+            this.CreateFolderButton.TabIndex = 25;
+            this.CreateFolderButton.Text = "Create folder";
+            this.CreateFolderButton.UseVisualStyleBackColor = true;
+            this.CreateFolderButton.Click += new System.EventHandler(this.CreateFolderButton_Click);
+            // 
+            // TestConnection
+            // 
+            this.TestConnection.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.TestConnection.Location = new System.Drawing.Point(360, 160);
+            this.TestConnection.Name = "TestConnection";
+            this.TestConnection.Size = new System.Drawing.Size(112, 24);
+            this.TestConnection.TabIndex = 26;
+            this.TestConnection.Text = "Test Connection";
+            this.TestConnection.UseVisualStyleBackColor = true;
+            this.TestConnection.Click += new System.EventHandler(this.TestConnection_Click);
+            // 
+            // SignUpLink
+            // 
+            this.SignUpLink.AutoSize = true;
+            this.SignUpLink.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.SignUpLink.Location = new System.Drawing.Point(330, 39);
+            this.SignUpLink.Name = "SignUpLink";
+            this.SignUpLink.Size = new System.Drawing.Size(151, 13);
+            this.SignUpLink.TabIndex = 24;
+            this.SignUpLink.TabStop = true;
+            this.SignUpLink.Text = "Click here for the sign up page";
+            this.SignUpLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.SignUpLink_LinkClicked);
+            // 
+            // Path
+            // 
+            this.Path.Location = new System.Drawing.Point(136, 80);
+            this.Path.Name = "Path";
+            this.Path.Size = new System.Drawing.Size(152, 20);
+            this.Path.TabIndex = 22;
+            this.Path.TextChanged += new System.EventHandler(this.Path_TextChanged);
+            // 
+            // Password
+            // 
+            this.Password.AskToEnterNewPassword = false;
+            this.Password.InitialPassword = null;
+            this.Password.IsPasswordVisible = false;
+            this.Password.Location = new System.Drawing.Point(136, 56);
+            this.Password.MaximumSize = new System.Drawing.Size(5000, 20);
+            this.Password.MinimumSize = new System.Drawing.Size(150, 20);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(152, 20);
+            this.Password.TabIndex = 20;
+            this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(24, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "SkyDrive folder path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(24, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Passport Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(24, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Passport Username";
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(136, 32);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(152, 20);
+            this.Username.TabIndex = 27;
+            this.Username.TextChanged += new System.EventHandler(this.Username_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(304, 48);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Please note that there is no official API and accessing SkyDrive through this bac" +
+                "kend may violate the ToS and may stop to work if Microsoft decides to change the" +
+                " API.";
+            // 
+            // SkyDriveUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.CreateFolderButton);
+            this.Controls.Add(this.TestConnection);
+            this.Controls.Add(this.SignUpLink);
+            this.Controls.Add(this.Path);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "SkyDriveUI";
+            this.Size = new System.Drawing.Size(506, 242);
+            this.Load += new System.EventHandler(this.SkyDriveUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateFolderButton;
+        private System.Windows.Forms.Button TestConnection;
+        private System.Windows.Forms.LinkLabel SignUpLink;
+        private System.Windows.Forms.TextBox Path;
+        private Duplicati.Winforms.Controls.PasswordControl Password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Duplicati/Library/Backend/SkyDrive/SkyDriveUI.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Duplicati.Library.Backend
+{
+    public partial class SkyDriveUI : UserControl
+    {
+        private const string USERNAME = "Username";
+        private const string PASSWORD = "Password";
+        private const string FOLDER = "Folder";
+
+        private const string HAS_TESTED = "UI: Has tested";
+        private const string INITIALPASSWORD = "UI: Temp password";
+
+        private bool m_hasTested;
+
+        private IDictionary<string, string> m_options;
+
+        public SkyDriveUI(IDictionary<string, string> options)
+            : this()
+        {
+            m_options = options;
+        }
+
+        private SkyDriveUI()
+        {
+            InitializeComponent();
+        }
+
+        internal bool Save(bool validate)
+        {
+            Save();
+
+            if (!validate)
+                return true;
+
+            if (!ValidateForm())
+                return false;
+
+            if (!m_hasTested)
+                switch (MessageBox.Show(this, Interface.CommonStrings.ConfirmTestConnectionQuestion, Application.ProductName, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question))
+                {
+                    case DialogResult.Yes:
+                        TestConnection_Click(null, null);
+                        if (!m_hasTested)
+                            return false;
+                        break;
+                    case DialogResult.No:
+                        break;
+                    default: //Cancel
+                        return false;
+                }
+
+            Save();
+
+            m_options.Remove(INITIALPASSWORD);
+
+            return true;
+        }
+
+        private void SkyDriveUI_Load(object sender, EventArgs e)
+        {
+            if (m_options.ContainsKey(USERNAME))
+                Username.Text = m_options[USERNAME];
+            if (m_options.ContainsKey(PASSWORD))
+                Password.Text = m_options[PASSWORD];
+            if (m_options.ContainsKey(FOLDER))
+                Path.Text = m_options[FOLDER];
+
+            if (!m_options.ContainsKey(INITIALPASSWORD))
+                m_options[INITIALPASSWORD] = m_options.ContainsKey(PASSWORD) ? m_options[PASSWORD] : "";
+            Password.AskToEnterNewPassword = !string.IsNullOrEmpty(m_options[INITIALPASSWORD]);
+            Password.InitialPassword = m_options[INITIALPASSWORD];
+
+            if (!m_options.ContainsKey(HAS_TESTED) || !bool.TryParse(m_options[HAS_TESTED], out m_hasTested))
+                m_hasTested = false;
+        }
+
+        private bool ValidateForm()
+        {
+
+            if (Path.Text.Trim().Length <= 0)
+            {
+                MessageBox.Show(this, Strings.SkyDriveUI.EmptyFolderPathError, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                try { Path.Focus(); }
+                catch { }
+
+                return false;
+            }
+
+            if (Username.Text.Trim().Length <= 0)
+            {
+                MessageBox.Show(this, Interface.CommonStrings.EmptyUsernameError, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                try { Username.Focus(); }
+                catch { }
+
+                return false;
+            }
+
+            if (Password.Text.Trim().Length <= 0)
+            {
+                MessageBox.Show(this, Interface.CommonStrings.EmptyPasswordError, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                try { Password.Focus(); }
+                catch { }
+
+                return false;
+            }
+
+            if (Password.Text.Length > 0 && !Password.VerifyPasswordIfChanged())
+                return false;
+
+
+
+            return true;
+        }
+
+        private void Save()
+        {
+            string initialPwd;
+            bool hasInitial = m_options.TryGetValue(INITIALPASSWORD, out initialPwd);
+
+            m_options.Clear();
+            m_options[HAS_TESTED] = m_hasTested.ToString();
+            m_options[FOLDER] = Path.Text;
+            m_options[USERNAME] = Username.Text;
+            m_options[PASSWORD] = Password.Text;
+
+            if (hasInitial)
+                m_options[INITIALPASSWORD] = initialPwd;
+        }
+
+        private void TestConnection_Click(object sender, EventArgs e)
+        {
+            if (ValidateForm())
+            {
+                bool retry = true;
+                while (retry == true)
+                {
+                    retry = false;
+                    Cursor c = this.Cursor;
+                    try
+                    {
+                        this.Cursor = Cursors.WaitCursor;
+
+                        Save();
+                        Dictionary<string, string> options = new Dictionary<string, string>();
+                        string destination = GetConfiguration(m_options, options);
+
+                        SkyDrive skyDrive = new SkyDrive(destination, options);
+                        skyDrive.List();
+
+                        MessageBox.Show(this, Interface.CommonStrings.ConnectionSuccess, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        m_hasTested = true;
+                    }
+                    catch (Interface.FolderMissingException)
+                    {
+                        switch (MessageBox.Show(this, Strings.SkyDriveUI.CreateMissingFolderQuestion, Application.ProductName, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question))
+                        {
+                            case DialogResult.Yes:
+                                CreateFolderButton.PerformClick();
+                                TestConnection.PerformClick();
+                                return;
+                            default:
+                                return;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(this, string.Format(Interface.CommonStrings.ConnectionFailure, ex.Message), Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        this.Cursor = c;
+                    }
+                }
+            }
+        }
+
+        private void Username_TextChanged(object sender, EventArgs e)
+        {
+            m_hasTested = false;
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+            m_hasTested = false;
+        }
+
+        private void Path_TextChanged(object sender, EventArgs e)
+        {
+            m_hasTested = false;
+        }
+
+        private void CreateFolderButton_Click(object sender, EventArgs e)
+        {
+            if (ValidateForm())
+            {
+                bool retry = true;
+                while (retry == true)
+                {
+                    retry = false;
+                    Cursor c = this.Cursor;
+                    try
+                    {
+                        this.Cursor = Cursors.WaitCursor;
+                        Save();
+
+                        Dictionary<string, string> options = new Dictionary<string, string>();
+                        string destination = GetConfiguration(m_options, options);
+
+                        SkyDrive skyDrive = new SkyDrive(destination, options);
+                        skyDrive.CreateFolder();
+
+                        MessageBox.Show(this, Interface.CommonStrings.FolderCreated, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        m_hasTested = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(this, string.Format(Interface.CommonStrings.ConnectionFailure, ex.Message), Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        this.Cursor = c;
+                    }
+                }
+            }
+        }
+
+        internal static string GetConfiguration(IDictionary<string, string> guiOptions, IDictionary<string, string> commandlineOptions)
+        {
+            if (guiOptions.ContainsKey(USERNAME) && !string.IsNullOrEmpty(guiOptions[USERNAME]))
+                commandlineOptions["ftp-username"] = guiOptions[USERNAME];
+
+            if (guiOptions.ContainsKey(PASSWORD) && !string.IsNullOrEmpty(guiOptions[PASSWORD]))
+                commandlineOptions["ftp-password"] = guiOptions[PASSWORD];
+
+            string folder;
+            guiOptions.TryGetValue(FOLDER, out folder);
+
+            if (string.IsNullOrEmpty(folder))
+                throw new Exception(string.Format(Interface.CommonStrings.ConfigurationIsMissingItemError, FOLDER));
+
+            if (folder.StartsWith("/"))
+                folder = folder.Substring(1);
+
+            if (string.IsNullOrEmpty(folder))
+                throw new Exception(string.Format(Interface.CommonStrings.ConfigurationIsMissingItemError, FOLDER));
+
+            return "skydrive://" + guiOptions[FOLDER];
+        }
+
+        public static string PageTitle
+        {
+            get { return Strings.SkyDriveUI.PageTitle; }
+        }
+
+        public static string PageDescription
+        {
+            get { return Strings.SkyDriveUI.PageDescription; }
+        }
+
+        private void SignUpLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Duplicati.Library.Utility.UrlUtillity.OpenUrl("https://accountservices.passport.net/reg.srf?fid=RegCredOnlyEASI");
+        }
+    }
+}
