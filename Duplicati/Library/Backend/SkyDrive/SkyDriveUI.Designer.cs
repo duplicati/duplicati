65c169b248aad505350324b5b59b17257bbdd862
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2011-06-30 07:26:07 +0000

    Fixed a bug with fake urls that are invalid (eg. spaces in hostname).
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@822 59da171f-624f-0410-aa54-27559c288bec

--- Duplicati/Library/Backend/SkyDrive/SkyDriveUI.Designer.cs
+++ Duplicati/Library/Backend/SkyDrive/SkyDriveUI.Designer.cs
@@ -46,7 +46,7 @@
             this.CreateFolderButton.Location = new System.Drawing.Point(362, 79);
             this.CreateFolderButton.Name = "CreateFolderButton";
             this.CreateFolderButton.Size = new System.Drawing.Size(112, 24);
-            this.CreateFolderButton.TabIndex = 25;
+            this.CreateFolderButton.TabIndex = 8;
             this.CreateFolderButton.Text = "Create folder";
             this.CreateFolderButton.UseVisualStyleBackColor = true;
             this.CreateFolderButton.Click += new System.EventHandler(this.CreateFolderButton_Click);
@@ -57,7 +57,7 @@
             this.TestConnection.Location = new System.Drawing.Point(360, 160);
             this.TestConnection.Name = "TestConnection";
             this.TestConnection.Size = new System.Drawing.Size(112, 24);
-            this.TestConnection.TabIndex = 26;
+            this.TestConnection.TabIndex = 9;
             this.TestConnection.Text = "Test Connection";
             this.TestConnection.UseVisualStyleBackColor = true;
             this.TestConnection.Click += new System.EventHandler(this.TestConnection_Click);
@@ -69,7 +69,7 @@
             this.SignUpLink.Location = new System.Drawing.Point(330, 39);
             this.SignUpLink.Name = "SignUpLink";
             this.SignUpLink.Size = new System.Drawing.Size(151, 13);
-            this.SignUpLink.TabIndex = 24;
+            this.SignUpLink.TabIndex = 7;
             this.SignUpLink.TabStop = true;
             this.SignUpLink.Text = "Click here for the sign up page";
             this.SignUpLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.SignUpLink_LinkClicked);
@@ -79,7 +79,7 @@
             this.Path.Location = new System.Drawing.Point(136, 80);
             this.Path.Name = "Path";
             this.Path.Size = new System.Drawing.Size(152, 20);
-            this.Path.TabIndex = 22;
+            this.Path.TabIndex = 6;
             this.Path.TextChanged += new System.EventHandler(this.Path_TextChanged);
             // 
             // Password
@@ -92,7 +92,7 @@
             this.Password.MinimumSize = new System.Drawing.Size(150, 20);
             this.Password.Name = "Password";
             this.Password.Size = new System.Drawing.Size(152, 20);
-            this.Password.TabIndex = 20;
+            this.Password.TabIndex = 4;
             this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
             // 
             // label3
@@ -102,7 +102,7 @@
             this.label3.Location = new System.Drawing.Point(24, 80);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(103, 13);
-            this.label3.TabIndex = 21;
+            this.label3.TabIndex = 5;
             this.label3.Text = "SkyDrive folder path";
             // 
             // label2
@@ -112,7 +112,7 @@
             this.label2.Location = new System.Drawing.Point(24, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(97, 13);
-            this.label2.TabIndex = 19;
+            this.label2.TabIndex = 3;
             this.label2.Text = "Passport Password";
             // 
             // label1
@@ -122,7 +122,7 @@
             this.label1.Location = new System.Drawing.Point(24, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 13);
-            this.label1.TabIndex = 17;
+            this.label1.TabIndex = 1;
             this.label1.Text = "Passport Username";
             // 
             // Username
@@ -130,7 +130,7 @@
             this.Username.Location = new System.Drawing.Point(136, 32);
             this.Username.Name = "Username";
             this.Username.Size = new System.Drawing.Size(152, 20);
-            this.Username.TabIndex = 27;
+            this.Username.TabIndex = 2;
             this.Username.TextChanged += new System.EventHandler(this.Username_TextChanged);
             // 
             // label4
@@ -139,7 +139,7 @@
             this.label4.Location = new System.Drawing.Point(24, 144);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(304, 48);
-            this.label4.TabIndex = 28;
+            this.label4.TabIndex = 0;
             this.label4.Text = "Please note that there is no official API and accessing SkyDrive through this bac" +
                 "kend may violate the ToS and may stop to work if Microsoft decides to change the" +
                 " API.";
