6289d80060b5f26a26a9fd50cc11fae642c3effd
kenneth.skovhede@gmail.com <kenneth.skovhede@gmail.com@59da171f-624f-0410-aa54-27559c288bec>
2011-10-16 15:08:04 +0000

    Fixed an issue with re-entering the password page, where the control would display the saved password as "password".
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@936 59da171f-624f-0410-aa54-27559c288bec

--- Duplicati/GUI/Wizard pages/Add backup/PasswordSettings.cs
+++ Duplicati/GUI/Wizard pages/Add backup/PasswordSettings.cs
@@ -78,6 +78,7 @@ namespace Duplicati.GUI.Wizard_pages.Add_backup
             m_settings["Password:WarnedChanged"] = m_warnedChanged;
             m_settings["Password:SettingsChanged"] = m_settingsChanged;
             m_settings["Password:NewPasswordGenerated"] = m_hasGeneratedNewPassword;
+            m_settings["Password:AskToEnterNewPassword"] = Password.AskToEnterNewPassword;
 
             if (args.Direction == PageChangedDirection.Back)
                 return;
@@ -151,6 +152,7 @@ namespace Duplicati.GUI.Wizard_pages.Add_backup
             m_settings["Password:SettingsChanged"] = m_settingsChanged;
             m_settings["Password:WarnedChanged"] = m_warnedChanged;
             m_settings["Password:NewPasswordGenerated"] = m_hasGeneratedNewPassword;
+            m_settings["Password:AskToEnterNewPassword"] = Password.AskToEnterNewPassword;
 
             m_wrapper.BackupPassword = EnablePassword.Checked ? Password.Text : "";
 
@@ -212,6 +214,8 @@ namespace Duplicati.GUI.Wizard_pages.Add_backup
                 m_settingsChanged = (bool)m_settings["Password:SettingsChanged"];
             if (m_settings.ContainsKey("Password:NewPasswordGenerated"))
                 m_hasGeneratedNewPassword = (bool)m_settings["Password:NewPasswordGenerated"];
+            if (m_settings.ContainsKey("Password:AskToEnterNewPassword"))
+                Password.AskToEnterNewPassword = (bool)m_settings.ContainsKey("Password:AskToEnterNewPassword");
 
             if (m_wrapper.PrimayAction == WizardSettingsWrapper.MainAction.RestoreSetup || m_wrapper.PrimayAction == WizardSettingsWrapper.MainAction.Restore)
             {
