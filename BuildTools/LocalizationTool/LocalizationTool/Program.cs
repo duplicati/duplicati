3f80700fb419041531435410be6298044c8ba13d
kenneth@hexad.dk <kenneth@hexad.dk@59da171f-624f-0410-aa54-27559c288bec>
2012-04-27 17:13:22 +0000

    Fixed a minor issue with the LocalizationTool
    git-svn-id: https://duplicati.googlecode.com/svn/branches/1.3.1@1211 59da171f-624f-0410-aa54-27559c288bec

--- BuildTools/LocalizationTool/LocalizationTool/Program.cs
+++ BuildTools/LocalizationTool/LocalizationTool/Program.cs
@@ -487,7 +487,7 @@ namespace LocalizationTool
 
                     string path = f.Key;
                     if (f.Key.StartsWith(filenameprefix, Duplicati.Library.Utility.Utility.ClientFilenameStringComparision))
-                        path = f.Key.Substring(pfl);
+                        path = f.Key.Substring(Math.Min(pfl, f.Key.Length));
 					
 					if (Duplicati.Library.Utility.Utility.IsClientLinux)
 						path = path.Replace(System.IO.Path.DirectorySeparatorChar.ToString(), "\\");
