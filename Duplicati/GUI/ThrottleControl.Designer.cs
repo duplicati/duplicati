27adbc2c16dceb287dfae843377702da43bbdb9f
kenneth@hexad.dk <kenneth@hexad.dk@59da171f-624f-0410-aa54-27559c288bec>
2012-08-16 20:03:49 +0000

    Fixed a missing Localizable property
    git-svn-id: https://duplicati.googlecode.com/svn/trunk@1429 59da171f-624f-0410-aa54-27559c288bec

--- Duplicati/GUI/ThrottleControl.Designer.cs
+++ Duplicati/GUI/ThrottleControl.Designer.cs
@@ -37,60 +37,47 @@
             // 
             // OKBtn
             // 
-            this.OKBtn.Location = new System.Drawing.Point(96, 128);
+            resources.ApplyResources(this.OKBtn, "OKBtn");
             this.OKBtn.Name = "OKBtn";
-            this.OKBtn.Size = new System.Drawing.Size(75, 23);
-            this.OKBtn.TabIndex = 4;
-            this.OKBtn.Text = "OK";
             this.OKBtn.UseVisualStyleBackColor = true;
             this.OKBtn.Click += new System.EventHandler(this.OKBtn_Click);
             // 
             // CancelBtn
             // 
             this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.CancelBtn.Location = new System.Drawing.Point(184, 128);
+            resources.ApplyResources(this.CancelBtn, "CancelBtn");
             this.CancelBtn.Name = "CancelBtn";
-            this.CancelBtn.Size = new System.Drawing.Size(75, 23);
-            this.CancelBtn.TabIndex = 5;
-            this.CancelBtn.Text = "Cancel";
             this.CancelBtn.UseVisualStyleBackColor = true;
             this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
             // 
             // ThreadPriorityPicker
             // 
-            this.ThreadPriorityPicker.Location = new System.Drawing.Point(16, 16);
+            resources.ApplyResources(this.ThreadPriorityPicker, "ThreadPriorityPicker");
             this.ThreadPriorityPicker.Name = "ThreadPriorityPicker";
-            this.ThreadPriorityPicker.Size = new System.Drawing.Size(322, 23);
-            this.ThreadPriorityPicker.TabIndex = 6;
             // 
             // BandwidthLimit
             // 
             this.BandwidthLimit.DownloadLimit = null;
             this.BandwidthLimit.DownloadLimitInBytes = ((long)(0));
-            this.BandwidthLimit.Location = new System.Drawing.Point(16, 48);
+            resources.ApplyResources(this.BandwidthLimit, "BandwidthLimit");
             this.BandwidthLimit.Name = "BandwidthLimit";
-            this.BandwidthLimit.Size = new System.Drawing.Size(321, 54);
-            this.BandwidthLimit.TabIndex = 7;
             this.BandwidthLimit.UploadLimit = null;
             this.BandwidthLimit.UploadLimitInBytes = ((long)(0));
             // 
             // ThrottleControl
             // 
             this.AcceptButton = this.OKBtn;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.CancelBtn;
-            this.ClientSize = new System.Drawing.Size(352, 166);
             this.Controls.Add(this.BandwidthLimit);
             this.Controls.Add(this.ThreadPriorityPicker);
             this.Controls.Add(this.CancelBtn);
             this.Controls.Add(this.OKBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ThrottleControl";
-            this.Text = "Throttle control";
             this.Load += new System.EventHandler(this.ThrottleControl_Load);
             this.ResumeLayout(false);
 
