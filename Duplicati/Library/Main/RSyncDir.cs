duplicati/duplicati

commit fa97957296b657ec1ee0c5da3e48aab0dae96287

Fixed a bug introduced with r1381 that could cause incomplete backups.
git-svn-id: https://duplicati.googlecode.com/svn/trunk@1437 59da171f-624f-0410-aa54-27559c288bec

Duplicati/Library/Main/RSyncDir.cs
@@ -1364,11 +1364,12 @@ namespace Duplicati.Library.Main.RSync
                 m_lastPartialFile = WritePossiblePartial(m_lastPartialFile, contentfile, signaturefile, volumesize);
             }
 
-            if (m_lastPartialFile != null)
-                return false;
 
             while (m_unproccesed.Files.Count > 0)
             {
+                if (m_lastPartialFile != null)
+                    return false;
+
                 if (totalSize >= volumesize)
                     break;
 
@@ -1574,7 +1575,7 @@ namespace Duplicati.Library.Main.RSync
                 }
             }
 
-            if (m_unproccesed.Files.Count == 0)
+            if (m_unproccesed.Files.Count == 0 && m_lastPartialFile != null)
                 return FinalizeMultiPass(signaturefile, contentfile, volumesize);
             else
                 return false;

commit 806f9fcad5caf6874c55dc5af3b7cbea8ca05ee8

Fixed a bug introduced with r1437 that could cause backups to generate infinite set of empty volumes.
git-svn-id: https://duplicati.googlecode.com/svn/trunk@1441 59da171f-624f-0410-aa54-27559c288bec

Duplicati/Library/Main/RSyncDir.cs
@@ -1575,7 +1575,7 @@ namespace Duplicati.Library.Main.RSync
                 }
             }
 
-            if (m_unproccesed.Files.Count == 0 && m_lastPartialFile != null)
+            if (m_unproccesed.Files.Count == 0 && m_lastPartialFile == null)
                 return FinalizeMultiPass(signaturefile, contentfile, volumesize);
             else
                 return false;