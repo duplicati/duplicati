687a645b98cd8d0f7bf56a95db37c8d8509c9a88
kenneth@hexad.dk <kenneth@hexad.dk@59da171f-624f-0410-aa54-27559c288bec>
2012-08-23 07:09:19 +0000

    Merged r1441 into 1.3.x branch
    git-svn-id: https://duplicati.googlecode.com/svn/branches/1.3.x@1442 59da171f-624f-0410-aa54-27559c288bec

--- Duplicati/Library/Main/RSyncDir.cs
+++ Duplicati/Library/Main/RSyncDir.cs
@@ -1561,7 +1561,7 @@ namespace Duplicati.Library.Main.RSync
                 }
             }
 
-            if (m_unproccesed.Files.Count == 0 && m_lastPartialFile != null)
+            if (m_unproccesed.Files.Count == 0 && m_lastPartialFile == null)
                 return FinalizeMultiPass(signaturefile, contentfile, volumesize);
             else
                 return false;
