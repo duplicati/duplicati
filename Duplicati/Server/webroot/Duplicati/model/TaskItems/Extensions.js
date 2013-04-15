82485efd990d9207cc133bf6428ff4214340c1eb
Kenneth Skovhede <kenneth@hexad.dk>
2013-04-15 22:06:30 +0200

    Fixed loading of some boolean values

--- Duplicati/Server/webroot/Duplicati/model/TaskItems/Extensions.js
+++ Duplicati/Server/webroot/Duplicati/model/TaskItems/Extensions.js
@@ -25,12 +25,14 @@ Ext.define('Duplicati.model.TaskItems.Extensions', {
         {
             name: 'AsyncTransfer',
             mapping: 'Async Transfer',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'IgnoreTimestamps',
             mapping: 'Ignore Timestamps',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'FileSizeLimit',
@@ -39,67 +41,80 @@ Ext.define('Duplicati.model.TaskItems.Extensions', {
         {
             name: 'DisableAESfallbackencryption',
             mapping: 'Disable AES fallback encryption',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'SelectFilesVersion',
             mapping: 'Select Files - Version',
-            type: 'int'
+            type: 'int',
+            convert: parseInt
         },
         {
             name: 'SelectFilesUseSimpleMode',
             mapping: 'Select Files - Use Simple Mode',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'SelectFilesIncludeDocuments',
             mapping: 'Select Files - Include Documents',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'SelectFilesIncludeDesktop',
             mapping: 'Select Files - Include Desktop',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'SelectFilesIncludeMusic',
             mapping: 'Select Files - Include Music',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'SelectFilesIncludeImages',
             mapping: 'Select Files - Include Images',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'SelectFilesIncludeAppData',
             mapping: 'Select Files - Include AppData',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'SelectWhenWarnedNoSchedule',
             mapping: 'Select When - Warned No Schedule',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'SelectWhenWarnedTooManyIncrementals',
             mapping: 'Select When - Warned Too Many Incrementals',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'SelectWhenWarnedNoIncrementals',
             mapping: 'Select When - Warned No Incrementals',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'PasswordSettingsWarnedNoPassword',
             mapping: 'Password Settings - Warned No Password',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         },
         {
             name: 'CleanupSettingsWarnedNoCleanup',
             mapping: 'Cleanup Settings - Warned No Cleanup',
-            type: 'boolean'
+            type: 'boolean',
+            convert: parseBool
         }
     ]
 });
