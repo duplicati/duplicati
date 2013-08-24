cf474b608b6798c8b7f6675a93be43b58a4dec91
Kenneth Skovhede <kenneth@hexad.dk>
2013-08-24 14:03:44 +0200

    Fixed a string format bug

--- Duplicati/Library/Main/Operation/CreateBugReportHandler.cs
+++ Duplicati/Library/Main/Operation/CreateBugReportHandler.cs
@@ -36,7 +36,7 @@ namespace Duplicati.Library.Main.Operation
 		public void Run()
         {
             if (System.IO.File.Exists(m_targetpath))
-                throw new Exception(string.Format("Output file already exists, not overwriting {0}"));
+                throw new Exception(string.Format("Output file already exists, not overwriting: {0}", m_targetpath));
 				
             if (!System.IO.File.Exists(m_options.Dbpath))
                 throw new Exception(string.Format("Database file does not exist: {0}", m_options.Dbpath));
