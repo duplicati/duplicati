4aef61f3f04e5525e35a584b0e9c3e066b41faed
Kenneth Skovhede <kenneth@hexad.dk>
2013-08-24 14:04:14 +0200

    Removed create-report output from the core, so the clients can phrase the message as they please

--- Duplicati/Library/Main/Operation/CreateBugReportHandler.cs
+++ Duplicati/Library/Main/Operation/CreateBugReportHandler.cs
@@ -48,9 +48,7 @@ namespace Duplicati.Library.Main.Operation
             {
                 m_result.SetDatabase(db);
                 db.Fix();
-            }
-				
-			m_result.AddMessage("Completed! Please examine the log table of the database to see that no filenames are accidentially left over. If you are conserned about privacy, do not attach the database to an issue!!!");
+            }				
 		}
 	}
 }
