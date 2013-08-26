162c04da6c93fbf60046d49060f8d95e17e998e8
Kenneth Skovhede <kenneth@hexad.dk>
2013-08-26 13:12:24 +0200

    Not asking for passphrase caused conflicts, so for now we just ask

--- Duplicati/Library/Modules/Builtin/ConsolePasswordInput.cs
+++ Duplicati/Library/Modules/Builtin/ConsolePasswordInput.cs
@@ -29,7 +29,7 @@ namespace Duplicati.Library.Modules.Builtin
         /// These actions only use the local database and do not require access to the data inside the files.
         /// For List and ListChanges this may not be true if there is no local database
         /// </summary>
-        private readonly static string[] PASSPHRASELESS_ACTIONS = { "Delete", "CreateLogDb", "List", "ListChanges" };
+        private readonly static string[] PASSPHRASELESS_ACTIONS = { "CreateLogDb" };
 
         #region IGenericModule Members
 
