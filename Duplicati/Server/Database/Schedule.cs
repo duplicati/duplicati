251b5984413aaccb19e3494c1f3471bb9fc85e43
Kenneth Skovhede <kenneth@hexad.dk>
2014-03-04 15:38:13 +0100

    Fixed a missing interface declaration

--- Duplicati/Server/Database/Schedule.cs
+++ Duplicati/Server/Database/Schedule.cs
@@ -17,10 +17,11 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 using System;
 using System.Linq;
+using Duplicati.Server.Serialization.Interface;
 
 namespace Duplicati.Server.Database
 {
-    public class Schedule
+    public class Schedule : ISchedule
     {
         public long ID { get; set; }
         public string[] Tags { get; set; }
