eed98e51238fb49e0bfcf3d83965dbaafd127f2b
Kenneth Skovhede <kenneth@hexad.dk>
2014-03-05 16:45:51 +0100

    Reduced polution of the global namespace

--- Duplicati/Server/webroot/scripts/plugins.js
+++ Duplicati/Server/webroot/scripts/plugins.js
@@ -1,40 +1,5 @@
 //TODO: These can be fetched from the server data, but requires some string parsing to get right
 
-PLUGIN_S3_HOSTS = {
-    'Amazon S3': 's3.amazonaws.com',
-    'Hosteurope': 'cs.hosteurope.de',
-    'Dunkel': 'dcs.dunkel.de',
-    'DreamHost': 'objects.dreamhost.com'
-}
-
-//Updated list: http://docs.amazonwebservices.com/general/latest/gr/rande.html#s3_region
-PLUGIN_S3_LOCATIONS = {
-    '(default)': '',
-    'Europe (EU, Ireland)': 'EU',
-    'US East (Northern Virginia)': 'us-east-1',
-    'US West (Northen California)': 'us-west-1',
-    'US West (Oregon)': 'us-west-2',
-    'Asia Pacific (Singapore)': 'ap-southeast-1',
-    'Asia Pacific (Sydney)': 'ap-southeast-2',
-    'Asia Pacific (Tokyo)': 'ap-northeast-1',
-    'South America (Sao Paulo)': 'sa-east-1'
-};
-
-PLUGIN_S3_SERVER_LOCATIONS = {
-    'EU': 's3-eu-west-1.amazonaws.com',
-    'eu-west-1': 's3-eu-west-1.amazonaws.com',
-    'us-east-1': 's3.amazonaws.com',
-    'us-west-1': 's3-us-west-1.amazonaws.com',
-    'us-west-2': 's3-us-west-2.amazonaws.com',
-    'ap-southeast-1': 's3-ap-southeast-1.amazonaws.com',
-    'ap-southeast-2': 's3-ap-southeast-2.amazonaws.com',
-    'ap-northeast-1': 's3-ap-northeast-1.amazonaws.com',
-    'sa-east-1': 's3-sa-east-1.amazonaws.com'
-};
-
-PLUGIN_S3_LINK = 'https://portal.aws.amazon.com/gp/aws/developer/registration/index.html';
-
-
 $(document).ready(function() {
 
     APP_DATA.plugins.backend['file'] = {
@@ -82,6 +47,37 @@ $(document).ready(function() {
 
 
     APP_DATA.plugins.backend['s3'] = {
+        PLUGIN_S3_HOSTS: {
+            'Amazon S3': 's3.amazonaws.com',
+            'Hosteurope': 'cs.hosteurope.de',
+            'Dunkel': 'dcs.dunkel.de',
+            'DreamHost': 'objects.dreamhost.com'
+        },
+        //Updated list: http://docs.amazonwebservices.com/general/latest/gr/rande.html#s3_region
+        PLUGIN_S3_LOCATIONS: {
+            '(default)': '',
+            'Europe (EU, Ireland)': 'EU',
+            'US East (Northern Virginia)': 'us-east-1',
+            'US West (Northen California)': 'us-west-1',
+            'US West (Oregon)': 'us-west-2',
+            'Asia Pacific (Singapore)': 'ap-southeast-1',
+            'Asia Pacific (Sydney)': 'ap-southeast-2',
+            'Asia Pacific (Tokyo)': 'ap-northeast-1',
+            'South America (Sao Paulo)': 'sa-east-1'
+        },
+        PLUGIN_S3_SERVER_LOCATIONS: {
+            'EU': 's3-eu-west-1.amazonaws.com',
+            'eu-west-1': 's3-eu-west-1.amazonaws.com',
+            'us-east-1': 's3.amazonaws.com',
+            'us-west-1': 's3-us-west-1.amazonaws.com',
+            'us-west-2': 's3-us-west-2.amazonaws.com',
+            'ap-southeast-1': 's3-ap-southeast-1.amazonaws.com',
+            'ap-southeast-2': 's3-ap-southeast-2.amazonaws.com',
+            'ap-northeast-1': 's3-ap-northeast-1.amazonaws.com',
+            'sa-east-1': 's3-sa-east-1.amazonaws.com'
+        },
+        PLUGIN_S3_LINK: 'https://portal.aws.amazon.com/gp/aws/developer/registration/index.html',
+
         hasssl: true,
         hideserverandport: true,
         usernamelabel: 'AWS Access ID',
@@ -99,17 +95,17 @@ $(document).ready(function() {
             var bucketfield = EDIT_URI.createFieldset({label: 'S3 Bucket name', name: 's3-bucket', after: $('#server-username-and-password'), title: 'Use / to access subfolders in the bucket', watermark: 'Enter bucket name'});
             var regiondrop = EDIT_URI.createFieldset({label: 'Bucket create region', name: 's3-region', before: $('#server-options-label'), watermark: 'Click for a list of regions', title: 'Note that region is only used when creating buckets'});
             var rrscheck = EDIT_URI.createFieldset({'label': 'Use RRS', name: 's3-rrs', type: 'checkbox', before: $('#server-options-label'), title: 'Reduced Redundancy Storage is cheaper, but less reliable'});
-            var signuplink = EDIT_URI.createFieldset({'label': '&nbsp;', href: PLUGIN_S3_LINK, type: 'link', before: bucketfield.outer, 'title': 'Click here for the sign up page'});
+            var signuplink = EDIT_URI.createFieldset({'label': '&nbsp;', href: this.PLUGIN_S3_LINK, type: 'link', before: bucketfield.outer, 'title': 'Click here for the sign up page'});
 
             signuplink.outer.css('margin-bottom', '10px');
 
             var servers = [];
-            for (var k in PLUGIN_S3_HOSTS)
-                servers.push({label: k + ' (' + PLUGIN_S3_HOSTS[k] + ')', value: PLUGIN_S3_HOSTS[k]});
+            for (var k in this.PLUGIN_S3_HOSTS)
+                servers.push({label: k + ' (' + this.PLUGIN_S3_HOSTS[k] + ')', value: this.PLUGIN_S3_HOSTS[k]});
 
             var buckets = [];
-            for (var k in PLUGIN_S3_LOCATIONS)
-                buckets.push({label: k, value: PLUGIN_S3_LOCATIONS[k]});
+            for (var k in this.PLUGIN_S3_LOCATIONS)
+                buckets.push({label: k, value: this.PLUGIN_S3_LOCATIONS[k]});
 
             regiondrop.field.autocomplete({
                 minLength: 0,
