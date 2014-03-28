820251074219719b015db4a2746cc49e7c965431
Kenneth Skovhede <kenneth@hexad.dk>
2014-03-28 11:37:28 +0100

    Added OEM handling to the RPM build script

--- Installer/fedora/duplicati-make-git-snapshot.sh
+++ Installer/fedora/duplicati-make-git-snapshot.sh
@@ -22,6 +22,14 @@ rm -rf $DIRNAME
 git clone ${REF:+--reference $REF} \
          https://code.google.com/p/duplicati/ $DIRNAME
 
+if [ -e "./oem.js" ]; then
+    cp ./oem.js $DIRNAME/Server/webroot/scripts/
+fi
+
+if [ -e "./oem.css" ]; then
+    cp ./oem.css $DIRNAME/Server/webroot/stylesheets/
+fi
+
 GIT_DIR=$DIRNAME/.git git archive --format=tar --prefix=$DIRNAME/ ${1:-HEAD} \
         | bzip2 > $DIRNAME.tar.bz2
 
