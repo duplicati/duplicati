f0f36083d0f3d778de8cc92be622adb001fe0310
Kenneth Skovhede <kenneth@hexad.dk>
2014-03-28 13:06:28 +0100

    Updated fedora src maker to not rely on GIT_DIR as it does not seem to work as expected

--- Installer/fedora/duplicati-make-git-snapshot.sh
+++ Installer/fedora/duplicati-make-git-snapshot.sh
@@ -22,19 +22,24 @@ rm -rf $DIRNAME
 git clone ${REF:+--reference $REF} \
          https://code.google.com/p/duplicati/ $DIRNAME
 
-if [ -e "./oem.js" ]; then
+cd "$DIRNAME"
+if [ -e "../oem.js" ]; then
     echo "Installing OEM script"
-    cp ./oem.js $DIRNAME/Duplicati/Server/webroot/scripts/
+    cp ../oem.js Duplicati/Server/webroot/scripts/
+    git add Duplicati/Server/webroot/scripts/oem.js
+    git commit -m "Added OEM branding script"
 fi
 
-if [ -e "./oem.css" ]; then
+if [ -e "../oem.css" ]; then
     echo "Installing OEM stylesheet"
-    cp ./oem.css $DIRNAME/Duplicati/Server/webroot/stylesheets/
+    cp ../oem.css Duplicati/Server/webroot/stylesheets/
+    git add Duplicati/Server/webroot/stylesheets/oem.css
+    git commit -m "Added OEM branding stylesheet"
 fi
 
-echo "Building tar ball"
-GIT_DIR=$DIRNAME/.git git archive --format=tar --prefix=$DIRNAME/ ${1:-HEAD} \
-        | bzip2 > $DIRNAME.tar.bz2
+git archive --format=tar --prefix=$DIRNAME/ ${1:-HEAD} \
+        | bzip2 > ../$DIRNAME.tar.bz2
 
+cd ..
 rm -rf $DIRNAME
 
