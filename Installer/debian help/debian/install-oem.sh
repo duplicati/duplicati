54202426ff8f7c20270cb1953b3d59b4c3437694
Kenneth Skovhede <kenneth@hexad.dk>
2014-04-01 00:44:33 +0200

    Added a little progress for debian scripts

--- Installer/debian help/debian/install-oem.sh
+++ Installer/debian help/debian/install-oem.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ -e ../oem.js ]; then
+    echo "Installing OEM script"
     cp ../oem.js build/usr/lib/duplicati/webroot/scripts/
 fi
 
 if [ -e ../oem.css ]; then
+    echo "Installing OEM stylesheet"
     cp ../oem.css build/usr/lib/duplicati/webroot/stylesheets/
 fi
