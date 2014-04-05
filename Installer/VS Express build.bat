a9af679ab0021aca23f0722e8ede713459cf999c
Kenneth Skovhede <kenneth@hexad.dk>
2014-04-05 15:36:53 +0200

    Cleaned up version of the VS Express tool

--- Installer/VS Express build.bat
+++ Installer/VS Express build.bat
@@ -1,118 +1,76 @@
 @echo off
-SET PARAFFIN_EXE=%CD%\paraffin.exe
 SET ORIGIN_PATH=%CD%
-
-if exist "%PARAFFIN_EXE%" goto paraffin_found
-echo *****************************************************
-echo Missing Paraffin.exe, download from here: 
-echo http://www.wintellect.com/CS/files/folders/8198/download.aspx
-echo *****************************************************
-pause
-goto end_of_program
-
-REM TODO: Consider rewriting this in Python so it becomes readable :(
-
-REM Should also write a new Paraffin replacement so we can distribute it,
-REM  it should be fairly simple and not support everything Paraffin does
-
-:paraffin_found
 
 rmdir /S /Q "bin\Release"
 
+msbuild /property:Configuration=Release /target:Clean ..\Duplicati.sln
+
+echo "Building support tools ..."
+msbuild /property:Configuration=Release ..\BuildTools\UpdateVersionStamp\UpdateVersionStamp.sln
+msbuild /property:Configuration=Release ..\BuildTools\WixIncludeMake\WixIncludeMake.sln
+msbuild /property:Configuration=Release ..\BuildTools\WixProjBuilder\WixProjBuilder.sln
+
+echo "Updating version number ..."
+..\BuildTools\UpdateVersionStamp\bin\Release\UpdateVersionStamp.exe --sourcefolder=..
+
+if exist .\buildtag.txt copy .\buildtag.txt ..\Duplicati\Library\License\VersionTag.txt
+
+echo "Building main project ..."
+msbuild /property:Configuration=Release ..\Duplicati.sln
+
+echo "Setting up target structure"
 mkdir bin
 cd bin
 mkdir Release
 cd Release
 
-
 mkdir Duplicati
 cd Duplicati
 
-xcopy /I /Y /E ..\..\..\..\Duplicati\GUI\Bin\Release\* .
+mkdir webroot
+
+xcopy /I /Y /E ..\..\..\..\Duplicati\GUI\Duplicati.GUI.TrayIcon\bin\Release\* .
+xcopy /I /Y /E ..\..\..\..\Duplicati\Server\webroot\* .\webroot
 del *.pdb /Q
+del *.mdb /Q
+del *.dll.config /Q
 xcopy /I /Y "..\..\..\linux help\*" .
 del "*.vshost.*" /Q
-xcopy /Y ..\..\..\..\Duplicati\GUI\StartDuplicati.sh .
 mkdir Tools
 xcopy /I /Y /E ..\..\..\..\Tools .\Tools
 
+if exist .\oem.js copy .\oem.js .\webroot\scripts
+if exist .\oem.css copy .\oem.css .\webroot\stylesheets
+if exist ..\oem.js copy ..\oem.js .\webroot\scripts
+if exist ..\oem.css copy ..\oem.css .\webroot\stylesheets
+
 REM Prepare the config file with version overrides
 echo "" > "Duplicati.CommandLine.exe.config"
-echo "" > "Duplicati.exe.config"
-xcopy /Y ..\..\..\AssemblyRedirects.xml "Duplicati.exe.config"
+echo "" > "Duplicati.GUI.TrayIcon.exe.config"
+echo "" > "Duplicati.Server.exe.config"
 xcopy /Y ..\..\..\AssemblyRedirects.xml "Duplicati.CommandLine.exe.config"
+xcopy /Y ..\..\..\AssemblyRedirects.xml "Duplicati.GUI.TrayIcon.exe.config"
+xcopy /Y ..\..\..\AssemblyRedirects.xml "Duplicati.Server.exe.config"
 
 cd ..\..\..
 
-REM Build translations
-cd "..\Duplicati\Localization"
-rmdir /S /Q compiled
-
-LocalizationTool.exe update
-LocalizationTool.exe webupdate
-for %%d in (".\*.csv") do call :reimportcsv %%d
-
-LocalizationTool.exe update
-LocalizationTool.exe build
-for /D %%d in ("compiled\*") do call :langbuild %%d
-
-cd "..\..\Installer"
-
-REM Create incBinFiles.wxs if required
-REM Does NOT set the FILE_DUPLICATI_MAIN_EXE id on the main file, which is required to build the MSI
-if not exist incBinFiles.wxs "%PARAFFIN_EXE%" -dir bin\Release\Duplicati -groupname DUPLICATIBIN -dirref INSTALLLOCATION -ext .pdb -ext .0 -alias bin\Release\Duplicati -norootdirectory -multiple -Win64var "$(var.Win64)" incBinFiles.wxs 
-
-REM Update version
-if exist incBinFiles.PARAFFIN del incBinFiles.PARAFFIN
-"%PARAFFIN_EXE%" -update incBinFiles.wxs
-if exist incBinFiles.PARAFFIN xcopy /I /Y incBinFiles.PARAFFIN incBinFiles.wxs
-if exist incBinFiles.PARAFFIN del incBinFiles.PARAFFIN
-
-REM Copy in translation files
-xcopy /I /Y /E "..\Duplicati\Localization\compiled\*" "bin\Release\Duplicati"
-
-REM Support for linux with old SQLite binaries
-xcopy /I /Y "..\thirdparty\SQLite\Bin\sqlite-3.6.12.so" "bin\Release\Duplicati"
-move sqlite-3.6.12.so libsqlite3.so.0
+echo "Updating WiX list ..."
+..\BuildTools\WixIncludeMake\bin\Release\WixIncludeMake.exe --sourcefolder=bin/Release/Duplicati --fileprefix=bin/Release/Duplicati --componentid=DUPLICATIBIN --ignorefilter="*.dll.config:*.mdb:*.log:*.sqlite:*/tempdir:*/control_dir/:*/SVGIcons/:*/lvm-scripts/:*/OSX Icons/"
 
 REM This dll enables Mono on Windows support
 xcopy /I /Y "..\thirdparty\SQLite\Bin\sqlite3.dll" "bin\Release\Duplicati"
 
-REM Build zip version
+echo "Building zip version ..."
 cd "bin\Release"
 "%PROGRAMFILES%\7-zip\7z.exe" a -r "Duplicati.zip" Duplicati
 cd "..\.."
 
-WixProjBuilder.exe WixInstaller.wixproj
+echo "Building msi version ..."
+..\BuildTools\WixProjBuilder\bin\Release\WixProjBuilder.exe WixInstaller.wixproj
 move "bin\Release\Duplicati.msi" "bin\Release\Duplicati.x86.msi"
-WixProjBuilder.exe --platform=x64 WixInstaller.wixproj
+..\BuildTools\WixProjBuilder\bin\Release\WixProjBuilder.exe --platform=x64 WixInstaller.wixproj
 move "bin\Release\Duplicati.msi" "bin\Release\Duplicati.x64.msi"
 pause
-
-goto end_of_program
-
-:reimportcsv
-set LANG_NAME=
-
-REM Read the second value delimited by . (the filename is .\report.en-US.csv)
-for /f "tokens=2 delims=." %%a in ("%1") do set LANG_NAME=%%a
-LocalizationTool.exe import %LANG_NAME% %1
-
-goto end_of_program
-
-:langbuild
-set FOLDERNAME=
-
-for /f "tokens=*" %%a in ("%1") do set FOLDERNAME=%%~na
-
-SET LANG_WXS_NAME=%ORIGIN_PATH%\incLocFiles.%FOLDERNAME%.wxs
-SET LANG_PRF_NAME=%ORIGIN_PATH%\incLocFiles.%FOLDERNAME%.PARAFFIN
-
-if not exist "%LANG_WXS_NAME%" "%PARAFFIN_EXE%" -dir %1 -alias ..\Duplicati\Localization\%1 -groupname DUPLICATI_LANG_%FOLDERNAME% -dirref INSTALLLOCATION "%LANG_WXS_NAME%" -Win64var "$(var.Win64)" -multiple -ext .pdb -direXclude .svn
-if exist "%LANG_PRF_NAME%" del "%LANG_PRF_NAME%"
-"%PARAFFIN_EXE%" -update "%ORIGIN_PATH%\incLocFiles.%FOLDERNAME%.wxs"
-if exist "%LANG_PRF_NAME%" xcopy /I /Y "%LANG_PRF_NAME%" "%LANG_WXS_NAME%"
-if exist "%LANG_PRF_NAME%" del "%LANG_PRF_NAME%"
 
 goto end_of_program
 
