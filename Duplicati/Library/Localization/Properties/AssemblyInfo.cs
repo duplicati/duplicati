5714a4430a71bc968c5742bd8e63400949faf171
Kenneth Skovhede <kenneth@hexad.dk>
2014-07-01 11:22:27 +0200

    Updated some assembly versions

--- Duplicati/Library/Localization/Properties/AssemblyInfo.cs
+++ Duplicati/Library/Localization/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿//  Copyright (C) 2014, Kenneth Skovhede
+//  Copyright (C) 2014, Kenneth Skovhede
 //  http://www.hexad.dk, opensource@hexad.dk
 //
 //  This library is free software; you can redistribute it and/or modify
@@ -33,7 +33,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.7")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
