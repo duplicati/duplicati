9dfd4f9acbf53bdd5afac235729294ff52880f72
Kenneth Skovhede <kenneth@hexad.dk>
2014-07-01 11:23:11 +0200

    Added a small deploy script

---
+++ deploy-debug.sh
@@ -0,0 +1,10 @@
+echo "2.0_CLI_experimental_`date +%Y-%m-%d`" > Duplicati/License/VersionTag.txt
+mono BuildTools/UpdateVersionStamp/bin/Debug/UpdateVersionStamp.exe
+xbuild /p:Configuration=Debug Duplicati.sln
+rm Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/*.mdb
+rm -rf Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/control_dir
+rm Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/Duplicati-server.sqlite
+rm Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/Duplicati.debug.log
+rm -rf Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/updates
+cp -R Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/* ~/Dropbox/Duplicati/2.0/2.0_snapshot/
+cp -R Duplicati/Server/webroot/ ~/Dropbox/Duplicati/2.0/2.0_snapshot/
