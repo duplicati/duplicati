25c095f177ba7f66ef2a190949d2efb445db5e8a
Kenneth Skovhede <kenneth@hexad.dk>
2014-07-03 11:02:10 +0200

    Updated build script to fit the new locations of key files

--- build-debug-update.sh
+++ build-debug-update.sh
@@ -7,7 +7,6 @@ RELEASE_NAME=2.0_CLI_experimental_${RELEASE_TIMESTAMP}
 RELEASE_CHANGEINFO=`cat Updates/debug_changeinfo.txt`
 RELEASE_VERSION="2.0.0.${RELEASE_INC_VERSION}"
 
-UPDATE_MANIFEST_URLS=http://updates.duplicati.com/debug/latest.manifest\;http://alt.updates.duplicati.com/debug/latest.manifest
 UPDATE_ZIP_URLS=http://updates.duplicati.com/debug/duplicati.zip\;http://alt.updates.duplicati.com/debug/duplicati.zip
 UPDATER_KEYFILE=/Users/kenneth/Dropbox/Privat/Duplicati-updater.key
 
@@ -21,19 +20,17 @@ read -s KEYFILE_PASSWORD
 echo
 
 echo "${RELEASE_NAME}" > Duplicati/License/VersionTag.txt
-echo "${UPDATE_MANIFEST_URLS}" > Duplicati/License/AutoUpdateURL.txt
+cp "Updates/debug_urls.txt"  Duplicati/Library/AutoUpdater/AutoUpdateURL.txt
+cp "Updates/debug_key.txt"  Duplicati/Library/AutoUpdater/AutoUpdateSignKey.txt
 
 mono BuildTools/UpdateVersionStamp/bin/Debug/UpdateVersionStamp.exe --version="${RELEASE_VERSION}"
+xbuild /p:Configuration=Debug BuildTools/AutoUpdateBuilder/AutoUpdateBuilder.sln
 xbuild /p:Configuration=Debug Duplicati.sln
 
-rm Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/*.mdb
-rm -rf Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/control_dir
-rm Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/Duplicati-server.sqlite
-rm Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/Duplicati.debug.log
-rm -rf Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/updates
+if [ ! -d "Updates/build" ]; then mkdir "Updates/build"; fi
 
-UPDATE_SOURCE=Updates/update_source-${RELEASE_TIMESTAMP}
-UPDATE_TARGET=Updates/update_target-${RELEASE_TIMESTAMP}
+UPDATE_SOURCE=Updates/build/update_source-${RELEASE_TIMESTAMP}
+UPDATE_TARGET=Updates/build/update_target-${RELEASE_TIMESTAMP}
 
 if [ -e "${UPDATE_SOURCE}" ]; then rm -rf "${UPDATE_SOURCE}"; fi
 if [ -e "${UPDATE_TARGET}" ]; then rm -rf "${UPDATE_TARGET}"; fi
@@ -41,8 +38,16 @@ if [ -e "${UPDATE_TARGET}" ]; then rm -rf "${UPDATE_TARGET}"; fi
 mkdir "${UPDATE_SOURCE}"
 mkdir "${UPDATE_TARGET}"
 
+# TODO: Figure out how to deal with OSX updates
+#cp -R Duplicati/GUI/Duplicati.GUI.MacTrayIcon/bin/Debug/* "${UPDATE_SOURCE}"
 cp -R Duplicati/GUI/Duplicati.GUI.TrayIcon/bin/Debug/* "${UPDATE_SOURCE}"
 cp -R Duplicati/Server/webroot "${UPDATE_SOURCE}"
+
+rm ${UPDATE_SOURCE}/*.mdb
+if [ -e "${UPDATE_SOURCE}/control_dir" ]; then rm -rf "${UPDATE_SOURCE}/control_dir"; fi
+if [ -e "${UPDATE_SOURCE}/Duplicati-server.sqlite" ]; then rm "${UPDATE_SOURCE}/Duplicati-server.sqlite"; fi
+if [ -e "${UPDATE_SOURCE}/Duplicati.debug.log" ]; then rm "${UPDATE_SOURCE}/Duplicati.debug.log"; fi
+if [ -e "${UPDATE_SOURCE}/updates" ]; then rm -rf "${UPDATE_SOURCE}/updates"; fi
 
 echo
 echo "Building signed package ..."
