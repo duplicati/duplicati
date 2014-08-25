duplicati/duplicati

commit 0198fb8fff2ab74c84eb7aa54a271168c3c3cd64

Fixed issue #1097.
The settings were wiped on startup.

Duplicati/Server/Database/ApplicationSettings.cs
@@ -60,12 +60,8 @@ namespace Duplicati.Server.Database
                 string nx;
 
                 m_values.Clear();
-                var settings = m_connection.GetSettings(Connection.APP_SETTINGS_ID).ToDictionary(x => x.Name, x => x.Value);
-                foreach(var n in typeof(CONST).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.DeclaredOnly | System.Reflection.BindingFlags.Static).Select(x => (string)x.GetValue(null)))
-                {
-                    settings.TryGetValue(n, out nx);
-                    m_values[n] = nx;
-                }
+                foreach(var n in m_connection.GetSettings(Connection.APP_SETTINGS_ID))
+                    m_values[n.Name] =  n.Value;
             }
         }
 

commit b50dac37f339129b64f4a1a683fa4428e5989765

Fixed a crash that was introduced with 0198fb8.

Duplicati/Server/Database/ApplicationSettings.cs
@@ -58,6 +58,8 @@ namespace Duplicati.Server.Database
             lock(m_connection.m_lock)
             {
                 m_values.Clear();
+                foreach(var n in typeof(CONST).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.DeclaredOnly | System.Reflection.BindingFlags.Static).Select(x => (string)x.GetValue(null)))
+                    m_values[n] = null;
                 foreach(var n in m_connection.GetSettings(Connection.APP_SETTINGS_ID))
                     m_values[n.Name] =  n.Value;
             }