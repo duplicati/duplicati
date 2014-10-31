cc4c320ec0bf73728862a26e3557256301d25db4
Kenneth Skovhede <kenneth@hexad.dk>
2014-10-31 18:27:12 +0100

    Fixed a syntax error.

--- Duplicati/Library/Main/Database/Database schema/1. Add index.sql
+++ Duplicati/Library/Main/Database/Database schema/1. Add index.sql
@@ -2,7 +2,7 @@
 CREATE UNIQUE INDEX "FilePath" ON "File" ("Path", "BlocksetID", "MetadataID");
 CREATE UNIQUE INDEX "BlocksetFullHash" ON "Blockset" ("FullHash", "Length");
 CREATE UNIQUE INDEX "BlockHashSize" ON Block ("Hash", "Size");
-CREATE UNIQUE INDEX "RemotevolumeName" ON "Remotevolume" ("Name", "State")
+CREATE UNIQUE INDEX "RemotevolumeName" ON "Remotevolume" ("Name", "State");
 
 CREATE INDEX "BlocksetEntryIds_Forward" ON "BlocksetEntry" ("BlocksetID", "BlockID");
 CREATE INDEX "BlocksetEntryIds_Backwards" ON "BlocksetEntry" ("BlockID", "BlocksetID");
