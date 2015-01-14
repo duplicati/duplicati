be09336a5a03f68e891ddc71a97dfc8b1959a743
Kenneth Skovhede <kenneth@hexad.dk>
2015-01-14 11:45:53 +0100

    Added dinCloud S3 endpoints to known providers

--- Duplicati/Library/Backend/S3/S3Backend.cs
+++ Duplicati/Library/Backend/S3/S3Backend.cs
@@ -39,6 +39,8 @@ namespace Duplicati.Library.Backend
             new KeyValuePair<string, string>("Hosteurope", "cs.hosteurope.de"),
             new KeyValuePair<string, string>("Dunkel", "dcs.dunkel.de"),
             new KeyValuePair<string, string>("DreamHost", "objects.dreamhost.com"),
+            new KeyValuePair<string, string>("dinCloud - Chicago", "d3-ord.dincloud.com"),
+            new KeyValuePair<string, string>("dinCloud - Los Angeles", "d3-lax.dincloud.com"),
         };
 
         //Updated list: http://docs.amazonwebservices.com/general/latest/gr/rande.html#s3_region
