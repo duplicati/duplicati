84049e8962efa46da99741073a5a08d7c3a8cd3a
Kenneth Skovhede <kenneth@hexad.dk>
2015-10-04 17:07:30 +0200

    Added helper to run backups from a link

--- Duplicati/Server/webroot/ngax/scripts/controllers/HomeController.js
+++ Duplicati/Server/webroot/ngax/scripts/controllers/HomeController.js
@@ -1,3 +1,7 @@
-backupApp.controller('HomeController', function ($scope, BackupList) {
+backupApp.controller('HomeController', function ($scope, BackupList, AppService) {
     $scope.backups = BackupList.watch($scope);
+
+    $scope.runBackup = function(id) {
+    	AppService.post('/backup/' + id + '/run');
+    };
 });
