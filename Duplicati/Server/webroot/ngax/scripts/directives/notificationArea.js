e6ef75bd76fea6c35e239e7b8ea9f185795ef00a
Kenneth Skovhede <kenneth@hexad.dk>
2015-12-03 22:04:47 +0100

    Added notifications to the ngax UI

---
+++ Duplicati/Server/webroot/ngax/scripts/directives/notificationArea.js
@@ -0,0 +1,53 @@
+backupApp.directive('notificationArea', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/notificationarea.html',
+    controller: function($scope, $location, $timeout, NotificationService, ServerStatus, AppService, AppUtils) {
+    	$scope.Notifications = NotificationService.watch($scope);
+        $scope.state = ServerStatus.watch($scope);
+
+        $scope.doDismiss = function(id) {
+            AppService.delete('/notification/' + id);
+        };
+
+        $scope.doShowLog = function(backupid) {
+            AppService.get('/backup/' + backupid + '/isactive').then(
+                function() {
+                    $location.path('/log/' + backupid);
+                },
+
+                function(resp) {
+
+                    if (resp.status == 404) {
+                        if ((parseInt(backupid) + '') != backupid)
+                            alert('The backup was temporary and does not exist anymore, so the log data is lost');
+                        else
+                            alert('The backup is missing, has it been delete?');
+                    } else {
+                        AppUtils.connectionError('Failed to find backup: ', resp);
+                    }
+
+                }
+            );
+        };
+
+        $scope.doInstallUpdate = function(id) {
+            AppService.post('/updates/install');
+        };
+
+        $scope.doActivateUpdate = function() {
+            AppService.post('/updates/activate').then(function() {}, AppUtils.connectionError('Activate failed: '));
+        };
+
+        $scope.doShowUpdate = function(id) {
+            $location.path('/updatechangelog'); 
+        };
+
+        $scope.doDownloadBugreport = function(item) {
+            var id = item.Action.substr('bug-report:created:'.length);
+            item.DownloadLink = AppService.get_bugreport_url(id);
+            document.getElementById('notification-download-iframe').src = item.DownloadLink;
+        };
+    }
+  }
+});
