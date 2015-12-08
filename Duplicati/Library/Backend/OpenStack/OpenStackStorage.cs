ba5e1b747a8ec91a8b1b1f55b7a363f3e0f8e8bd
Kenneth Skovhede <kenneth@hexad.dk>
2015-12-08 21:00:59 +0100

    Removed a trailing / from the OVH auth uri

--- Duplicati/Library/Backend/OpenStack/OpenStackStorage.cs
+++ Duplicati/Library/Backend/OpenStack/OpenStackStorage.cs
@@ -56,7 +56,7 @@ namespace Duplicati.Library.Backend.OpenStack
         public static readonly KeyValuePair<string, string>[] KNOWN_OPENSTACK_PROVIDERS = new KeyValuePair<string,string>[] {
             new KeyValuePair<string, string>("Rackspace US", "https://identity.api.rackspacecloud.com/v2.0"),
             new KeyValuePair<string, string>("Rackspace UK", "https://lon.identity.api.rackspacecloud.com/v2.0"),
-            new KeyValuePair<string, string>("OVH Cloud Storage", "https://auth.cloud.ovh.net/v2.0/"),
+            new KeyValuePair<string, string>("OVH Cloud Storage", "https://auth.cloud.ovh.net/v2.0"),
         };
             
         private class OpenStackAuthRequest
