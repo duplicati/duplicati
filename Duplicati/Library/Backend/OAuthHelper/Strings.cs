ba827a31af7da500572731fce0155de087726207
Kenneth Skovhede <kenneth@hexad.dk>
2016-01-12 09:52:58 +0100

    Bugfix for showing the correct error message when the AuthID parameter is missing

--- Duplicati/Library/Backend/OAuthHelper/Strings.cs
+++ Duplicati/Library/Backend/OAuthHelper/Strings.cs
@@ -3,7 +3,7 @@
 namespace Duplicati.Library.Strings
 {
     public static class OAuthHelper {
-        public static string MissingAuthID(string url) { return LC.L(@"You need an AuthID, you can get it from: {0}"); }
+        public static string MissingAuthID(string url) { return LC.L(@"You need an AuthID, you can get it from: {0}", url); }
         public static string AuthorizationFailure(string message, string url) { return LC.L(@"Failed to authorize using the OAuth service: {0}. If the problem persists, try generating a new authid token from: {1}", message, url); }
         public static string UnexpectedError(System.Net.HttpStatusCode statuscode, string description) { return LC.L(@"Unexpected error code: {0} - {1}", statuscode, description); }
         public static string AuthidShort { get { return LC.L(@"The authorization code"); } }
