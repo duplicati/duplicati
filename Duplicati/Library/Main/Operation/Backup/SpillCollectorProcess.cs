0287c10f911b5190f1a505de7b9f30fd526b449c
Kenneth Skovhede <kenneth@hexad.dk>
2016-02-15 09:01:05 +0100

    Wrote the spil collector

--- Duplicati/Library/Main/Operation/Backup/SpillCollectorProcess.cs
+++ Duplicati/Library/Main/Operation/Backup/SpillCollectorProcess.cs
@@ -19,6 +19,8 @@ using CoCoL;
 using System.Threading.Tasks;
 using Duplicati.Library.Main.Operation.Common;
 using System.Collections.Generic;
+using System.Linq;
+using Duplicati.Library.Main.Volumes;
 
 namespace Duplicati.Library.Main.Operation.Backup
 {
@@ -30,7 +32,7 @@ namespace Duplicati.Library.Main.Operation.Backup
     /// </summary>
     internal static class SpillCollectorProcess
     {
-        public static Task Run()
+        public static Task Run(Options options, BackupDatabase database)
         {
             return AutomationExtensions.RunTask(
                 new
@@ -58,13 +60,85 @@ namespace Duplicati.Library.Main.Operation.Backup
 
                     while(lst.Count > 1)
                     {
-                        // TODO: Merge
-                        
+
+                        UploadRequest target = null;
+                        var source = lst[0];
+
+                        // Finalize the current work
+                        source.BlockVolume.Close();
+
+                        // We rebuild the index volume from the database
+                        if (source.IndexVolume != null)
+                            source.IndexVolume.Close();
+
+                        // Remove it from the list of active operations
+                        lst.RemoveAt(0);
+
+                        var buffer = new byte[options.Blocksize];
+
+                        using(var rd = new BlockVolumeReader(options.CompressionModule, source.BlockVolume.LocalFilename, options))
+                        {
+                            foreach(var file in rd.Blocks)
+                            {
+                                // Grab a target
+                                if (target == null)
+                                {
+                                    if (lst.Count == 0)
+                                    {
+                                        // No more targets, make one
+                                        target = new UploadRequest(new BlockVolumeWriter(options), options.IndexfilePolicy == Options.IndexFileStrategy.None ? null : new IndexVolumeWriter(options));
+                                        target.BlockVolume.VolumeID = await database.RegisterRemoteVolumeAsync(target.BlockVolume.RemoteFilename, RemoteVolumeType.Blocks, RemoteVolumeState.Temporary);
+                                        if (target.IndexVolume != null)
+                                            target.IndexVolume = await database.RegisterRemoteVolumeAsync(target.IndexVolume.RemoteFilename, RemoteVolumeType.Index, RemoteVolumeState.Temporary);
+                                    }
+                                    else
+                                    {
+                                        // Grab the next target
+                                        target = lst[0];
+                                        lst.RemoveAt(0);
+                                    }
+                                }
+
+
+                                var len = rd.ReadBlock(file.Key, buffer);
+                                target.BlockVolume.AddBlock(file.Key, buffer, 0, len, Duplicati.Library.Interface.CompressionHint.Default);
+                                await database.MoveBlockToVolumeAsync(file.Key, target.BlockVolume.RemoteFilename);
+
+                                if (target.BlockVolume.Filesize > options.VolumeSize - options.Blocksize)
+                                {
+                                    if (options.IndexfilePolicy == Options.IndexFileStrategy.Full && target.IndexVolume != null && source.IndexVolume != null)
+                                    {
+                                        using(var ixr = DynamicLoader.CompressionLoader.GetModule(options.CompressionModule, source.IndexVolume.LocalFilename, options.RawOptions))
+                                        foreach(var blocklisthash in await database.GetBlocklistHashes(source.BlockVolume.RemoteFilename))
+                                            using(var fs = ixr.OpenRead(blocklisthash))
+                                                target.IndexVolume.WriteBlocklist(blocklisthash, fs);
+                                        // TODO: Do we need to move something for the blocklist db registration?
+
+                                    }
+
+                                    self.Output.WriteAsync(target);
+                                    target = null;
+                                }
+                            }
+                        }
+
+                        // Make sure they are out of the database
+                        System.IO.File.Delete(source.BlockVolume.LocalFilename);
+                        await database.SafeDeleteRemoteVolme(source.BlockVolume.RemoteFilename);
+                        if (source.IndexVolume != null)
+                        {
+                            System.IO.File.Delete(source.IndexVolume.LocalFilename);
+                            await database.SafeDeleteRemoteVolme(source.IndexVolume.RemoteFilename);
+                        }
+
+                        // Re-inject the target if it has content
+                        if (target != null)
+                            lst.Insert(lst.Count == 0 ? 0 : 1);
+
                     }
 
-                    if (lst.Count != 0)
-                        foreach(var n in lst)
-                            await self.Output.WriteAsync(n);
+                    foreach(var n in lst)
+                        await self.Output.WriteAsync(n);
 
                 }
             );
