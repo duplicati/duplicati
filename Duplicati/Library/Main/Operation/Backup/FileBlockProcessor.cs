988746c5f9e29c082a9702f79b8009fc3c02707b
Kenneth Skovhede <kenneth@hexad.dk>
2016-03-30 12:24:34 +0200

    Fixed correct recording of zero-length files

--- Duplicati/Library/Main/Operation/Backup/FileBlockProcessor.cs
+++ Duplicati/Library/Main/Operation/Backup/FileBlockProcessor.cs
@@ -79,10 +79,13 @@ namespace Duplicati.Library.Main.Operation.Backup
                                 var lastread = 0;
                                 var buf = new byte[blocksize];
                                 var lastupdate = DateTime.Now;
+                                var firstBlock = true;
 
                                 // Core processing loop, read blocks of data and hash individually
-                                while((lastread = await fs.ForceStreamReadAsync(buf, blocksize)) != 0)
+                                while(((lastread = await fs.ForceStreamReadAsync(buf, blocksize)) != 0) || firstBlock)
                                 {
+                                    firstBlock = false;
+
                                     // Run file hashing concurrently to squeeze a little extra concurrency out of it
                                     var pftask = Task.Run(() => filehasher.TransformBlock(buf, 0, lastread, buf, 0));
 
