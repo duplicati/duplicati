848dd7c85df25b0cdd08de0e9d5f50bd8c6623bb
Kenneth Skovhede <kenneth@hexad.dk>
2016-04-13 10:54:29 +0200

    Fixed a few issues where reporting the error message would cause an exception that masks the real error

--- Duplicati/Library/Backend/OneDrive/Strings.cs
+++ Duplicati/Library/Backend/OneDrive/Strings.cs
@@ -6,7 +6,7 @@ namespace Duplicati.Library.Backend.Strings
         public static string AutoCreatedFolderLabel { get { return LC.L(@"Autocreated folder"); } }
         public static string UnexpectedError(System.Net.HttpStatusCode statuscode, string description) { return LC.L(@"Unexpected error code: {0} - {1}", statuscode, description); }
         public static string MissingFolderError(string folder) { return LC.L(@"Missing the folder: {0}", folder); }
-        public static string FileNotFoundError(string name) { return LC.L(@"File not found: {0}"); }
+        public static string FileNotFoundError(string name) { return LC.L(@"File not found: {0}", name); }
         public static string DisplayName { get { return LC.L(@"Microsoft OneDrive"); } }
         public static string AuthidShort { get { return LC.L(@"The authorization code"); } }
         public static string AuthidLong(string url) { return LC.L(@"The authorization token retrieved from {0}", url); }
