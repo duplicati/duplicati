53d243aae84c089a6c9b5c479d6181ba727e208d
Kenneth Skovhede <kenneth@hexad.dk>
2016-04-25 13:50:41 +0200

    Commented required lines back in for fedora build script

--- Installer/fedora/build-package.sh
+++ Installer/fedora/build-package.sh
@@ -6,8 +6,8 @@ DATE=`date +%Y%m%d`
 VERSION=`git describe --tags | cut -d '-' -f 1 | cut -d 'v' -f 2`
 GITTAG=`git describe --tags | cut -d '-' -f 6`
 
-#bash duplicati-make-git-snapshot.sh "${GITTAG}" "${DATE}" "${VERSION}"
-#mv duplicati-$DATE.tar.bz2 ~/rpmbuild/SOURCES/ 
+bash duplicati-make-git-snapshot.sh "${GITTAG}" "${DATE}" "${VERSION}"
+mv duplicati-$DATE.tar.bz2 ~/rpmbuild/SOURCES/ 
 cp *.sh ~/rpmbuild/SOURCES/
 cp *.patch ~/rpmbuild/SOURCES/
 
