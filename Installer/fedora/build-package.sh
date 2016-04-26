d048cdc50fa22953371b8412eb4fb17268f00fb4
Kenneth Skovhede <kenneth@hexad.dk>
2016-04-26 11:05:54 +0200

    Updates to rpm scripts

--- Installer/fedora/build-package.sh
+++ Installer/fedora/build-package.sh
@@ -6,8 +6,10 @@ DATE=`date +%Y%m%d`
 VERSION=`git describe --tags | cut -d '-' -f 1 | cut -d 'v' -f 2`
 GITTAG=`git rev-parse --short HEAD`
 RELEASETYPE=`git describe --tags | cut -d '_' -f 2`
+BUILDTAG=`git describe --tags | cut -d '-' -f 2-4`
 
-bash duplicati-make-git-snapshot.sh "${GITTAG}" "${DATE}" "${VERSION}" "${RELEASETYPE}"
+
+bash duplicati-make-git-snapshot.sh "${GITTAG}" "${DATE}" "${VERSION}" "${RELEASETYPE}" "${BUILDTAG}-${GITTAG}"
 mv duplicati-$DATE.tar.bz2 ~/rpmbuild/SOURCES/ 
 cp *.sh ~/rpmbuild/SOURCES/
 cp *.patch ~/rpmbuild/SOURCES/
