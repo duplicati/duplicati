37eaaa59b3eba11af86be19c1fc26cc16f7d908e
Kenneth Skovhede <kenneth@hexad.dk>
2016-05-13 00:05:57 +0200

    Fixed the Synology package

--- Installer/Synology/make-binary-package.sh
+++ Installer/Synology/make-binary-package.sh
@@ -46,15 +46,41 @@ done
 
 cd "${DIRNAME}"
 
+# Remove items unused on the Synology platform
+rm -rf ./win-tools
+rm -rf ./SQLite/win64
+rm -rf ./SQLite/win32
+rm -rf ./MonoMac.dll
+rm -rf ./alphavss
+rm -rf ./OSX\ Icons
+rm -rf ./OSXTrayHost
+rm ./AlphaFS.dll
+rm ./AlphaVSS.Common.dll
+rm -rf ./licenses/alphavss
+rm -rf ./licenses/MonoMac
+rm -rf ./licenses/gpg
+
+# Install extra control items for Synology
+cp -R ../web-extra/* webroot/
+cp ../CGIProxyHandler.exe .
+
+DIRSIZE_KB=`BLOCKSIZE=1024 du -s | cut -d '.' -f 1`
+let "DIRSIZE=DIRSIZE_KB*1024"
+
 tar cvf ../package.tgz ./*
 cd ..
 
 rm -rf "${DIRNAME}"
 
 git checkout INFO
-echo "version=\"${VERSION}\"" >> INFO
+echo "version=\"${VERSION}\"" >> "INFO"
+MD5=`md5 "package.tgz" | awk -F ' ' '{print $NF}'` 
+echo "checksum=\"${MD5}\"" >> "INFO"
+echo "extractsize=\"${DIRSIZE}\"" >> "INFO"
+
+chmod +x scripts/*
 
 tar cf "${BASE_FILE_NAME}.spk" INFO LICENSE *.PNG package.tgz scripts conf WIZARD_UIFILES
-git reset INFO
+git checkout INFO
 
 rm package.tgz
