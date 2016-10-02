b628934ee7cd281b2f54b967316fcc72679ff0d8
agrajaghh <agrajaghh@gmail.com>
2016-10-02 12:32:16 +0200

    angular-gettext wip

---
+++ Duplicati/Server/webroot/ngax/scripts/angular-gettext-cli_compiled_js_output.js
@@ -0,0 +1,5 @@
+angular.module('backupApp').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('de', {"About {0}":"About {0} (DE TRANSLATED)","Show log (not translated)":"Show log (DE TRANSLATED)","You are currently running {0} {1}":"You are currently running {0} {1} (DE TRANSLATED)"});
+/* jshint +W100 */
+}]);
