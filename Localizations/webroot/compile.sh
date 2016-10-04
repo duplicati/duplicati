0ce0a7fcda86b74628c6d54c3f59167c2670a910
agrajaghh <agrajaghh@gmail.com>
2016-10-04 21:10:59 +0200

    add scripts to manage translations

---
+++ Localizations/webroot/compile.sh
@@ -0,0 +1,3 @@
+# angular-gettext-cli in PATH necessary
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+angular-gettext-cli --compile --files "*.po" --dest ../../Duplicati/Server/webroot/ngax/scripts/angular-gettext-cli_compiled_js_output.js --format javascript --module backupApp
--- Localizations/webroot/extract.bat
+++
@@ -1,1 +0,0 @@
-angular-gettext-cli --files "../../Duplicati/Server/webroot/ngax/**/*.+(js|html)" --dest extracted_html_js.pot
