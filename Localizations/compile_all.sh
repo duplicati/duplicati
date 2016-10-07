c9e81b3c30aece825ca4d5983f6678ca427133a1
agrajaghh <agrajaghh@gmail.com>
2016-10-07 10:51:06 +0200

    add more language scripts

---
+++ Localizations/compile_all.sh
@@ -0,0 +1,4 @@
+# gettext and angular-gettext-cli in PATH necessary
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+./duplicati/convert_to_mo.sh
+./webroot/compile.sh
---
+++ Localizations/extract_all.sh
@@ -0,0 +1,4 @@
+# gettext and angular-gettext-cli in PATH necessary
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+./duplicati/extract.sh
+./webroot/extract.sh
