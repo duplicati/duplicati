cf3c567144524eeb7322a386c924f8c45272b2d5
AndersGron <AndersGron@users.noreply.github.com>
2016-10-16 22:07:32 +0200

    Amazon cloud drive consistency delay
    Use delay for Delete, Rename, GetCurrentDirectory and Get. Calculate the
    delay from the completion of the operations instead of the start. I have
    no idea if this is correct or not, but it seems to fix my issues with
    consistency.

--- Duplicati/Library/Backend/AmazonCloudDrive/AmzCD.cs
+++ Duplicati/Library/Backend/AmazonCloudDrive/AmzCD.cs
@@ -85,6 +85,7 @@ namespace Duplicati.Library.Backend.AmazonCloudDrive
                 m_delayTimeSpan = new TimeSpan(0);
             else
                 m_delayTimeSpan = Library.Utility.Timeparser.ParseTimeSpan(delay);
+            m_waitUntil = DateTime.Now + m_delayTimeSpan;
 
             m_oauth = new OAuthHelper(authid, this.ProtocolKey) { AutoAuthHeader = true };
             m_userid = authid.Split(new string[] {":"}, StringSplitOptions.RemoveEmptyEntries).First();
@@ -155,6 +156,10 @@ namespace Duplicati.Library.Backend.AmazonCloudDrive
 
         private ResourceModel GetCurrentDirectory(bool createMissing, string altpath = null)
         {
+            var wait = m_waitUntil - DateTime.Now;
+            if (wait.Ticks > 0)
+                System.Threading.Thread.Sleep(wait);
+
             var rootResponse = m_oauth.GetJSONData<ListResponse>(string.Format("{0}/nodes?filters=isRoot:true", MetadataUrl));
             var parent = rootResponse.Data.First();
             var curpath = new List<string>();
@@ -193,6 +198,7 @@ namespace Duplicati.Library.Backend.AmazonCloudDrive
                                 rs.Write(data, 0, data.Length);
                         }
                     );
+                    m_waitUntil = DateTime.Now + m_delayTimeSpan;
                 }
                 else if (self != null && self.Count > 1)
                     throw new Exception(Strings.AmzCD.MultipleEntries(p, "/" + string.Join("/", curpath)));
@@ -248,8 +254,6 @@ namespace Duplicati.Library.Backend.AmazonCloudDrive
         #region IStreamingBackend implementation
         public void Put(string remotename, System.IO.Stream stream)
         {
-            m_waitUntil = DateTime.Now + m_delayTimeSpan;
-
             var overwrite = FileCache.ContainsKey(remotename);
             var fileid = overwrite ? m_filecache[remotename] : null;
 
@@ -291,9 +295,17 @@ namespace Duplicati.Library.Backend.AmazonCloudDrive
                 m_filecache = null;
                 throw;
             }
+            finally
+            {
+                m_waitUntil = DateTime.Now + m_delayTimeSpan;
+            }
         }
         public void Get(string remotename, System.IO.Stream stream)
         {
+            var wait = m_waitUntil - DateTime.Now;
+            if (wait.Ticks > 0)
+                System.Threading.Thread.Sleep(wait);
+
             using (var resp = m_oauth.GetResponse(string.Format("{0}/nodes/{1}/content", ContentUrl, GetFileID(remotename))))
             using(var rs = Library.Utility.AsyncHttpRequest.TrySetTimeout(resp.GetResponseStream()))
                 Utility.Utility.CopyStream(rs, stream);
@@ -365,10 +377,11 @@ namespace Duplicati.Library.Backend.AmazonCloudDrive
         }
         public void Delete(string remotename)
         {
+            var wait = m_waitUntil - DateTime.Now;
+            if (wait.Ticks > 0)
+                System.Threading.Thread.Sleep(wait);
             try
             {
-                m_waitUntil = DateTime.Now + m_delayTimeSpan;
-
                 using(m_oauth.GetResponse(string.Format("{0}/trash/{1}", MetadataUrl, GetFileID(remotename)), null, "PUT"))
                 {
                 }
@@ -379,6 +392,7 @@ namespace Duplicati.Library.Backend.AmazonCloudDrive
             {
                 m_filecache = null;
             }
+            m_waitUntil = DateTime.Now + m_delayTimeSpan;
         }
         public void Test()
         {
@@ -432,6 +446,10 @@ namespace Duplicati.Library.Backend.AmazonCloudDrive
         #region IRenameEnabledBackend
         public void Rename(string oldname, string newname)
         {
+            var wait = m_waitUntil - DateTime.Now;
+            if (wait.Ticks > 0)
+                System.Threading.Thread.Sleep(wait);
+
             var id = GetFileID(oldname);
 
             var data = System.Text.Encoding.UTF8.GetBytes(
@@ -463,6 +481,10 @@ namespace Duplicati.Library.Backend.AmazonCloudDrive
             {
                 m_filecache = null;
                 throw;
+            }
+            finally
+            {
+                m_waitUntil = DateTime.Now + m_delayTimeSpan;
             }
         }
         #endregion
