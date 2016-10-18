8c48937caa626d0e82398d85465ff83185eb50e8
Kenneth Skovhede <kenneth@hexad.dk>
2016-10-18 15:18:31 +0200

    Typo fix

--- Duplicati/Library/Main/Operation/BackupHandler.cs
+++ Duplicati/Library/Main/Operation/BackupHandler.cs
@@ -109,7 +109,7 @@ namespace Duplicati.Library.Main.Operation
         {
             using(new Logging.Timer("BackupMainOperation"))
             {
-                // Make sure the CompressionHins table is initialized, otherwise all workers will initialize it
+                // Make sure the CompressionHints table is initialized, otherwise all workers will initialize it
                 var tb = options.CompressionHints.Count;
 
                 Task all;
