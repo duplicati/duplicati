1a47e30be8be3f5aed8e41a76766335006916d16
agrajaghh <agrajaghh@gmail.com>
2016-10-26 13:08:33 +0200

    set charset to utf-8 in .pot file
    xgettext isn't setting it sometimes

--- Localizations/duplicati/extract.sh
+++ Localizations/duplicati/extract.sh
@@ -5,4 +5,5 @@ cd $SCRIPT_DIR
 cd ../../Duplicati/
 find . -type f -name *.cs > $SCRIPT_DIR"/"filelist_cs.txt
 xgettext -k --from-code=UTF-8 --output=$SCRIPT_DIR"/"localization.pot --files-from=$SCRIPT_DIR"/"filelist_cs.txt --language=C# --keyword=LC.L
+sed -i "s/charset=CHARSET/charset=UTF-8/g" $SCRIPT_DIR"/"localization.pot
 rm $SCRIPT_DIR"/"filelist_cs.txt
