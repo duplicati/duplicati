1731c341b58cd9af96c84ca7271f396af56830a8
Kenneth Skovhede <kenneth@hexad.dk>
2017-09-01 09:48:56 +0200

    String fix

--- Duplicati/Library/Backend/SSHv2/Strings.cs
+++ Duplicati/Library/Backend/SSHv2/Strings.cs
@@ -32,7 +32,7 @@ namespace Duplicati.Library.Backend.Strings {
         public static string DescriptionSshkeyfileShort { get { return LC.L(@"Uses a SSH private key to authenticate"); } }
         public static string DescriptionSshkeyLong(string urlprefix) { return LC.L(@"An url-encoded SSH private key. The private key must be prefixed with {0}. If the file is encrypted, the password supplied is used to decrypt the keyfile.  If this option is supplied, the password is not used to authenticate. This option only works when using the managed SSH client.", urlprefix); }
         public static string DescriptionSshkeyShort { get { return LC.L(@"Uses a SSH private key to authenticate"); } }
-        public static string DescriptionSshtimeoutLong { get { return LC.L(@"Use this option to manage the internal timeout for SSH operations if operations. If this options is set to zero, the operations will not time out"); } }
+        public static string DescriptionSshtimeoutLong { get { return LC.L(@"Use this option to manage the internal timeout for SSH operations. If this options is set to zero, the operations will not time out"); } }
 		public static string DescriptionSshtimeoutShort { get { return LC.L(@"Sets the operation timeout value"); } }
 		public static string DescriptionSshkeepaliveLong { get { return LC.L(@"This option can be used to enable the keep-alive interval for the SSH connection. If the connection is idle, aggressive firewalls might close the connection. Using keep-alive will keep the connection open in this scenario. If this value is set to zero, the keep-alive is disabled."); } }
 		public static string DescriptionSshkeepaliveShort { get { return LC.L(@"Sets a keepalive value"); } }
