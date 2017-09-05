7d346a1f9a1d0657c15c1ad532f4db41d3767810
Kenneth Skovhede <kenneth@hexad.dk>
2017-09-05 11:41:31 +0200

    Fixed error in Wasabi endpoint url

--- Duplicati/Library/Backend/S3/S3Backend.cs
+++ Duplicati/Library/Backend/S3/S3Backend.cs
@@ -43,7 +43,7 @@ namespace Duplicati.Library.Backend
             new KeyValuePair<string, string>("dinCloud - Chicago", "d3-ord.dincloud.com"),
             new KeyValuePair<string, string>("dinCloud - Los Angeles", "d3-lax.dincloud.com"),
             new KeyValuePair<string, string>("IBM COS (S3) Public US", "s3-api.us-geo.objectstorage.softlayer.net"),
-            new KeyValuePair<string, string>("Wasabi Hot Storage", "s3.wasasbisys.com"),
+            new KeyValuePair<string, string>("Wasabi Hot Storage", "s3.wasabisys.com"),
         };
 
         //Updated list: http://docs.amazonwebservices.com/general/latest/gr/rande.html#s3_region
