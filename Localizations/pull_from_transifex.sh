74498b6b0578dd04eae747a3e1fe4794a9d1388b
Kenneth Skovhede <kenneth@hexad.dk>
2017-10-13 11:48:09 +0200

    Added Czech (cs) translations from Transifex This fixes #2787

--- Localizations/pull_from_transifex.sh
+++ Localizations/pull_from_transifex.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # transifex client in PATH necessary
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-tx pull --language=de,fr,es,zh_CN,nl_NL,pl,fi,ru,da,it,zh_TW
+tx pull --language=de,fr,es,zh_CN,nl_NL,pl,fi,ru,da,it,zh_TW,cs
