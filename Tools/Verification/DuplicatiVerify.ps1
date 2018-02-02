da9014f3638d0e762020bb6a17150865300a02f1
Max <michal@naiman.eu>
2018-02-02 13:39:34 +0100

    fixes

--- Tools/Verification/DuplicatiVerify.ps1
+++ Tools/Verification/DuplicatiVerify.ps1
@@ -1,9 +1,39 @@
+#Requires -Version 5.1
 param
 (
 	[Parameter(Mandatory = $true)]
 	[ValidateNotNullOrEmpty()]
 	[string]$FileOrDir
 )
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+enum RemoteVolumeState
+{
+        # Indicates that the remote volume is being created
+        Temporary
+        # Indicates that the remote volume is being uploaded
+        Uploading
+        # Indicates that the remote volume has been uploaded
+        Uploaded
+        # Indicates that the remote volume has been uploaded, and seen by a list operation
+        Verified
+        # Indicattes that the remote volume should be deleted
+        Deleting
+        # Indicates that the remote volume was successfully deleted from the remote location
+        Deleted
+}
+
+enum RemoteVolumeType
+{
+    # Contains data blocks
+    Blocks
+    # Contains file lists
+    Files
+    # Contains redundant lookup information
+    Index
+}
 
 function HexToBase64
 {
@@ -33,27 +63,37 @@ function Verify-Hashes
 	
     [int]$errorCount = 0
     [int]$checked = 0
-
-    if(-not $(Test-Path $filename)) {
-        Write-Host "Specified file does not exist: $filename"
-        return -1
-    }
-    
     [string]$folder = Split-Path -Path $filename
-    $remoteVolumes = (Get-Content $filename) -Join "`n" | ConvertFrom-Json
+    $remoteVolumes = (Get-Content $filename) -Join "`n" | ConvertFrom-Json | Sort-Object Name
+
+    $statsState = @{}
+    foreach ($n in 0..5) { $statsState.Add($n, $(New-Object PSObject -Property @{SumSize=0;SumCount=0})) }
+    $statsType = @{}
+    foreach ($n in 0..2) { $statsType.Add($n, $(New-Object PSObject -Property @{SumSize=0;SumCount=0})) }
     
     foreach ($remoteVolume in $remoteVolumes) {
         [string]$volFileName = $remoteVolume.Name
-        [long]$volFileSize = $remoteVolume.Size
         [string]$volFilePath = Join-Path -Path $folder -ChildPath $volFileName
 
         if(-not $(Test-Path $volFilePath)) {
-            Write-Host "File missing: $volFileName" -ForegroundColor Red
-            $errorCount++
+            if($remoteVolume.State -eq $([RemoteVolumeState]::Deleted -as [int])) {
+                $checked++
+                $statsState[$($remoteVolume.State)].SumCount += 1
+                $statsState[$($remoteVolume.State)].SumSize += $remoteVolume.Size
+                $statsType[$($remoteVolume.Type)].SumCount += 1
+                $statsType[$($remoteVolume.Type)].SumSize += $remoteVolume.Size
+            } else {
+                Write-Host "File missing: $volFileName" -ForegroundColor Red
+                $errorCount++
+            }
         } else {
-            $checked++
-        
             Write-Host "Verifying file $volFileName"
+
+            $checked++
+            $statsState[$($remoteVolume.State)].SumCount += 1
+            $statsState[$($remoteVolume.State)].SumSize += $remoteVolume.Size
+            $statsType[$($remoteVolume.Type)].SumCount += 1
+            $statsType[$($remoteVolume.Type)].SumSize += $remoteVolume.Size
 
             $shaHash = Get-FileHash -Path $volFilePath -Algorithm SHA256
 
@@ -68,8 +108,7 @@ function Verify-Hashes
     [array]$filesInStorage = Get-ChildItem $folder -Include "$prefix*" -Exclude $(Split-Path -Path $filename -Leaf) | `
         Select-Object -ExpandProperty Name | Sort-Object
     [array]$filesInVerification = $remoteVolumes | Select-Object -ExpandProperty Name | Sort-Object
-
-
+    
     $filesInStorage | Where-Object {$filesInVerification -NotContains $_} | ForEach-Object { 
         Write-Host "Found extra file which is not in verification file: $_" -ForegroundColor Red 
         $errorCount++
@@ -81,28 +120,44 @@ function Verify-Hashes
         Write-Host "`nNo errors found" -ForegroundColor Green
     }
 
+    Write-Host "`nSTATISTICS:"
+
+    foreach ($n in 0..5) {
+        Write-Host "`t$([Enum]::ToObject([RemoteVolumeState], $n)) - Count $($statsState[$n].SumCount), Size $("{0:n1} GB" -f ($statsState[$n].SumSize / 1gb))"
+    }
+    
+    Write-Host 
+
+    foreach ($n in 0..2) {
+        Write-Host "`t$([Enum]::ToObject([RemoteVolumeType], $n)) - Count $($statsType[$n].SumCount), Size $("{0:n1} GB" -f ($statsType[$n].SumSize / 1gb))"
+    }
+
+    Write-Host 
+
     return $errorCount
 }
 
-Set-StrictMode -Version Latest
-
 if(-not $(Test-Path $FileOrDir)) {
     Write-Host "No such file or directory: $FileOrDir" -ForegroundColor Red
+    exit 1
 }
 
 if(Test-Path $FileOrDir -PathType Leaf) {
-    exit Verify-Hashes -filename $FileOrDir
-} else {
-    [int]$files = 0
-    [int]$errorCount = 0
-
-    Get-ChildItem $FileOrDir -Filter "*-verification.json" | 
-    Foreach-Object {
-        $files++
-        Write-Host "Verifying file: $($_.Name)"
-        $errorCount = $errorCount + $(Verify-Hashes -filename $_.FullName)
-    }
-
-    if ($files -eq 0) { Write-Host "No verification files in folder: $FileOrDir" }
-    exit $errorCount
+    exit $(Verify-Hashes -filename $FileOrDir)
 }
+
+$verFiles = @(Get-ChildItem $FileOrDir -Filter "*-verification.json")
+
+if ($verFiles.Count -eq 0) { 
+    Write-Host "No verification files in folder: $FileOrDir"
+    exit 1
+}
+
+[int]$errorCount = 0
+
+$verFiles | % {
+    Write-Host "Verifying file: $($_.Name)"
+    $errorCount += $(Verify-Hashes -filename $_.FullName)
+}
+        
+exit $errorCount
