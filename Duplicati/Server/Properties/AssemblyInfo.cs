3dc964b3db5fee026242c844a769a931b6ae3cca
Catalin Atudori <catalin.atudori@ness.com>
2018-03-05 14:19:49 +0200

    Rename TrustBackup agent executables

--- Duplicati/Server/Properties/AssemblyInfo.cs
+++ Duplicati/Server/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Duplicati.Server")]
+[assembly: AssemblyTitle("TrustBackup.Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Duplicati Team")]
