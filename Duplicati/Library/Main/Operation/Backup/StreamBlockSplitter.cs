30c9c35398ba48697ceb0a4604ff239c0cad586a
Kenneth Skovhede <kenneth@hexad.dk>
2018-06-16 23:01:34 +0200

    No longer sending progress reports when handling metadata

--- Duplicati/Library/Main/Operation/Backup/StreamBlockSplitter.cs
+++ Duplicati/Library/Main/Operation/Backup/StreamBlockSplitter.cs
@@ -100,8 +100,12 @@ namespace Duplicati.Library.Main.Operation.Backup
                                 fslen = stream.Length;
                             }
 
-                            await self.ProgressChannel.WriteAsync(new ProgressEvent() { Filepath = e.Path, Length = fslen, Type = EventType.FileStarted });
-                            send_close = true;
+                            // Don't send progress reports for metadata
+                            if (!e.IsMetadata)
+                            {
+                                await self.ProgressChannel.WriteAsync(new ProgressEvent() { Filepath = e.Path, Length = fslen, Type = EventType.FileStarted });
+                                send_close = true;
+                            }
 
                             filehasher.Initialize();
                             var lastread = 0;
@@ -134,7 +138,7 @@ namespace Duplicati.Library.Main.Operation.Backup
                                 filesize += lastread;
 
                                 // Don't spam updates
-                                if ((DateTime.Now - lastupdate).TotalSeconds > 10)
+                                if (send_close && (DateTime.Now - lastupdate).TotalSeconds > 5)
                                 {
                                     await self.ProgressChannel.WriteAsync(new ProgressEvent() { Filepath = e.Path, Length = filesize, Type = EventType.FileProgressUpdate });
                                     lastupdate = DateTime.Now;
