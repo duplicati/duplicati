3fc88878c14cfc3b060699c82b46db6464510f32
Kenneth Skovhede <kenneth@hexad.dk>
2018-06-28 11:00:35 +0200

    Added some flexibility to the upgrade process to avoid unittest failures caused by repeated database upgrades.

--- Duplicati/Library/SQLiteHelper/DatabaseUpgrader.cs
+++ Duplicati/Library/SQLiteHelper/DatabaseUpgrader.cs
@@ -246,9 +246,20 @@ namespace Duplicati.Library.SQLiteHelper
                 }
                 else if (versions.Count > dbversion)
                 {
-                    string backupfile = System.IO.Path.Combine(
-                        System.IO.Path.GetDirectoryName(sourcefile), 
-                        Strings.DatabaseUpgrader.BackupFilenamePrefix + " " + DateTime.Now.ToString("yyyyMMddhhmmss", System.Globalization.CultureInfo.InvariantCulture) + ".sqlite");
+                    // In some cases (mostly test setups)
+                    // the database upgrades can happen within a second of each other
+                    // causing the upgrades to fail. This scheme adds up to 15 seconds
+                    // delay, making room for multiple rapid upgrade calls
+                    var backupfile = string.Empty;
+                    for (var i = 0; i < 10; i++)
+                    {
+                        backupfile = System.IO.Path.Combine(
+                            System.IO.Path.GetDirectoryName(sourcefile),
+                            Strings.DatabaseUpgrader.BackupFilenamePrefix + " " + (DateTime.Now + TimeSpan.FromSeconds(i * 1.5)).ToString("yyyyMMddhhmmss", System.Globalization.CultureInfo.InvariantCulture) + ".sqlite");
+
+                        if (!System.IO.File.Exists(backupfile))
+                            break;
+                    }
 
                     try
                     {
