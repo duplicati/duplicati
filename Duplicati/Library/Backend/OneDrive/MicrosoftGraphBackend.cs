duplicati/duplicati

commit f9022fec66cf33cb28e039b3046c31a111008493

Avoid referencing virtual members in constructor.
Since the call to GetRootPathFromUrl may be expensive, we cache the
value using a lazy initializer.

Duplicati/Library/Backend/OneDrive/MicrosoftGraphBackend.cs
@@ -74,12 +74,14 @@ namespace Duplicati.Library.Backend
 
         private readonly JsonSerializer m_serializer = new JsonSerializer();
         private readonly OAuthHttpClient m_client;
-        private readonly string m_path;
         private readonly int fragmentSize;
         private readonly int fragmentRetryCount;
         private readonly int fragmentRetryDelay; // In milliseconds
 
         private string[] dnsNames = null;
+
+        private Lazy<string> rootPathFromURL;
+        private string m_path => this.rootPathFromURL.Value;
 
         protected MicrosoftGraphBackend() { } // Constructor needed for dynamic loading to find it
 
@@ -121,7 +123,7 @@ namespace Duplicati.Library.Backend
             this.m_client.BaseAddress = new System.Uri(BASE_ADDRESS);
 
             // Extract out the path to the backup root folder from the given URI
-            this.m_path = NormalizeSlashes(this.GetRootPathFromUrl(url));
+            this.rootPathFromURL = new Lazy<string>(() => this.GetRootPathFromUrl(url));
         }
 
         public abstract string ProtocolKey { get; }

commit a567b467dc1bc7cc5de233c9fbff0e8d75b0c8b2

Fix slashes in Microsoft Graph URLs.
This fixes an issue introduced in revision f9022fec ("Avoid referencing
virtual members in constructor").

This addresses issue #3460.

Duplicati/Library/Backend/OneDrive/MicrosoftGraphBackend.cs
@@ -124,7 +124,7 @@ namespace Duplicati.Library.Backend
 
             // Extract out the path to the backup root folder from the given URI.  Since this can be an expensive operation, 
             // we will cache the value using a lazy initializer.
-            this.rootPathFromURL = new Lazy<string>(() => this.GetRootPathFromUrl(url));
+            this.rootPathFromURL = new Lazy<string>(() => MicrosoftGraphBackend.NormalizeSlashes(this.GetRootPathFromUrl(url)));
         }
 
         public abstract string ProtocolKey { get; }