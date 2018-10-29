215334376b25e1a3fb024df231af68ff05a8f92d
verhoek <30193551+verhoek@users.noreply.github.com>
2018-10-29 21:31:49 +0100

    Added unit test for GetPathRoot in case of long paths.

---
+++ Duplicati/UnitTest/IOTests.cs
@@ -0,0 +1,47 @@
+﻿//  Copyright (C) 2018, The Duplicati Team
+//  http://www.duplicati.com, info@duplicati.com
+//
+//  This library is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation; either version 2.1 of the
+//  License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+using System.Text;
+using NUnit.Framework;
+
+using Duplicati.Library.IO;
+using Duplicati.Library.Utility;
+
+namespace Duplicati.UnitTest
+{
+    [Category("IO")]
+    public class IOTests
+    {
+
+        public static string LongPath(string pathRoot)
+        {
+            string subFolder = "test";
+            return pathRoot + new StringBuilder().Insert(0, subFolder + Util.DirectorySeparatorString, 100);
+        }
+
+        [Test]
+        public void TestGetPathRootWithLongPath()
+        {
+            var pathRoot =  Utility.IsClientWindows ? "C:\\" : "/";
+            var root = SystemIO.IO_OS(Utility.IsClientWindows).GetPathRoot(LongPath(pathRoot));
+
+            Assert.AreEqual(pathRoot, root);
+
+            return;
+        }
+    }
+}
