aee08485011d87eb9be9b43a6dcf4b952b41bc96
Kenneth Skovhede <kenneth@hexad.dk>
2018-11-13 11:23:20 +0100

    Added version number to the common project

--- Duplicati/Library/Common/Properties/AssemblyInfo.cs
+++ Duplicati/Library/Common/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿//  Copyright (C) 2018, The Duplicati Team
+//  Copyright (C) 2018, The Duplicati Team
 //  http://www.duplicati.com, info@duplicati.com
 //
 //  This library is free software; you can redistribute it and/or modify
@@ -33,7 +33,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.7")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
