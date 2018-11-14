17170a504e7a84a2542af8f5c36aaacacd3d3455
verhoek <30193551+verhoek@users.noreply.github.com>
2018-11-14 17:12:38 +0100

    Made signing cryptographically optional.

--- BuildTools/AutoUpdateBuilder/Program.cs
+++ BuildTools/AutoUpdateBuilder/Program.cs
@@ -40,28 +40,22 @@ namespace AutoUpdateBuilder
                 return 4;
             }
 
-            if (string.IsNullOrWhiteSpace(keyfile))
-            {
-                Console.WriteLine("Missing keyfile");
-                return 4;
-            }
-
             if (!System.IO.Directory.Exists(inputfolder))
             {
                 Console.WriteLine("Input folder not found");
                 return 4;
             }
 
-            if (string.IsNullOrWhiteSpace(keyfilepassword))
+            if (!string.IsNullOrWhiteSpace(keyfile) && string.IsNullOrWhiteSpace(keyfilepassword))
             {
                 Console.WriteLine("Enter keyfile passphrase: ");
                 keyfilepassword = Console.ReadLine().Trim();
             }
 
-            if (!System.IO.File.Exists(keyfile))
+            if (!string.IsNullOrWhiteSpace(keyfile) && !System.IO.File.Exists(keyfile))
             {
                 Console.WriteLine("Keyfile not found, creating new");
-                var newkey = System.Security.Cryptography.RSACryptoServiceProvider.Create().ToXmlString(true);
+                var newkey = System.Security.Cryptography.RSA.Create().ToXmlString(true);
                 using (var enc = new Duplicati.Library.Encryption.AESEncryption(keyfilepassword, new Dictionary<string, string>()))
                 using (var fs = System.IO.File.OpenWrite(keyfile))
                 using (var ms = new System.IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes(newkey)))
@@ -71,27 +65,29 @@ namespace AutoUpdateBuilder
             if (!System.IO.Directory.Exists(outputfolder))
                 System.IO.Directory.CreateDirectory(outputfolder);
 
-            var privkey = (System.Security.Cryptography.RSACryptoServiceProvider)System.Security.Cryptography.RSACryptoServiceProvider.Create();
+            var privkey = (System.Security.Cryptography.RSACryptoServiceProvider)System.Security.Cryptography.RSA.Create();
 
-            using(var enc = new Duplicati.Library.Encryption.AESEncryption(keyfilepassword, new Dictionary<string, string>()))
-            using(var ms = new System.IO.MemoryStream())
-            using(var fs = System.IO.File.OpenRead(keyfile))
+            if (!string.IsNullOrWhiteSpace(keyfile))
             {
-                enc.Decrypt(fs, ms);
-                ms.Position = 0;
+                using (var enc = new Duplicati.Library.Encryption.AESEncryption(keyfilepassword, new Dictionary<string, string>()))
+                using (var ms = new System.IO.MemoryStream())
+                using (var fs = System.IO.File.OpenRead(keyfile))
+                {
+                    enc.Decrypt(fs, ms);
+                    ms.Position = 0;
 
-                using(var sr = new System.IO.StreamReader(ms))
-                    privkey.FromXmlString(sr.ReadToEnd());
+                    using (var sr = new System.IO.StreamReader(ms))
+                        privkey.FromXmlString(sr.ReadToEnd());
+                }
+
+                if (Duplicati.Library.AutoUpdater.AutoUpdateSettings.SignKey == null || privkey.ToXmlString(false) != Duplicati.Library.AutoUpdater.AutoUpdateSettings.SignKey.ToXmlString(false))
+                {
+                    Console.WriteLine("The public key in the project is not the same as the public key from the file");
+                    Console.WriteLine("Try setting the key to: ");
+                    Console.WriteLine(privkey.ToXmlString(false));
+                    return 5;
+                }
             }
-
-            if (Duplicati.Library.AutoUpdater.AutoUpdateSettings.SignKey == null || privkey.ToXmlString(false) != Duplicati.Library.AutoUpdater.AutoUpdateSettings.SignKey.ToXmlString(false))
-            {
-                Console.WriteLine("The public key in the project is not the same as the public key from the file");
-                Console.WriteLine("Try setting the key to: ");
-                Console.WriteLine(privkey.ToXmlString(false));
-                return 5;
-            }
-
 
 			string gpgkeyid = null;
 			string gpgkeypassphrase = null;
