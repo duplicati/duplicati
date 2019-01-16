3b743f09f1075e3c65ca929f2225273c192bbff8
Kenneth Hsu <kennethhsu@gmail.com>
2019-01-15 19:36:44 -0800

    Remove unused using directives.

--- Duplicati/ConfigurationImporter/Program.cs
+++ Duplicati/ConfigurationImporter/Program.cs
@@ -14,8 +14,6 @@
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-using Duplicati.Library.SQLiteHelper;
-using Duplicati.Server;
 using Duplicati.Server.Serializable;
 using Duplicati.Server.WebServer.RESTMethods;
 using System;
