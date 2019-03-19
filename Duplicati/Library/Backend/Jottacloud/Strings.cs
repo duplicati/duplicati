c46e07b1e5efe76e99766d34ff94545664bdf14f
Joris <joris@talma.nl>
2019-03-19 19:32:00 +0100

    Implemented Jottacloud multithreaded downloading support

--- Duplicati/Library/Backend/Jottacloud/Strings.cs
+++ Duplicati/Library/Backend/Jottacloud/Strings.cs
@@ -17,5 +17,9 @@ namespace Duplicati.Library.Backend.Strings {
         public static string DescriptionDeviceLong(string mountPointOption) { return LC.L(@"The backup device to use. Will be created if not already exists. You can manage your devices from the backup panel in the Jottacloud web interface. When you specify a custom device you should also specify the mount point to use on this device with the ""{0}"" option.", mountPointOption); }
         public static string DescriptionMountPointShort { get { return LC.L(@"Supplies the mount point to use on the server"); } }
         public static string DescriptionMountPointLong(string deviceOptionName) { return LC.L(@"The mount point to use on the server. The default is ""Archive"" for using the built-in archive mount point. Set this option to ""Sync"" to use the built-in synchronization mount point instead, or if you have specified a custom device with option ""{0}"" you are free to name the mount point as you like.", deviceOptionName); }
+        public static string ThreadsShort { get { return LC.L(@"Number of threads for restore operations."); } }
+        public static string ThreadsLong { get { return LC.L(@"Number of threads for restore operations. In some cases the download rate is limited to 18.5 Mbps per stream. Use multiple threads to increase throughput."); } }
+        public static string ChunksizeShort { get { return LC.L(@"The chunk size for simultaneous downloading."); } }
+        public static string ChunksizeLong { get { return LC.L(@"The chunk size for simultaneous downloading. These chunks will be held in memory, so keep it as low as possible."); } }
     }
 }
