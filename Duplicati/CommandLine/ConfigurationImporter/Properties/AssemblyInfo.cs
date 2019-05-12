86e3e97692da100941cc1c89a6456299b9863c0e
Kenneth Skovhede <kenneth@hexad.dk>
2019-05-12 21:46:40 +0200

    Pin version to work better with the build script

--- Duplicati/CommandLine/ConfigurationImporter/Properties/AssemblyInfo.cs
+++ Duplicati/CommandLine/ConfigurationImporter/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyVersion("2.0.0.7")]
 [assembly: AssemblyVersion("2.0.0.7")]
 [assembly: AssemblyFileVersion("2.0.0.7")]
