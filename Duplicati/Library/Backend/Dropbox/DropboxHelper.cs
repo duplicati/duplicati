5716f6c1ee9b7dc1c3ac08ff0bc021323f56b214
Kenneth Hsu <kennethhsu@gmail.com>
2019-05-26 19:08:11 -0700

    Remove unused properties.
    According to the Dropbox API v2 documentation, these are optional.

--- Duplicati/Library/Backend/Dropbox/DropboxHelper.cs
+++ Duplicati/Library/Backend/Dropbox/DropboxHelper.cs
@@ -339,8 +339,6 @@ namespace Duplicati.Library.Backend
         [JsonProperty(".tag")]
         public string tag { get; set; }
         public string name { get; set; }
-        public string path_lower { get; set; }
-        public string path_display { get; set; }
         public string id { get; set; }
 
         public string server_modified { get; set; }
