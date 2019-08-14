7b6ac6b5caf5bc49571012c5e8268873159b6fd9
BlueBlock <danb@pobox.com>
2019-08-14 13:17:30 -0400

    update box backend

--- Duplicati/Library/Backend/Box/BoxBackend.cs
+++ Duplicati/Library/Backend/Box/BoxBackend.cs
@@ -1,24 +1,28 @@
-﻿//  Copyright (C) 2015, The Duplicati Team
-//  http://www.duplicati.com, info@duplicati.com
-//
-//  This library is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as
-//  published by the Free Software Foundation; either version 2.1 of the
-//  License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful, but
-//  WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+﻿#region Disclaimer / License
+// Copyright (C) 2019, The Duplicati Team
+// http://www.duplicati.com, info@duplicati.com
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+#endregion
 using Duplicati.Library.Common.IO;
 using Duplicati.Library.Interface;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading;
@@ -30,7 +34,7 @@ namespace Duplicati.Library.Backend.Box
     // This class is instantiated dynamically in the BackendLoader.
     public class BoxBackend : IBackend, IStreamingBackend
     {
-		private static readonly string LOGTAG = Logging.Log.LogTagFromType<BoxBackend>();
+        private static readonly string LOGTAG = Logging.Log.LogTagFromType<BoxBackend>();
 
         private const string AUTHID_OPTION = "authid";
         private const string REALLY_DELETE_OPTION = "box-delete-from-trash";
@@ -41,12 +45,15 @@ namespace Duplicati.Library.Backend.Box
         private const int PAGE_SIZE = 200;
 
         private readonly BoxHelper m_oauth;
-        private readonly string m_path;
+        private readonly string m_rootPath;
         private readonly bool m_deleteFromTrash;
 
-        private string m_currentfolder;
-        private readonly Dictionary<string, string> m_filecache = new Dictionary<string, string>();
+        private static readonly Dictionary<string, string> m_folderCache = new Dictionary<string, string>();
+        private readonly Dictionary<string, string> m_fileCache = new Dictionary<string, string>();
 
+        private string m_rootFolderId;
+
+        #region BoxHelper
         private class BoxHelper : OAuthHelper
         {
             public BoxHelper(string authid)
@@ -64,13 +71,13 @@ namespace Duplicati.Library.Backend.Box
                     {
                         string rawdata = null;
                         var hs = (ex as WebException).Response as HttpWebResponse;
-                        using(var rs = Library.Utility.AsyncHttpRequest.TrySetTimeout(hs.GetResponseStream()))
-                        using(var sr = new System.IO.StreamReader(rs))
+                        using (var rs = Library.Utility.AsyncHttpRequest.TrySetTimeout(hs.GetResponseStream()))
+                        using (var sr = new System.IO.StreamReader(rs))
                             rawdata = sr.ReadToEnd();
 
                         if (string.IsNullOrWhiteSpace(rawdata))
                             return;
-                        
+
                         newex = new Exception("Raw message: " + rawdata);
 
                         var msg = JsonConvert.DeserializeObject<ErrorResponse>(rawdata);
@@ -81,15 +88,16 @@ namespace Duplicati.Library.Backend.Box
                         */
                     }
                 }
-                catch(Exception ex2)
+                catch (Exception ex2)
                 {
-					Library.Logging.Log.WriteWarningMessage(LOGTAG, "BoxErrorParser", ex2, "Failed to parse error from Box");
+                    Library.Logging.Log.WriteWarningMessage(LOGTAG, "BoxErrorParser", ex2, "Failed to parse error from Box");
                 }
 
                 if (newex != null)
-                    throw newex;                
+                    throw newex;
             }
         }
+        #endregion
 
         // ReSharper disable once UnusedMember.Global
         // This constructor is needed by the BackendLoader.
@@ -103,7 +111,7 @@ namespace Duplicati.Library.Backend.Box
         {
             var uri = new Utility.Uri(url);
 
-            m_path = Util.AppendDirSeparator(uri.HostAndPath, "/");
+            m_rootPath = Util.AppendDirSeparator(uri.HostAndPath, "/");
 
             string authid = null;
             if (options.ContainsKey(AUTHID_OPTION))
@@ -114,31 +122,39 @@ namespace Duplicati.Library.Backend.Box
             m_oauth = new BoxHelper(authid);
         }
 
-        private string CurrentFolder
+        private string GetRootFolderId
         {
             get
             {
-                if (m_currentfolder == null)
-                    GetCurrentFolder(false);
-                
-                return m_currentfolder;
+                if (m_rootFolderId != null) return m_rootFolderId;
+
+                m_rootFolderId = GetFolderId(m_rootPath, false);
+
+                lock (m_folderCache)
+                {
+                    m_folderCache[m_rootPath] = m_rootFolderId;
+                }
+
+                return m_rootFolderId;
             }
         }
 
-        private void GetCurrentFolder(bool create)
+        private string GetFolderId(string path, bool autocreate)
         {
             var parentid = "0";
+            var currentPath = "";
 
-            foreach(var p in m_path.Split(new string[] {"/"}, StringSplitOptions.RemoveEmptyEntries))
+            foreach (var p in path.Split(new string[] { "/" }, StringSplitOptions.RemoveEmptyEntries))
             {
-                var el = (MiniFolder)PagedFileListResponse(parentid, true).FirstOrDefault(x => x.Name == p);
+                currentPath += $"{p}/";
+                var el = (MiniFolder)GetRemoteListResponse(parentid, true).FirstOrDefault(x => x.Name == p);
                 if (el == null)
                 {
-                    if (!create)
+                    if (!autocreate)
                         throw new FolderMissingException();
 
                     el = m_oauth.PostAndGetJSONData<ListFolderResponse>(
-                        string.Format("{0}/folders", BOX_API_URL),
+                        $"{BOX_API_URL}/folders",
                         new CreateItemRequest() { Name = p, Parent = new IDReference() { ID = parentid } }
                     );
                 }
@@ -146,107 +162,181 @@ namespace Duplicati.Library.Backend.Box
                 parentid = el.ID;
             }
 
-            m_currentfolder = parentid;
+            return parentid;
         }
 
         private string GetFileID(string name)
         {
-            if (m_filecache.ContainsKey(name))
-                return m_filecache[name];
+            if (m_fileCache.ContainsKey(name))
+            {
+                return m_fileCache[name];
+            }
 
             // Make sure we enumerate this, otherwise the m_filecache is empty.
-            PagedFileListResponse(CurrentFolder, false).LastOrDefault();
+            GetRemoteList("", GetRootFolderId).Where(x => x.IsFolder == false);
 
-            if (m_filecache.ContainsKey(name))
-                return m_filecache[name];
+            if (m_fileCache.ContainsKey(name)){
+                return m_fileCache[name];}
 
             throw new FileMissingException();
         }
 
-        private IEnumerable<FileEntity> PagedFileListResponse(string parentid, bool onlyfolders)
+        private IEnumerable<RemoteFileEntry> GetRemoteList(string path, string folderId)
         {
-            var offset = 0;
-            var done = false;
+            List<RemoteFileEntry> foundFiles = new List<RemoteFileEntry>();
 
-            if (!onlyfolders)
-                m_filecache.Clear();
-            
-            do
-            {
-                var resp = m_oauth.GetJSONData<ShortListResponse>(string.Format("{0}/folders/{1}/items?limit={2}&offset={3}&fields=name,size,modified_at", BOX_API_URL, parentid, PAGE_SIZE, offset));
-
-                if (resp.Entries == null || resp.Entries.Length == 0)
-                    break;
-
-                foreach(var f in resp.Entries)
+            var allItems = from n in GetRemoteListResponse(folderId, false)
+                select new RemoteFileEntry
                 {
-                    if (onlyfolders && f.Type != "folder")
+                    Name = string.IsNullOrEmpty(path) ? n.Name : $"{path}/{n.Name}",
+                    Size = n.Size,
+                    LastModification = n.ModifiedAt,
+                    IsFolder = n.Type == "folder",
+                    Path = path,
+                    ID = n.ID
+                };
+
+            foreach (var item in allItems)
+            {
+                if (item.IsFolder)
+                {
+                    lock (m_folderCache)
                     {
-                        done = true;
-                        break;
-                    }
-                    else
-                    {
-                        if (!onlyfolders && f.Type == "file")
-                            m_filecache[f.Name] = f.ID;
-                        
-                        yield return f;
+                        m_folderCache[item.Name] = item.ID;
                     }
                 }
+                else
+                {
+                    m_fileCache[item.Name] = item.ID;
+                }
 
-                offset = offset + PAGE_SIZE;
+                foundFiles.Add(item);
+
+                if (item.IsFolder)
+                {
+                    foundFiles.AddRange(GetRemoteList(string.IsNullOrEmpty(path) ? item.Name : $"{path}/{item.Name}", item.ID));
+                }
+            }
+
+            return foundFiles;
+        }
+
+        private IEnumerable<BoxFileEntity> GetRemoteListResponse(string parentid, bool onlyfolders)
+        {
+            var offset = 0;
+
+            do
+            {
+                var resp = m_oauth.GetJSONData<ShortListResponse>($"{BOX_API_URL}/folders/{parentid}/items?limit={PAGE_SIZE}&offset={offset}&fields=name,size,modified_at");
+
+                if (resp.Entries == null || resp.Entries.Length == 0)
+                {
+                    break;
+                }
+
+                foreach (BoxFileEntity f in resp.Entries)
+                {
+                    yield return f;
+                }
+
+                offset += PAGE_SIZE;
 
                 if (offset >= resp.TotalCount)
+                {
                     break;
+                }
 
-            } while(!done);
+            } while (true);
         }
 
         #region IStreamingBackend implementation
 
+        private string NormalizePath(string path)
+        {
+            return path.Replace(@"\", "/");
+        }
+
+        private string GetPath(string remotename)
+        {
+            var path = NormalizePath(remotename).Split(new[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
+            return path.Length > 1 ? NormalizePath(Path.Combine(path.Take(path.Length - 1).ToArray())) : string.Empty;
+        }
+
+        private string GetFilename(string remotename)
+        {
+            return NormalizePath(remotename).Split(new[] { "/" }, StringSplitOptions.RemoveEmptyEntries).Last();
+        }
+
         public async Task PutAsync(string remotename, System.IO.Stream stream, CancellationToken cancelToken)
         {
-            var createreq = new CreateItemRequest() {
-                Name = remotename,
-                Parent = new IDReference() {
-                    ID = CurrentFolder
+            string targetFolderId;
+            string targetFolderPath = GetPath($"{m_rootPath}{remotename}");
+            string targetFilename = GetFilename(remotename);
+
+            if (m_fileCache.Count == 0)
+            {
+                targetFolderId = GetFolderId(targetFolderPath, true);
+                lock (m_folderCache)
+                {
+                    m_folderCache[targetFolderPath] = targetFolderId;
+                }
+            }
+            else
+            {
+                lock (m_folderCache)
+                {
+                    targetFolderId = m_folderCache[targetFolderPath];
+                }
+            }
+
+            var createreq = new CreateItemRequest()
+            {
+                Name = targetFilename,
+                Parent = new IDReference()
+                {
+                    ID = targetFolderId
                 }
             };
-
-            if (m_filecache.Count == 0)
-                PagedFileListResponse(CurrentFolder, false);
-
-            var existing = m_filecache.ContainsKey(remotename);
+            
+            var existing = m_fileCache.ContainsKey(remotename);
 
             try
             {
                 string url;
                 var items = new List<MultipartItem>(2);
 
+                // Figure out if we update or create the file
                 if (existing)
-                    url = $"{BOX_UPLOAD_URL}/{m_filecache[remotename]}/content";
+                {
+                    url = $"{BOX_UPLOAD_URL}/{m_fileCache[remotename]}/content";
+                }
                 else
                 {
                     url = $"{BOX_UPLOAD_URL}/content";
                     items.Add(new MultipartItem(createreq, "attributes"));
                 }
-
-                items.Add(new MultipartItem(stream, "file", remotename));
+                
+                items.Add(new MultipartItem(stream, "file", targetFilename));
 
                 var res = (await m_oauth.PostMultipartAndGetJSONDataAsync<FileList>(url, null, cancelToken, items.ToArray())).Entries.First();
-                m_filecache[remotename] = res.ID;
+                m_fileCache[remotename] = res.ID;
             }
             catch
             {
-                m_filecache.Clear();
+                lock (m_folderCache)
+                {
+                    m_folderCache.Clear();
+                }
+                m_fileCache.Clear();
                 throw;
             }
         }
 
         public void Get(string remotename, System.IO.Stream stream)
         {
-            using (var resp = m_oauth.GetResponse(string.Format("{0}/files/{1}/content", BOX_API_URL, GetFileID(remotename))))
-            using(var rs = Duplicati.Library.Utility.AsyncHttpRequest.TrySetTimeout(resp.GetResponseStream()))
+            var id = GetFileID(remotename);
+            using (var resp = m_oauth.GetResponse($"{BOX_API_URL}/files/{GetFileID(remotename)}/content"))
+            using (var rs = Duplicati.Library.Utility.AsyncHttpRequest.TrySetTimeout(resp.GetResponseStream()))
                 Library.Utility.Utility.CopyStream(rs, stream);
         }
 
@@ -254,11 +344,11 @@ namespace Duplicati.Library.Backend.Box
 
         #region IBackend implementation
 
-        public System.Collections.Generic.IEnumerable<IFileEntry> List()
+        public IEnumerable<IFileEntry> List()
         {
             return
-                from n in PagedFileListResponse(CurrentFolder, false)
-                select (IFileEntry)new FileEntry(n.Name, n.Size, n.ModifiedAt, n.ModifiedAt) { IsFolder = n.Type == "folder" };
+                from n in GetRemoteList("", GetRootFolderId).Where(x => x.IsFolder == false)
+                select new FileEntry { Name = n.Name, Size = n.Size, LastModification = n.LastModification, IsFolder = n.IsFolder };
         }
 
         public Task PutAsync(string remotename, string filename, CancellationToken cancelToken)
@@ -278,18 +368,22 @@ namespace Duplicati.Library.Backend.Box
             var fileid = GetFileID(remotename);
             try
             {
-                using(var r = m_oauth.GetResponse(string.Format("{0}/files/{1}", BOX_API_URL, fileid), null, "DELETE"))
+                using (var r = m_oauth.GetResponse($"{BOX_API_URL}/files/{fileid}", null, "DELETE"))
                 {
                 }
 
                 if (m_deleteFromTrash)
-                    using(var r = m_oauth.GetResponse(string.Format("{0}/files/{1}/trash", BOX_API_URL, fileid), null, "DELETE"))
+                    using (var r = m_oauth.GetResponse($"{BOX_API_URL}/files/{fileid}/trash", null, "DELETE"))
                     {
                     }
             }
             catch
             {
-                m_filecache.Clear();
+                lock (m_folderCache)
+                {
+                    m_folderCache.Clear();
+                }
+                m_fileCache.Clear();
                 throw;
             }
         }
@@ -301,7 +395,7 @@ namespace Duplicati.Library.Backend.Box
 
         public void CreateFolder()
         {
-            GetCurrentFolder(true);
+            GetFolderId(m_rootPath, true);
         }
 
         public string DisplayName
@@ -322,7 +416,8 @@ namespace Duplicati.Library.Backend.Box
 
         public IList<ICommandLineArgument> SupportedCommands
         {
-            get {
+            get
+            {
                 return new List<ICommandLineArgument>(new ICommandLineArgument[] {
                     new CommandLineArgument(AUTHID_OPTION, CommandLineArgument.ArgumentType.Password, Strings.Box.AuthidShort, Strings.Box.AuthidLong(OAuthHelper.OAUTH_LOGIN_URL("box.com"))),
                     new CommandLineArgument(REALLY_DELETE_OPTION, CommandLineArgument.ArgumentType.Boolean, Strings.Box.ReallydeleteShort, Strings.Box.ReallydeleteLong),
@@ -353,6 +448,12 @@ namespace Duplicati.Library.Backend.Box
 
         #endregion
 
+        public class RemoteFileEntry : FileEntry
+        {
+            public string ID { get; set; }
+            public string Path { get; set; }
+        }
+
         private class MiniUser : IDReference
         {
             [JsonProperty("type")]
@@ -375,9 +476,9 @@ namespace Duplicati.Library.Backend.Box
             public string SequenceID { get; set; }
         }
 
-        private class FileEntity : MiniFolder
+        private class BoxFileEntity : MiniFolder
         {
-            public FileEntity() { Size = -1; }
+            public BoxFileEntity() { Size = -1; }
 
             [JsonProperty("sha1")]
             public string SHA1 { get; set; }
@@ -401,7 +502,7 @@ namespace Duplicati.Library.Backend.Box
             [JsonProperty("total_count")]
             public long TotalCount { get; set; }
             [JsonProperty("entries")]
-            public FileEntity[] Entries { get; set; }
+            public BoxFileEntity[] Entries { get; set; }
             [JsonProperty("offset")]
             public long Offset { get; set; }
             [JsonProperty("limit")]
