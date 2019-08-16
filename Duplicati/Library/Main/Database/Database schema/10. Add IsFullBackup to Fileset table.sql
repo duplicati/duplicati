226cd337ff2561fe54757aeffc03118abbd896d7
BlueBlock <danb@pobox.com>
2019-08-16 00:26:52 -0400

    modify db update 10 to conform to structure of others db updates

--- Duplicati/Library/Main/Database/Database schema/10. Add IsFullBackup to Fileset table.sql
+++ Duplicati/Library/Main/Database/Database schema/10. Add IsFullBackup to Fileset table.sql
@@ -1,1 +1,13 @@
-﻿ALTER TABLE "Fileset" ADD COLUMN "IsFullBackup" INTEGER NOT NULL DEFAULT 1;
+﻿CREATE TABLE "Fileset_Temp" (
+	"ID" INTEGER PRIMARY KEY,
+	"OperationID" INTEGER NOT NULL,
+	"VolumeID" INTEGER NOT NULL,
+	"IsFullBackup" INTEGER NOT NULL,
+	"Timestamp" INTEGER NOT NULL
+);
+
+INSERT INTO "Fileset_Temp" SELECT "ID", "OperationID", "VolumeID", 1, "Timestamp" FROM "Fileset";
+DROP TABLE "Fileset";
+ALTER TABLE "Fileset_Temp" RENAME TO "Fileset";
+
+UPDATE "Version" SET "Version" = 10;
