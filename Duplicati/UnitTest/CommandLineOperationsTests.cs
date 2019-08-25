81eb1ba58c56988d17cf8833d757fe1f0106e340
BlueBlock <danb@pobox.com>
2019-08-25 13:43:46 -0400

    update references

--- Duplicati/UnitTest/CommandLineOperationsTests.cs
+++ Duplicati/UnitTest/CommandLineOperationsTests.cs
@@ -20,10 +20,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Duplicati.UnitTest
 {
