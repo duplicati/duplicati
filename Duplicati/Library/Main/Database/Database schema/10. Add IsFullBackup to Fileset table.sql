cc0b0472dc7bafd59c522f6e49bf3294ad19ea41
BlueBlock <danb@pobox.com>
2019-08-28 15:00:02 -0400

    codacy is very particular with sql
    codacy uses sqlint so formatting looks to be inportant or an error is found.
    Format the SQL using sqlint or a tool that uses sqlint such as https://www.onlinegdb.com/online_sqlite_editor and run Beautify.

--- Duplicati/Library/Main/Database/Database schema/10. Add IsFullBackup to Fileset table.sql
+++ Duplicati/Library/Main/Database/Database schema/10. Add IsFullBackup to Fileset table.sql
@@ -1,12 +1,11 @@
-﻿CREATE TABLE "Fileset_Temp" (
-	"ID" INTEGER PRIMARY KEY,
-	"OperationID" INTEGER NOT NULL,
-	"VolumeID" INTEGER NOT NULL,
-	"IsFullBackup" INTEGER NOT NULL,
-	"Timestamp" INTEGER NOT NULL
-);
+﻿CREATE TABLE "Fileset_Temp" ("ID" INTEGER PRIMARY KEY,
+			     "OperationID" INTEGER NOT NULL,
+			     "VolumeID" INTEGER NOT NULL,
+			     "IsFullBackup" INTEGER NOT NULL,
+			     "Timestamp" INTEGER NOT NULL);
 
-INSERT INTO "Fileset_Temp" SELECT "ID", "OperationID", "VolumeID", 1, "Timestamp" FROM "Fileset";
+INSERT INTO "Fileset_Temp" SELECT "ID", "OperationID", "VolumeID", 1,
+  "Timestamp" FROM "Fileset";
 DROP TABLE "Fileset";
 ALTER TABLE "Fileset_Temp" RENAME TO "Fileset";
 
