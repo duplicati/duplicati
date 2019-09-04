310e5d70115401e2eeba9139ed6dab63ab0c55f6
BlueBlock <danb@pobox.com>
2019-09-04 14:20:37 -0400

    revert testing assembly version
    and update copyright

--- Duplicati/GUI/Duplicati.GUI.TrayIcon/Properties/AssemblyInfo.cs
+++ Duplicati/GUI/Duplicati.GUI.TrayIcon/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Duplicati Team")]
 [assembly: AssemblyProduct("Duplicati.GUI.TrayIcon")]
-[assembly: AssemblyCopyright("LGPL, Copyright © Duplicati Team 2015")]
+[assembly: AssemblyCopyright("LGPL, Copyright © Duplicati Team 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("2.0.0.7")]
-[assembly: AssemblyVersion("2.0.4.28")]
-[assembly: AssemblyFileVersion("2.0.4.28")]
+[assembly: AssemblyVersion("2.0.0.7")]
+[assembly: AssemblyFileVersion("2.0.0.7")]
