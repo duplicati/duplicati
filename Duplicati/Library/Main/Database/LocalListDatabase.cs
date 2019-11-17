c395e77bc2058702258e8dcd04580e5097ac260e
Kenneth Hsu <kennethhsu@gmail.com>
2019-11-17 09:35:42 -0800

    Unhide partial filesets from QuickSets property getter.
    This concerns issue #3982.

--- Duplicati/Library/Main/Database/LocalListDatabase.cs
+++ Duplicati/Library/Main/Database/LocalListDatabase.cs
@@ -363,36 +363,14 @@ namespace Duplicati.Library.Main.Database
 
                     using (var cmd = m_connection.CreateCommand())
                     {
-                        using (var rd =
-                            cmd.ExecuteReader(
-                                @"SELECT DISTINCT ""ID"", ""IsFullBackup"" FROM ""Fileset"" ORDER BY ""Timestamp"" DESC ")
-                        )
+                        using (var rd = cmd.ExecuteReader(@"SELECT DISTINCT ""ID"", ""IsFullBackup"" FROM ""Fileset"" ORDER BY ""Timestamp"" DESC "))
                         {
-                            // partial backup sets are only included until the first full backup is encountered
-                            var isFullBackupEncountered = false;
                             while (rd.Read())
                             {
                                 var id = rd.GetInt64(0);
-
-                                if (!dict.ContainsKey(id))
-                                {
-                                    continue;
-                                }
-
                                 var backupType = rd.GetInt32(1);
                                 var e = dict[id];
-
-                                if (isFullBackupEncountered && backupType != BackupType.FULL_BACKUP)
-                                {
-                                    continue;
-                                }
-
                                 yield return new Fileset(e, backupType, m_filesets[e].Value, -1L, -1L);
-
-                                if (!isFullBackupEncountered && backupType == BackupType.FULL_BACKUP)
-                                {
-                                    isFullBackupEncountered = true;
-                                }
                             }
                         }
                     }
