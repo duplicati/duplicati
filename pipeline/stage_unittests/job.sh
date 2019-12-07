bb9bba172298e2e233123ce95bff7e4252d46045
verhoek <30193551+verhoek@users.noreply.github.com>
2019-12-07 22:59:06 +0100

    Remove test data download functionality.
    This is built in unit tests and doesn't need outside steering anymore.

--- pipeline/stage_unittests/job.sh
+++ pipeline/stage_unittests/job.sh
@@ -2,15 +2,6 @@
 . /pipeline/docker-run/markers.sh
 . /pipeline/shared/duplicati.sh
 
-function get_and_extract_test_zip () {
-    travis_mark_begin "DOWNLOADING TEST DATA $CAT"
-    # test if zip file exists and contains no errors, otherwise redownload
-    unzip -t ~/download/"${CAT}"/"${testdata}" &> /dev/null || \
-    wget --progress=dot:giga "https://s3.amazonaws.com/duplicati-test-file-hosting/${testdata}" -O ~/download/"${CAT}"/"${testdata}"
-    unzip -q ~/download/"${CAT}"/"${testdata}" -d ${UNITTEST_BASEFOLDER}
-    travis_mark_end "DOWNLOADING TEST DATA $CAT"
-}
-
 function start_test () {
     travis_mark_begin "SETUP NUGET"
     nuget install NUnit.Runners -Version 3.10.0 -OutputDirectory testrunner
@@ -18,14 +9,6 @@ function start_test () {
 
     for CAT in $(echo $testcategories | sed "s/,/ /g")
     do
-        mkdir -p ~/download/"${CAT}"
-        export UNITTEST_BASEFOLDER=~/duplicati_testdata/"${CAT}"
-        rm -rf ${UNITTEST_BASEFOLDER} && mkdir -p ${UNITTEST_BASEFOLDER}
-
-        if [[ ${testdata} != "" ]]; then
-            get_and_extract_test_zip
-        fi
-
         travis_mark_begin "UNIT TESTING CATEGORY $CAT"
         mono "${DUPLICATI_ROOT}"/testrunner/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe \
         "${DUPLICATI_ROOT}"/Duplicati/UnitTest/bin/Release/Duplicati.UnitTest.dll --where:cat==$CAT --workers=1
--- pipeline/start.sh
+++ pipeline/start.sh
@@ -4,9 +4,9 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 . "${SCRIPT_DIR}/.local_config.sh"
 
 ${ROOT_DIR}/pipeline/jobs/build_job.sh
-${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories BulkNormal --testdata data.zip
-${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories BulkNoSize --testdata data.zip
-${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories SVNDataLong,SVNData --testdata DSMCBE.zip
+${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories BulkNormal
+${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories BulkNoSize
+${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories SVNDataLong,SVNData
 ${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories Border
 ${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories Filter,Targeted,Purge,Serialization,WebApi,Utility,UriUtility,IO,ImportExport,Disruption
 
