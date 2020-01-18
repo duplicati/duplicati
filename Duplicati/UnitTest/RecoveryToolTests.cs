bfbae6fcaf9ca5f65327dc975e586aede0d18745
Kenneth Hsu <kennethhsu@gmail.com>
2020-01-17 17:13:30 -0800

    Add test for restoring files using RecoveryTool.

---
+++ Duplicati/UnitTest/RecoveryToolTests.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Duplicati.Library.Main;
+using NUnit.Framework;
+
+namespace Duplicati.UnitTest
+{
+    [TestFixture]
+    public class RecoveryToolTests : BasicSetupHelper
+    {
+        [Test]
+        [Category("RecoveryTool")]
+        public void Recover()
+        {
+            // Files to create in MB.
+            int[] fileSizes = {10, 20, 30};
+            foreach (int size in fileSizes)
+            {
+                byte[] data = new byte[size * 1024 * 1024];
+                Random rng = new Random();
+                rng.NextBytes(data);
+                File.WriteAllBytes(Path.Combine(this.DATAFOLDER, size + "MB"), data);
+            }
+
+            const string subdirectoryName = "subdirectory";
+            string subdirectoryPath = Path.Combine(this.DATAFOLDER, subdirectoryName);
+            Directory.CreateDirectory(subdirectoryPath);
+            foreach (int size in fileSizes)
+            {
+                byte[] data = new byte[size * 1024 * 1024];
+                Random rng = new Random();
+                rng.NextBytes(data);
+                File.WriteAllBytes(Path.Combine(subdirectoryPath, size + "MB"), data);
+            }
+
+            // Run a backup.
+            Dictionary<string, string> options = new Dictionary<string, string>(this.TestOptions);
+            string backendURL = "file://" + this.TARGETFOLDER;
+            using (Controller c = new Controller(backendURL, options, null))
+            {
+                c.Backup(new[] {this.DATAFOLDER});
+            }
+
+            // Download the backend files.
+            string downloadFolder = Path.Combine(this.RESTOREFOLDER, "downloadedFiles");
+            Directory.CreateDirectory(downloadFolder);
+            int status = CommandLine.RecoveryTool.Program.RealMain(new[] {"download", $"{backendURL}", $"{downloadFolder}", $"--passphrase={options["passphrase"]}"});
+            Assert.AreEqual(0, status);
+
+            // Create the index.
+            status = CommandLine.RecoveryTool.Program.RealMain(new[] {"index", $"{downloadFolder}"});
+            Assert.AreEqual(0, status);
+
+            // Restore to a different folder.
+            string restoreFolder = Path.Combine(this.RESTOREFOLDER, "restoredFiles");
+            Directory.CreateDirectory(restoreFolder);
+            status = CommandLine.RecoveryTool.Program.RealMain(new[] {"restore", $"{downloadFolder}", $"--targetpath={restoreFolder}"});
+            Assert.AreEqual(0, status);
+
+            // Since this.DATAFOLDER is a folder, Path.GetFileName will return the name of the
+            // last folder in the path.
+            string baseFolder = Path.GetFileName(this.DATAFOLDER);
+
+            foreach (string filepath in Directory.EnumerateFiles(this.DATAFOLDER))
+            {
+                string filename = Path.GetFileName(filepath);
+                Assert.IsTrue(TestUtils.CompareFiles(filepath, Path.Combine(restoreFolder, baseFolder, filename ?? String.Empty), filename, false));
+            }
+
+            foreach (string filepath in Directory.EnumerateFiles(subdirectoryPath))
+            {
+                string filename = Path.GetFileName(filepath);
+                Assert.IsTrue(TestUtils.CompareFiles(filepath, Path.Combine(restoreFolder, baseFolder, subdirectoryName, filename ?? String.Empty), filename, false));
+            }
+        }
+    }
+}
--- pipeline/start.sh
+++ pipeline/start.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 ${ROOT_DIR}/pipeline/jobs/build_job.sh
 ${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories BulkNormal
 ${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories BulkNoSize
-${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories SVNDataLong,SVNData
+${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories SVNDataLong,SVNData,RecoveryTool
 ${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories Border
 ${ROOT_DIR}/pipeline/jobs/unittest_job.sh --testcategories Filter,Targeted,Purge,Serialization,WebApi,Utility,UriUtility,IO,ImportExport,Disruption,RestoreHandler
 
