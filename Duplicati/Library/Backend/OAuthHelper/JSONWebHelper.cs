2f1f5bb7e095fd0d5b061790d805da95c6456a8c
Kenneth Skovhede <kenneth@hexad.dk>
2020-05-10 14:14:35 +0200

    The purpose of this update is to provide better error messages from servers that return error messages, but are expected to return JSON data. In the current setup, the response stream is parsed by the JSON decoder, which throws an exception due to reading a non-json formatted error message. The reported exception simply states that the first character on the first line was not valid json, which is true, but does not help diagnose the problem.
    In many cases this is a human readable message, such as "over quota", "permission denied", etc.
    This update adds a stream that keeps the first 2kb of the response in an internal buffer, which can then be printed in case the JSON parsing fails.
    Most of the added code is just simple redirects for calling the base stream in the peeking stream.

--- Duplicati/Library/Backend/OAuthHelper/JSONWebHelper.cs
+++ Duplicati/Library/Backend/OAuthHelper/JSONWebHelper.cs
@@ -289,10 +289,24 @@ namespace Duplicati.Library
 
         public virtual T ReadJSONResponse<T>(HttpWebResponse resp)
         {
-            using(var rs = Duplicati.Library.Utility.AsyncHttpRequest.TrySetTimeout(resp.GetResponseStream()))
-            using(var tr = new System.IO.StreamReader(rs))
-            using(var jr = new Newtonsoft.Json.JsonTextReader(tr))
-                return new Newtonsoft.Json.JsonSerializer().Deserialize<T>(jr);
+            using (var rs = Duplicati.Library.Utility.AsyncHttpRequest.TrySetTimeout(resp.GetResponseStream()))
+            using(var ps = new StreamPeekReader(rs))
+            {
+                try
+                {
+                    using (var tr = new System.IO.StreamReader(ps))
+                    using (var jr = new Newtonsoft.Json.JsonTextReader(tr))
+                        return new Newtonsoft.Json.JsonSerializer().Deserialize<T>(jr);
+                }
+                catch (Exception ex)
+                {
+                    // If we get invalid JSON, report the peek value
+                    if (ex is Newtonsoft.Json.JsonReaderException)
+                        throw new IOException($"Invalid JSON data: \"{ps.PeekData()}\"", ex);
+                    // Otherwise, we have no additional help to offer
+                    throw;
+                }
+            }
         }
 
         /// <summary>
@@ -518,6 +532,90 @@ namespace Duplicati.Library
                 Header = header;
                 Part = part;
             }
+        }
+
+        /// <summary>
+        /// A utility class that shadows the real stream but provides access
+        /// to the first 2kb of the stream to use in error reporting
+        /// </summary>
+        protected class StreamPeekReader : Stream
+        {
+            private readonly Stream m_base;
+            private readonly byte[] m_peekbuffer = new byte[1024 * 2];
+            private int m_peekbytes = 0;
+
+            public StreamPeekReader(Stream source)
+            {
+                m_base = source;
+            }
+
+            public string PeekData()
+            {
+                if (m_peekbuffer.Length == 0)
+                    return string.Empty;
+
+                return Encoding.UTF8.GetString(m_peekbuffer, 0, m_peekbytes);
+            }
+
+            public override bool CanRead => m_base.CanRead;
+            public override bool CanSeek => m_base.CanSeek;
+            public override bool CanWrite => m_base.CanWrite;
+            public override long Length => m_base.Length;
+            public override long Position { get => m_base.Position; set => m_base.Position = value; }
+            public override void Flush() => m_base.Flush();
+            public override long Seek(long offset, SeekOrigin origin) => m_base.Seek(offset, origin);
+            public override void SetLength(long value) => m_base.SetLength(value);
+            public override void Write(byte[] buffer, int offset, int count) => m_base.Write(buffer, offset, count);
+            public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback callback, object state) => m_base.BeginRead(buffer, offset, count, callback, state);
+            public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state) => m_base.BeginWrite(buffer, offset, count, callback, state);
+            public override bool CanTimeout => m_base.CanTimeout;
+            public override void Close() => m_base.Close();
+            public override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken) => m_base.CopyToAsync(destination, bufferSize, cancellationToken);
+            protected override void Dispose(bool disposing) => base.Dispose(disposing);
+            public override int EndRead(IAsyncResult asyncResult) => m_base.EndRead(asyncResult);
+            public override void EndWrite(IAsyncResult asyncResult) => m_base.EndWrite(asyncResult);
+            public override Task FlushAsync(CancellationToken cancellationToken) => m_base.FlushAsync(cancellationToken);
+            public override int ReadTimeout { get => m_base.ReadTimeout; set => m_base.ReadTimeout = value; }
+            public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) => m_base.WriteAsync(buffer, offset, count, cancellationToken);
+            public override int WriteTimeout { get => m_base.WriteTimeout; set => m_base.WriteTimeout = value; }
+
+            public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+            {
+                var br = 0;
+                if (m_peekbytes < m_peekbuffer.Length - 1)
+                {
+                    var maxb = Math.Min(count, m_peekbuffer.Length - m_peekbytes);
+                    br = await m_base.ReadAsync(m_peekbuffer, m_peekbytes, maxb, cancellationToken);
+                    Array.Copy(m_peekbuffer, m_peekbytes, buffer, offset, br);
+                    m_peekbytes += br;
+                    offset += br;
+                    count -= br;
+                    if (count == 0 || br < maxb)
+                        return br;
+                }
+
+                return await m_base.ReadAsync(buffer, offset, count, cancellationToken) + br;
+            }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                var br = 0;
+                if (m_peekbytes < m_peekbuffer.Length - 1)
+                {
+                    var maxb = Math.Min(count, m_peekbuffer.Length - m_peekbytes);
+                    br = m_base.Read(m_peekbuffer, m_peekbytes, maxb);
+                    Array.Copy(m_peekbuffer, m_peekbytes, buffer, offset, br);
+                    m_peekbytes += br;
+                    offset += br;
+                    count -= br;
+
+                    if (count == 0 || br < maxb)
+                        return br;
+                }
+
+                return m_base.Read(buffer, offset, count) + br;
+            }
+
         }
     }
 }
