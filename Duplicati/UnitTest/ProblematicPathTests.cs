7f6411eca98eb3157269849e8c4517db49524d5f
Kenneth Hsu <kennethhsu@gmail.com>
2020-07-18 18:11:37 -0700

    Add test to backup and restore problematic long path.
    This tests a path that exceeds typical limits on Windows.
    
    This concerns issue #3839.
    This concerns pull request #4256.

--- Duplicati/UnitTest/ProblematicPathTests.cs
+++ Duplicati/UnitTest/ProblematicPathTests.cs
@@ -39,5 +39,47 @@ namespace Duplicati.UnitTest
                 }
             }
         }
+
+        [Test]
+        [Category("ProblematicPath")]
+        public void RestoreLongPath()
+        {
+            string folderPath = Path.Combine(this.DATAFOLDER, new string('x', 10));
+            SystemIO.IO_OS.DirectoryCreate(folderPath);
+            using (new DisposablePath(folderPath))
+            {
+                string fileName = new string('y', 255);
+                string filePath = SystemIO.IO_OS.PathCombine(folderPath, fileName);
+                using (new DisposablePath(filePath))
+                {
+                    using (FileStream fileStream = SystemIO.IO_OS.FileOpenWrite(filePath))
+                    {
+                        Utility.CopyStream(new MemoryStream(new byte[] {0, 1, 2}), fileStream);
+                    }
+
+                    Dictionary<string, string> options = new Dictionary<string, string>(this.TestOptions);
+                    using (Controller c = new Controller("file://" + this.TARGETFOLDER, options, null))
+                    {
+                        IBackupResults backupResults = c.Backup(new[] {this.DATAFOLDER});
+                        Assert.AreEqual(0, backupResults.Errors.Count());
+                        Assert.AreEqual(0, backupResults.Warnings.Count());
+                    }
+
+                    string restoreFilePath = SystemIO.IO_OS.PathCombine(this.RESTOREFOLDER, fileName);
+                    using (new DisposablePath(restoreFilePath))
+                    {
+                        Dictionary<string, string> restoreOptions = new Dictionary<string, string>(this.TestOptions) {["restore-path"] = this.RESTOREFOLDER};
+                        using (Controller c = new Controller("file://" + this.TARGETFOLDER, restoreOptions, null))
+                        {
+                            IRestoreResults restoreResults = c.Restore(new[] {filePath});
+                            Assert.AreEqual(0, restoreResults.Errors.Count());
+                            Assert.AreEqual(0, restoreResults.Warnings.Count());
+                        }
+
+                        Assert.IsTrue(SystemIO.IO_OS.FileExists(restoreFilePath));
+                    }
+                }
+            }
+        }
     }
 }
