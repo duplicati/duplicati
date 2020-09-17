5e82ba7d84b53917c944648c6a8d914d142f61eb
Karen Martikyan <karen.martikyan@digitain.com>
2020-09-17 22:43:39 +0400

    Bug fixes made with putting/getting data, list fixed

--- Duplicati/Library/Backend/Telegram/ChannelFileInfo.cs
+++ Duplicati/Library/Backend/Telegram/ChannelFileInfo.cs
@@ -4,7 +4,7 @@ using TeleSharp.TL;
 
 namespace Duplicati.Library.Backend
 {
-    public class ChannelFileInfo
+    public class ChannelFileInfo : IEquatable<ChannelFileInfo>
     {
         public int MessageId { get; set; }
         public long MediaDocAccessHash { get; set; }
@@ -47,6 +47,56 @@ namespace Duplicati.Library.Backend
                 Version = Version,
                 AccessHash = MediaDocAccessHash
             };
+        }
+
+        public bool Equals(ChannelFileInfo other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return MessageId == other.MessageId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((ChannelFileInfo)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return MessageId;
+        }
+
+        public static bool operator ==(ChannelFileInfo left, ChannelFileInfo right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ChannelFileInfo left, ChannelFileInfo right)
+        {
+            return !Equals(left, right);
         }
     }
 }
