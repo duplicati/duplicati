d4f8a079f622cce616519bdd2a1920b16ea5e526
Karen Martikyan <karen.martikyan@digitain.com>
2020-09-20 18:54:24 +0400

    Removed disclaimer: should be added later, but not by me

--- Duplicati/Library/Backend/Telegram/TelegramBackend.cs
+++ Duplicati/Library/Backend/Telegram/TelegramBackend.cs
@@ -1,26 +1,4 @@
-﻿#region Disclaimer / License
-
-// Copyright (C) 2015, The Duplicati Team
-// http://www.duplicati.com, info@duplicati.com
-// 
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-// 
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-// 
-
-#endregion
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -39,8 +17,6 @@ using TLRequestDeleteMessages = TeleSharp.TL.Channels.TLRequestDeleteMessages;
 
 namespace Duplicati.Library.Backend
 {
-    // ReSharper disable once UnusedMember.Global
-    // This class is instantiated dynamically in the BackendLoader.
     public class Telegram : IBackend
     {
         private const int MEBIBYTE_IN_BYTES = 1048576;
