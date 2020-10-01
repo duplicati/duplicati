6732f7435200da30cdc0bcdc76bb05d0916954f1
Dean Ferreyra <dean@octw.com>
2020-09-30 20:23:19 -0700

    Unit tests for "follow" for explicitly selected symlinks
    Add unit tests to confirm that "follow" policy is used
    when symlinks are explicitly selected for backup.

--- Duplicati/UnitTest/SymLinkTests.cs
+++ Duplicati/UnitTest/SymLinkTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Duplicati.Library.Common.IO;
 using Duplicati.Library.Interface;
 using Duplicati.Library.Main;
 using Duplicati.Library.Snapshots;
@@ -85,6 +86,66 @@ namespace Duplicati.UnitTest
                         Assert.Fail($"Unexpected symlink policy");
                         break;
                 }
+            }
+        }
+
+        [Test]
+        [Category("SymLink")]
+        [TestCase(Options.SymlinkStrategy.Store)]
+        [TestCase(Options.SymlinkStrategy.Follow)]
+        [TestCase(Options.SymlinkStrategy.Ignore)]
+        public void SymLinkPolicyOfFollowForExplicitlyChosenSymlinks(Options.SymlinkStrategy symlinkPolicy)
+        {
+            // Create symlink target directory
+            const string targetDirName = "target";
+            var targetDir = systemIO.PathCombine(this.DATAFOLDER, targetDirName);
+            systemIO.DirectoryCreate(targetDir);
+            // Create files in symlink target directory
+            var fileNames = new[] { "a.txt", "b.txt", "c.txt" };
+            foreach (var file in fileNames)
+            {
+                var targetFile = systemIO.PathCombine(targetDir, file);
+                TestUtils.WriteFile(targetFile, Encoding.Default.GetBytes(file));
+            }
+
+            // Create actual symlink directory linking to the target directory
+            const string symlinkDirName = "symlink";
+            var symlinkDir = systemIO.PathCombine(this.DATAFOLDER, symlinkDirName);
+            try
+            {
+                systemIO.CreateSymlink(symlinkDir, targetDir, asDir: true);
+            }
+            catch (Exception e)
+            {
+                // If client cannot create symlinks, mark test as ignored
+                Assert.Ignore($"Client could not create a symbolic link.  Error reported: {e.Message}");
+            }
+
+            // Backup all files with given symlink policy
+            Dictionary<string, string> restoreOptions = new Dictionary<string, string>(this.TestOptions) { ["restore-path"] = this.RESTOREFOLDER };
+            Dictionary<string, string> backupOptions = new Dictionary<string, string>(this.TestOptions) { ["symlink-policy"] = symlinkPolicy.ToString() };
+            using (Controller c = new Controller("file://" + this.TARGETFOLDER, backupOptions, null))
+            {
+                // Select actual symlink directory explicitly.  Regardless
+                // of symlink policy, a policy of "follow" should be used.
+                IBackupResults backupResults = c.Backup(new[] { targetDir, symlinkDir });
+                Assert.AreEqual(0, backupResults.Errors.Count());
+                Assert.AreEqual(0, backupResults.Warnings.Count());
+            }
+            // Restore all files
+            using (Controller c = new Controller("file://" + this.TARGETFOLDER, restoreOptions, null))
+            {
+                IRestoreResults restoreResults = c.Restore(null);
+                Assert.AreEqual(0, restoreResults.Errors.Count());
+                Assert.AreEqual(0, restoreResults.Warnings.Count());
+
+                // Verify that, regardless of the configured symlink policy,
+                // because the symlink was selected explicitly, a symlink
+                // policy of "follow" was used instead
+                var restoreSymlinkDir = systemIO.PathCombine(this.RESTOREFOLDER, symlinkDirName);
+                // Restore should contain a regular directory with copies of the files in the symlink target
+                Assert.That(systemIO.IsSymlink(restoreSymlinkDir), Is.False);
+                TestUtils.AssertDirectoryTreesAreEquivalent(targetDir, restoreSymlinkDir, true, "Restore");
             }
         }
     }
