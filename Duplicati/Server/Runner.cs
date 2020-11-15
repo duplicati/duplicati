1e2e0f876387ab4053b25b8583fc17d4ca179bf9
Kenneth Hsu <kennethhsu@gmail.com>
2020-11-15 15:44:26 -0800

    Improve notification title message to indicate warning or error.

--- Duplicati/Server/Runner.cs
+++ Duplicati/Server/Runner.cs
@@ -796,22 +796,26 @@ namespace Duplicati.Server
                 if (r.FilesWithError > 0 || r.Warnings.Any() || r.Errors.Any())
                 {
                     string message;
+                    string titleType;
                     if (r.FilesWithError > 0)
                     {
                         message = $"Errors affected {r.FilesWithError} file(s).";
+                        titleType = "Error";
                     }
                     else if (r.Errors.Any())
                     {
                         message = r.Errors.Count() == 1 ? r.Errors.Single() : $"Encountered {r.Errors.Count()} errors.";
+                        titleType = "Error";
                     }
                     else
                     {
                         message = r.Warnings.Count() == 1 ? r.Warnings.Single() : $"Encountered {r.Warnings.Count()} warnings.";
+                        titleType = "Warning";
                     }
 
                     Program.DataConnection.RegisterNotification(
                         r.FilesWithError == 0 && !r.Errors.Any() ? NotificationType.Warning : NotificationType.Error,
-                        backup.IsTemporary ? "Warning" : string.Format("Warning while running {0}", backup.Name),
+                        backup.IsTemporary ? "Warning" : $"{titleType} while running {backup.Name}",
                         message,
                         null,
                         backup.ID,
