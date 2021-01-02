53de83e24724ae103216ca005880ba65da6a680d
Игорь Дронов <igor.drono@inbox.ru>
2021-01-02 15:18:18 +0300

    Fixing paths to binaries

--- Duplicati/CommandLine/CLI/Checkout MapGuide Data.bat
+++ Duplicati/CommandLine/CLI/Checkout MapGuide Data.bat
@@ -1,7 +1,7 @@
-"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 100 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\MapGuide\r100
-"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 1000 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\MapGuide\r1000
-"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 2000 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\MapGuide\r2000
-"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 3000 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\MapGuide\r3000
-"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 3100 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\MapGuide\r3100
-"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 3200 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\MapGuide\r3200
+"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 100 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\net471\MapGuide\r100
+"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 1000 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\net471\MapGuide\r1000
+"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 2000 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\net471\MapGuide\r2000
+"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 3000 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\net471\MapGuide\r3000
+"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 3100 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\net471\MapGuide\r3100
+"%ProgramFiles%\CollabNet Subversion\svn" checkout -r 3200 http://svn.osgeo.org/mapguide/trunk/MgDev bin\Debug\net471\MapGuide\r3200
 pause
--- Duplicati/CommandLine/CLI/Run Unittest on MapGuide data.bat
+++ Duplicati/CommandLine/CLI/Run Unittest on MapGuide data.bat
@@ -1,3 +1,3 @@
-cd bin\Debug
+cd bin\Debug\net471
 Duplicati.CommandLine.exe UNITTEST MapGuide\r100 MapGuide\r1000 MapGuide\r2000 MapGuide\r3000 MapGuide\r3100 MapGuide\r3200
 pause
