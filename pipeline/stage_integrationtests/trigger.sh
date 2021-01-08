cda1e2e09a3fff60beac508fbc223221ece77c15
Thomas Suckow <tsuckow@gmail.com>
2021-01-08 08:16:30 -0800

    Remove old build scripts

--- pipeline/stage_integrationtests/trigger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. error_handling.sh
-
-PACKAGES="python3-pip rsync"
-docker-run --image selenium/standalone-firefox \
---packages "$PACKAGES" \
---asroot \
---sharedmem \
---command "sudo -u seluser /pipeline/stage_integrationtests/job.sh" $@
--- pipeline/stage_unittests/trigger.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. error_handling.sh
-
-PACKAGES="wget unzip rsync"
-docker-run --image mono \
---packages "$PACKAGES" \
---command "/pipeline/stage_unittests/job.sh" $@
