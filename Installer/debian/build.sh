aad89b9bdada66b8e5f04c4d050345bbd35ced40
Thomas Suckow <tsuckow@gmail.com>
2021-01-16 16:38:12 -0800

    Try to mitigate newline

--- Installer/debian/build.sh
+++ Installer/debian/build.sh
@@ -5,7 +5,8 @@ SCRIPTDIR=$( cd "$(dirname "$0")" ; pwd -P )
 
 docker build $SCRIPTDIR/docker -t duplicati-debian
 
-export VERSION=`grep '<Version>' < $SCRIPTDIR/../../Duplicati/Server/Duplicati.Server.csproj | sed 's/.*<Version>\(.*\)<\/Version>/\1/'`
+VERSION=`grep '<Version>' < $SCRIPTDIR/../../Duplicati/Server/Duplicati.Server.csproj | sed 's/.*<Version>\(.*\)<\/Version>/\1/'`
+VERSION=${VERSION//$'\n'}
 echo Building version: $VERSION
 
 export MSYS_NO_PATHCONV=1
--- Installer/debian/docker/runner.sh
+++ Installer/debian/docker/runner.sh
@@ -3,6 +3,8 @@ cd /deb
 DATE_STAMP=$(LANG=C date -R)
 sed -e "s;%VERSION%;$VERSION;g" -e "s;%DATE%;$DATE_STAMP;g" -i "debian/changelog"
 
+cat debian/changelog
+
 cp /sources/changelog.txt ./
 
 dpkg-buildpackage -b --no-sign
