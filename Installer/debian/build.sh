46440ebf4012c0baa20bbc061137b9b159ad9174
Thomas Suckow <tsuckow@gmail.com>
2021-01-17 10:13:40 -0800

    More greedy regex

--- Installer/debian/build.sh
+++ Installer/debian/build.sh
@@ -5,7 +5,7 @@ SCRIPTDIR=$( cd "$(dirname "$0")" ; pwd -P )
 
 docker build $SCRIPTDIR/docker -t duplicati-debian
 
-VERSION=`grep '<Version>' < $SCRIPTDIR/../../Duplicati/Server/Duplicati.Server.csproj | sed 's/.*<Version>\(.*\)<\/Version>/\1/'`
+VERSION=`grep '<Version>' < $SCRIPTDIR/../../Duplicati/Server/Duplicati.Server.csproj | sed 's/.*<Version>\(.*\)<\/Version>.*/\1/'`
 VERSION=${VERSION//$'\r\n'}
 echo "Building version: ($VERSION)"
 
