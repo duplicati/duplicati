1e8752a0cddc73ba27859088e3b09a358edde7b5
Kenneth Hsu <kennethhsu@gmail.com>
2021-02-07 13:59:34 -0800

    Increase tolerance for last write time tests on macOS.
    There are occasionally times where the difference is greater than 100ms.

--- Duplicati/UnitTest/TestUtils.cs
+++ Duplicati/UnitTest/TestUtils.cs
@@ -248,7 +248,7 @@ namespace Duplicati.UnitTest
                 // OSX seem to like to actually set the time to some value earlier than what you set by tens of milliseconds.
                 // Reading the time right after it is set gives the expected value but when read later it is slightly different.
                 // Maybe a bug in .net?
-                var granularity = Platform.IsClientOSX ? 100 : 1;
+                int granularity = Platform.IsClientOSX ? 250 : 1;
                 Assert.That(
                     SystemIO.IO_OS.GetLastWriteTimeUtc(actualFile),
                     Is.EqualTo(SystemIO.IO_OS.GetLastWriteTimeUtc(expectedFile)).Within(granularity).Milliseconds,
