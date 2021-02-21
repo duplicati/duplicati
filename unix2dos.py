f84bdaf6f803f800419ecc72e5794456f7bf25dc
Jean-François Paris <jfparis@rouge.eu.org>
2021-02-21 13:52:04 +0200

    Rewrote unix2dos to make it compatible with python2 (now unsupported) and python3

--- unix2dos.py
+++ unix2dos.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 import sys
 
 for fname in sys.argv[1:]:
-    infile = open( fname, "rb" )
-    instr = infile.read()
-    infile.close()
-    outstr = instr.replace( "\r\n", "\n" ).replace( "\r", "\n" ).replace( "\n", "\r\n" )
+    with open(fname, 'rb') as infile:
+        instr = infile.read()
+
+    outstr = instr.replace( b"\r\n", b"\n" ).replace( b"\r", b"\n" ).replace( b"\n", b"\r\n" )
 
     if len(outstr) == len(instr):
         continue
     
-    outfile = open( fname, "wb" )
-    outfile.write( outstr )
-    outfile.close()
+    with open( fname, "wb" ) as outfile:
+        outfile.write( outstr )
