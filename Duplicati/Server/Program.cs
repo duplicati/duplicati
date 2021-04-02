753f61ee1fd0e3aff8186371b2e9fbd5ae2929ea
Thomas Suckow <tsuckow@gmail.com>
2021-04-01 21:03:11 -0700

    Don't stop at the debugger in release builds
    * Also don't show a terminal window for the trayicon

--- Duplicati/Server/Program.cs
+++ Duplicati/Server/Program.cs
@@ -177,7 +177,9 @@ namespace Duplicati.Server
 
         public static int RealMain(string[] _args)
         {
+#if DEBUG
             System.Diagnostics.Debugger.Launch();
+#endif
 
             //If we are on Windows, append the bundled "win-tools" programs to the search path
             //We add it last, to allow the user to override with other versions
