duplicati/duplicati

commit 71c6aca8a787fe09ea0013732e099b6953e1a83a

Fixed IO calls in Filebackend. Moved FileEntry to Library.IO due to AlphaFS not implementing System.IO.FileInfo/DirectoryInfo.

Duplicati/Library/Backend/File/FileBackend.cs
@@ -39,8 +39,9 @@ namespace Duplicati.Library.Backend
         private bool m_hasAutenticated;
         private readonly bool m_forceReauth;
 
-        private readonly byte[] m_copybuffer = new byte[Duplicati.Library.Utility.Utility.DEFAULT_BUFFER_SIZE];
+        private readonly byte[] m_copybuffer = new byte[Utility.Utility.DEFAULT_BUFFER_SIZE];
 
+        private static ISystemIO systemIO = SystemIO.IO_OS(Utility.Utility.IsClientWindows);
 
         public File()
         {
@@ -61,12 +62,14 @@ namespace Duplicati.Library.Backend
                 m_password = uri.Password;
             
             if (!System.IO.Path.IsPathRooted(m_path))
-                m_path = System.IO.Path.GetFullPath(m_path);
+                m_path = systemIO.PathGetFullPath(m_path);
 
             if (options.ContainsKey(OPTION_ALTERNATE_PATHS))
             {
-                List<string> paths = new List<string>();
-                paths.Add(m_path);
+                List<string> paths = new List<string>
+                {
+                    m_path
+                };
                 paths.AddRange(options[OPTION_ALTERNATE_PATHS].Split(new string[] { System.IO.Path.PathSeparator.ToString() }, StringSplitOptions.RemoveEmptyEntries));
 
                 //On windows we expand the drive letter * to all drives
@@ -101,7 +104,7 @@ namespace Duplicati.Library.Backend
                 {
                     try
                     {
-                        if (System.IO.Directory.Exists(p) && (markerfile == null || System.IO.File.Exists(System.IO.Path.Combine(p, markerfile))))
+                        if (systemIO.DirectoryExists(p) && (markerfile == null || systemIO.FileExists(systemIO.PathCombine(p, markerfile))))
                         {
                             m_path = p;
                             break;
@@ -125,13 +128,10 @@ namespace Duplicati.Library.Backend
         {
             try
             {
-                if (!string.IsNullOrEmpty(m_username) && m_password != null)
+                if (!string.IsNullOrEmpty(m_username) && m_password != null && !m_hasAutenticated)
                 {
-                    if (!m_hasAutenticated)
-                    {
-                        Win32.PreAuthenticate(m_path, m_username, m_password, m_forceReauth);
-                        m_hasAutenticated = true;
-                    }
+                    Win32.PreAuthenticate(m_path, m_username, m_password, m_forceReauth);
+                    m_hasAutenticated = true;
                 }
             }
             catch
@@ -142,10 +142,10 @@ namespace Duplicati.Library.Backend
         {
             PreAuthenticate();
 
-            if (!System.IO.Directory.Exists(m_path))
+            if (!systemIO.DirectoryExists(m_path))
                 throw new FolderMissingException(Strings.FileBackend.FolderMissingError(m_path));
 
-            return SystemIO.IO_OS(Utility.Utility.IsClientWindows).PathCombine(m_path, remotename);
+            return systemIO.PathCombine(m_path, remotename);
         }
 
         #region IBackendInterface Members
@@ -169,21 +169,17 @@ namespace Duplicati.Library.Backend
         {
             PreAuthenticate();
 
-            if (!System.IO.Directory.Exists(m_path))
+            if (!systemIO.DirectoryExists(m_path))
                 throw new FolderMissingException(Strings.FileBackend.FolderMissingError(m_path));
 
-            foreach (string s in System.IO.Directory.EnumerateFiles(m_path))
+            foreach (string s in systemIO.EnumerateFileSystemEntries(m_path))
             {
-                System.IO.FileInfo fi = new System.IO.FileInfo(s);
-                yield return new FileEntry(fi.Name, fi.Length, fi.LastAccessTime, fi.LastWriteTime);
+                yield return systemIO.FileEntry(s);
             }
 
-            foreach (string s in System.IO.Directory.EnumerateDirectories(m_path))
+            foreach (string s in systemIO.EnumerateDirectories(m_path))
             {
-                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(s);
-                FileEntry fe = new FileEntry(di.Name, 0, di.LastAccessTime, di.LastWriteTime);
-                fe.IsFolder = true;
-                yield return fe;
+                yield return systemIO.DirectoryEntry(s);
             }
         }
 
@@ -191,7 +187,7 @@ namespace Duplicati.Library.Backend
         private static Random random = new Random();
         public void Put(string remotename, System.IO.Stream stream)
         {
-            using(System.IO.FileStream writestream = System.IO.File.Open(GetRemoteName(remotename), System.IO.FileMode.Create, System.IO.FileAccess.Write, System.IO.FileShare.None))
+            using(System.IO.FileStream writestream = systemIO.FileOpenWrite(GetRemoteName(remotename)))
             {
                 if (random.NextDouble() > 0.6666)
                     throw new Exception("Random upload failure");
@@ -201,14 +197,15 @@ namespace Duplicati.Library.Backend
 #else
         public void Put(string remotename, System.IO.Stream stream)
         {
-            using(System.IO.FileStream writestream = System.IO.File.Open(GetRemoteName(remotename), System.IO.FileMode.Create, System.IO.FileAccess.Write, System.IO.FileShare.None))
+            using(System.IO.FileStream writestream = systemIO.FileOpenWrite(GetRemoteName(remotename)))
                 Utility.Utility.CopyStream(stream, writestream, true, m_copybuffer);
         }
 #endif
 
         public void Get(string remotename, System.IO.Stream stream)
         {
-            using (System.IO.FileStream readstream = System.IO.File.Open(GetRemoteName(remotename), System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read))
+            // FileOpenRead has flags System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read
+            using (System.IO.FileStream readstream = systemIO.FileOpenRead(GetRemoteName(remotename)))
                 Utility.Utility.CopyStream(readstream, stream, true, m_copybuffer);
         }
 
@@ -217,23 +214,23 @@ namespace Duplicati.Library.Backend
             string path = GetRemoteName(remotename);
             if (m_moveFile)
             {
-                if (System.IO.File.Exists(path))
-                    System.IO.File.Delete(path);
+                if (systemIO.FileExists(path))
+                    systemIO.FileDelete(path);
                 
-                System.IO.File.Move(filename, path);
+                systemIO.FileMove(filename, path);
             }
             else
-                System.IO.File.Copy(filename, path, true);
+                systemIO.FileCopy(filename, path, true);
         }
 
         public void Get(string remotename, string filename)
         {
-            System.IO.File.Copy(GetRemoteName(remotename), filename, true);
+            systemIO.FileCopy(GetRemoteName(remotename), filename, true);
         }
 
         public void Delete(string remotename)
         {
-            System.IO.File.Delete(GetRemoteName(remotename));
+            systemIO.FileDelete(GetRemoteName(remotename));
         }
 
         public IList<ICommandLineArgument> SupportedCommands
@@ -267,10 +264,10 @@ namespace Duplicati.Library.Backend
 
         public void CreateFolder()
         {
-            if (System.IO.Directory.Exists(m_path))
+            if (systemIO.DirectoryExists(m_path))
                 throw new FolderAreadyExistedException();
 
-            System.IO.Directory.CreateDirectory(m_path);
+            systemIO.DirectoryCreate(m_path);
         }
 
         #endregion
@@ -279,10 +276,8 @@ namespace Duplicati.Library.Backend
 
         public void Dispose()
         {
-            if (m_username != null)
-                m_username = null;
-            if (m_password != null)
-                m_password = null;
+            m_username = null;
+            m_password = null;
         }
 
         #endregion
@@ -297,7 +292,7 @@ namespace Duplicati.Library.Backend
             string root;
             if (Utility.Utility.IsClientLinux)
             {
-                string path = Util.AppendDirSeparator(System.IO.Path.GetFullPath(m_path));
+                string path = Util.AppendDirSeparator(systemIO.PathGetFullPath(m_path));
                 root = "/";
 
                 //Find longest common prefix from mounted devices
@@ -309,7 +304,7 @@ namespace Duplicati.Library.Backend
             }
             else
             {
-                root = SystemIO.IO_OS(Utility.Utility.IsClientWindows).GetPathRoot(m_path);
+                root = systemIO.GetPathRoot(m_path);
             }
 
             // On Windows, DriveInfo is only valid for lettered drives. (e.g., not for UNC paths and shares)
@@ -338,16 +333,15 @@ namespace Duplicati.Library.Backend
                 {
                     return new QuotaInfo(driveInfo.TotalSize, driveInfo.AvailableFreeSpace);
                 }
-                else if (Utility.Utility.IsClientWindows)
+
+                if (Utility.Utility.IsClientWindows)
                 {
                     // If we can't get the DriveInfo on Windows, fallback to GetFreeDiskSpaceEx
                     // https://stackoverflow.com/questions/2050343/programmatically-determining-space-available-from-unc-path
                     return GetDiskFreeSpace(m_path);
                 }
-                else
-                {
-                    return null;
-                }
+
+                return null;
             }
         }
 
@@ -360,9 +354,9 @@ namespace Duplicati.Library.Backend
         {
             var source = GetRemoteName(oldname);
             var target = GetRemoteName(newname);
-            if (System.IO.File.Exists(target))
-                System.IO.File.Delete(target);
-            System.IO.File.Move(source, target);
+            if (systemIO.FileExists(target))
+                systemIO.FileDelete(target);
+            systemIO.FileMove(source, target);
         }
 
         /// <summary>

Duplicati/Library/Backend/AlternativeFTP/AlternativeFTPBackend.cs
@@ -27,6 +27,7 @@ using System.Security.Authentication;
 using Duplicati.Library.Interface;
 using Uri = System.Uri;
 using CoreUtility = Duplicati.Library.Utility.Utility;
+using Duplicati.Library.IO;
 
 namespace Duplicati.Library.Backend.AlternativeFTP
 {

Duplicati/Library/Backend/AzureBlob/AzureBlobWrapper.cs
@@ -21,6 +21,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Duplicati.Library.IO;
 using Duplicati.Library.Interface;
 using Duplicati.Library.Utility;
 using Microsoft.WindowsAzure.Storage;

Duplicati/Library/Backend/AzureBlob/Duplicati.Library.Backend.AzureBlob.csproj
@@ -79,6 +79,10 @@
       <Project>{B68F2214-951F-4F78-8488-66E1ED3F50BF}</Project>
       <Name>Duplicati.Library.Localization</Name>
     </ProjectReference>
+    <ProjectReference Include="..\..\IO\Duplicati.Library.IO.csproj">
+      <Project>{D63E53E4-A458-4C2F-914D-92F715F58ACF}</Project>
+      <Name>Duplicati.Library.IO</Name>
+    </ProjectReference>
   </ItemGroup>
   <ItemGroup>
     <None Include="Duplicati.snk" />

Duplicati/Library/Backend/CloudFiles/CloudFiles.cs
@@ -23,6 +23,7 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using Duplicati.Library.Interface;
+using Duplicati.Library.IO;
 
 namespace Duplicati.Library.Backend
 {

Duplicati/Library/Backend/CloudFiles/Duplicati.Library.Backend.CloudFiles.csproj
@@ -57,6 +57,10 @@
       <Project>{B68F2214-951F-4F78-8488-66E1ED3F50BF}</Project>
       <Name>Duplicati.Library.Localization</Name>
     </ProjectReference>
+    <ProjectReference Include="..\..\IO\Duplicati.Library.IO.csproj">
+      <Project>{D63E53E4-A458-4C2F-914D-92F715F58ACF}</Project>
+      <Name>Duplicati.Library.IO</Name>
+    </ProjectReference>
   </ItemGroup>
   <ItemGroup>
     <None Include="app.config" />

Duplicati/Library/Backend/Dropbox/Dropbox.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using Duplicati.Library.Interface;
+using Duplicati.Library.IO;
 
 namespace Duplicati.Library.Backend
 {
@@ -49,7 +50,7 @@ namespace Duplicati.Library.Backend
             get { return "dropbox"; }
         }
 
-        private FileEntry ParseEntry(MetaData md)
+        private IFileEntry ParseEntry(MetaData md)
         {
             var ife = new FileEntry(md.name);
             if (md.IsFile)

Duplicati/Library/Backend/Dropbox/Duplicati.Library.Backend.Dropbox.csproj
@@ -61,6 +61,10 @@
       <Project>{B68F2214-951F-4F78-8488-66E1ED3F50BF}</Project>
       <Name>Duplicati.Library.Localization</Name>
     </ProjectReference>
+    <ProjectReference Include="..\..\IO\Duplicati.Library.IO.csproj">
+      <Project>{D63E53E4-A458-4C2F-914D-92F715F58ACF}</Project>
+      <Name>Duplicati.Library.IO</Name>
+    </ProjectReference>
   </ItemGroup>
   <ItemGroup>
     <None Include="packages.config" />

commit c14ace0d68de0b31d8c8cd986a4911384ef3fd3f

Make File backend truncate existing files when overwriting.
The implementations of ISystemIO.FileOpenWrite call File.OpenWrite,
which does not truncate the existing file.  As such, if we overwrite a
larger file with a smaller one, tail remnants of the existing file will
remain.

Since most of the put operations involve unique filenames, this is only
a problem when overwriting existing files (e.g.,
duplicati-verification.json).

This fixes a regression introduced in revision 71c6aca8a7 ("Fixed IO
calls in Filebackend") from pull request #3456.  The previous
implementation called File.Open with FileMode.Create, which truncates
existing files.

This fixes #4486.

Duplicati/Library/Backend/File/FileBackend.cs
@@ -187,7 +187,7 @@ namespace Duplicati.Library.Backend
         private static Random random = new Random();
         public async Task Put(string remotename, System.IO.Stream stream, CancellationToken cancelToken)
         {
-            using(System.IO.FileStream writestream = systemIO.FileOpenWrite(GetRemoteName(remotename)))
+            using(System.IO.FileStream writestream = systemIO.FileCreate(GetRemoteName(remotename)))
             {
                 if (random.NextDouble() > 0.6666)
                     throw new Exception("Random upload failure");
@@ -197,7 +197,7 @@ namespace Duplicati.Library.Backend
 #else
         public async Task PutAsync(string remotename, System.IO.Stream stream, CancellationToken cancelToken)
         {
-            using (System.IO.FileStream writestream = systemIO.FileOpenWrite(GetRemoteName(remotename)))
+            using (System.IO.FileStream writestream = systemIO.FileCreate(GetRemoteName(remotename)))
                 await Utility.Utility.CopyStreamAsync(stream, writestream, true, cancelToken, m_copybuffer);
         }
 #endif