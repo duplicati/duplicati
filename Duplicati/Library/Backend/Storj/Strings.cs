fbf559c1095f4b64851595ed336801a639d78362
TopperDEL <github_com@tparth.de>
2021-05-06 09:06:25 +0200

    fix: Remove unnecessary strings

--- Duplicati/Library/Backend/Storj/Strings.cs
+++ Duplicati/Library/Backend/Storj/Strings.cs
@@ -20,14 +20,11 @@ namespace Duplicati.Library.Backend.Strings
         public static string StorjAPIKeyDescriptionLong { get { return LC.L(@"The API key grants access to a specific project on your chosen satellite. Head over to the dashboard of your satellite to create one if you do not already have an API key."); } }
         public static string StorjSecretDescriptionShort { get { return LC.L(@"The encryption passphrase"); } }
         public static string StorjSecretDescriptionLong { get { return LC.L(@"The encryption passphrase is used to encrypt your data before sending it to the Storj network. This passphrase can be the only secret to provide - for Storj you do not necessary need any additional encryption (from Duplicati) in place."); } }
-        public static string StorjSecretVerifyDescriptionShort { get { return LC.L(@"The encryption passphrase (for verification)"); } }
-        public static string StorjSecretVerifyDescriptionLong { get { return LC.L(@"The encryption passphrase verification to make sure you provided the expected value."); } }
         public static string StorjSharedAccessDescriptionShort { get { return LC.L(@"The access grant"); } }
         public static string StorjSharedAccessDescriptionLong { get { return LC.L(@"An access grant contains all information in one encrypted string. You may use it instead of a satellite, API key and secret."); } }
         public static string StorjBucketDescriptionShort { get { return LC.L(@"The bucket"); } }
         public static string StorjBucketDescriptionLong { get { return LC.L(@"The bucket where the backup will reside in."); } }
         public static string StorjFolderDescriptionShort { get { return LC.L(@"The folder"); } }
         public static string StorjFolderDescriptionLong { get { return LC.L(@"The folder within the bucket where the backup will reside in."); } }
-        public static string StorjEncryptionPassphrasesDoNotMatchError { get { return LC.L(@"The encryption passphrases do not match"); } }
     }
 }
