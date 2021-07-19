a785324102ef43ec430d32b0851afd97ee24dba6
TopperDEL <github_com@tparth.de>
2021-07-19 09:22:05 +0200

    fix: Add "Deprecated" to Tardigrade-Strings

--- Duplicati/Library/Backend/Tardigrade/Strings.cs
+++ Duplicati/Library/Backend/Tardigrade/Strings.cs
@@ -9,7 +9,7 @@ namespace Duplicati.Library.Backend.Strings
 {
     internal static class Tardigrade
     {
-        public static string DisplayName { get { return LC.L(@"Tardigrade Decentralised Cloud Storage"); } }
-        public static string Description { get { return LC.L(@"This backend can read and write data to the Tardigrade Decentralized Cloud Storage. It is deprecated - please move over to the new Storj DCS."); } }
+        public static string DisplayName { get { return LC.L(@"Tardigrade Decentralised Cloud Storage (Deprecated)"); } }
+        public static string Description { get { return LC.L(@"This backend can read and write data to the Tardigrade Decentralized Cloud Storage. It is deprecated - please move over to the new Storj DCS. (Deprecated)"); } }
     }
 }
