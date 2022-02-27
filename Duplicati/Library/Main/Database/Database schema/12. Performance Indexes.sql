d44496f6d4fc7c62d349caf1cf55977de191ad1a
tariandeath <tariandeath210@gmail.com>
2022-02-27 13:29:58 -0700

    Add semicolons

--- Duplicati/Library/Main/Database/Database schema/12. Performance Indexes.sql
+++ Duplicati/Library/Main/Database/Database schema/12. Performance Indexes.sql
@@ -1,12 +1,12 @@
-CREATE INDEX "nnc_Metadataset" ON Metadataset ("ID","BlocksetID")
-CREATE INDEX "nn_FilesetentryFile" on FilesetEntry ("FilesetID","FileID")
+CREATE INDEX "nnc_Metadataset" ON Metadataset ("ID","BlocksetID");
+CREATE INDEX "nn_FilesetentryFile" on FilesetEntry ("FilesetID","FileID");
 
 -- Line 602 & 603 LocalBackupDatabase.cs 
 -- CREATE INDEX "tmpName1" ON "{0}" ("Path"),tmpName1
 -- CREATE INDEX "tmpName2" ON "{0}" ("Path"),tmpName2
 
-CREATE INDEX "nn_FileLookup_BlockMeta" ON FileLookup ("BlocksetID", "MetadataID")
+CREATE INDEX "nn_FileLookup_BlockMeta" ON FileLookup ("BlocksetID", "MetadataID");
 
-CREATE INDEX "nnc_BlocksetEntry" ON "BlocksetEntry" ("Index", "BlocksetID", "BlockID")
+CREATE INDEX "nnc_BlocksetEntry" ON "BlocksetEntry" ("Index", "BlocksetID", "BlockID");
 
 UPDATE "Version" SET "Version" = 12;
