06bca4b5d8f53befb97f1b26be5b5fbb66955734
Thomas Suckow <tsuckow@gmail.com>
2022-04-10 20:52:01 -0700

    Try making test path absolute

--- Duplicati/UnitTest/BasicSetupHelper.cs
+++ Duplicati/UnitTest/BasicSetupHelper.cs
@@ -30,10 +30,11 @@ namespace Duplicati.UnitTest
         /// <summary>
         /// The base folder where all data is trashed around
         /// </summary>
-        protected static readonly string BASEFOLDER =
+        protected static readonly string BASEFOLDER = Path.GetFullPath(
             string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("UNITTEST_BASEFOLDER"))
             ? "duplicati_testdata"
-            : Environment.GetEnvironmentVariable("UNITTEST_BASEFOLDER");
+            : Environment.GetEnvironmentVariable("UNITTEST_BASEFOLDER")
+            );
 
         /// <summary>
         /// The folder path that serves as the backup destination
