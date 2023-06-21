01feaf0a49a43a2d303ac40b4866608809aafd92
Jojo-1000 <33495614+Jojo-1000@users.noreply.github.com>
2023-06-21 15:10:55 +0200

    Add workflow to build recovery tool.

---
+++ Installer/Windows/artifact_recov_win.bat
@@ -0,0 +1,47 @@
+@rem start this batch from the root Duplicati git directory
+
+@rem Installation instructions when building locally (March 2023)
+@rem The life expectancy of any URL on MS servers is about one or two years
+@rem so links can become obsolete fast.
+@rem   - setup a Windows 2022 VM 
+@rem     (should work for Win10 but a 2022 VM is valid for 6 months vs 1 for Win10)
+@rem   - install git from https://git-scm.com/download/win
+@rem   - add to the PATH the directory c:\program files\git
+@rem   - install .NET SDK 4.7 from https://dotnet.microsoft.com/en-us/download/visual-studio-sdks
+@rem   - install msbuild (visual studio community)
+@rem   - install nuget from https://learn.microsoft.com/en-us/nuget/install-nuget-client-tools
+@rem   - install wix 3 from https://wixtoolset.org
+@rem   - add to the PATH msbuild, wix3 and nuget
+
+for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value') do set dt=%%a
+set RELEASE_TIMESTAMP=%dt:~0,4%-%dt:~4,2%-%dt:~6,2%
+
+set RELEASE_INC_VERSION=$(cat Updates/build_version.txt)
+for /f %%a in ('type updates\build_version.txt') do set RELEASE_INC_VERSION=%%a
+set /a RELEASE_INC_VERSION=%RELEASE_INC_VERSION%+1
+
+set RELEASE_TYPE=canary
+
+set RELEASE_VERSION=2.0.7.%RELEASE_INC_VERSION%
+set RELEASE_NAME=%RELEASE_VERSION%_%RELEASE_TYPE%_%RELEASE_TIMESTAMP%
+
+set RELEASE_FILE_NAME=duplicati-%RELEASE_NAME%
+
+set RUNTMP=%USERPROFILE%
+set ZIPBUILDFILE=%1
+if "%ZIPBUILDFILE%" == "" (
+  where /q bash.exe
+  if ERRORLEVEL 1 (
+    git-bash -x Installer\bundleduplicati_recov.sh %RELEASE_NAME%
+  ) ELSE (
+    bash -x Installer\bundleduplicati_recov.sh %RELEASE_NAME%
+  )
+  set ZIPBUILDFILE=%RUNTMP%\%RELEASE_NAME%
+)
+cd Installer\Windows
+call build-msi %ZIPBUILDFILE%
+mkdir %RUNTMP%\artifacts
+move duplicati.msi %RUNTMP%\artifacts\duplicati-%RELEASE_NAME%.msi
+move duplicati-32bit.msi %RUNTMP%\artifacts\duplicati-32bit-%RELEASE_NAME%.msi
+cd ..\..
+
---
+++ Installer/bundleduplicati_recov.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# ripped from build-release.sh
+
+# directory where files are stored
+UPDATE_SOURCE="${RUNTMP}/tmpinstduplicati"
+# zip output to be used by the installers
+ZIPRESULT="${RUNTMP}/$1" 
+
+if [ -e "${UPDATE_SOURCE}" ]; then rm -rf "${UPDATE_SOURCE}"; fi
+if [ -f "${ZIPRESULT}" ]; then rm -rf "${ZIPRESULT}"; fi
+
+mkdir -p "${UPDATE_SOURCE}"
+
+cp -R Duplicati/CommandLine/RecoveryTool/bin/Release/* "${UPDATE_SOURCE}"
+
+# We copy some files for alphavss manually as they are not picked up by xbuild
+mkdir "${UPDATE_SOURCE}/alphavss"
+for FN in Duplicati/Library/Snapshots/bin/Release/AlphaVSS.*.dll; do
+	cp "${FN}" "${UPDATE_SOURCE}/alphavss/"
+done
+
+# Fix for some support libraries not being picked up
+for BACKEND in Duplicati/Library/Backend/*; do
+	if [ -d "${BACKEND}/bin/Release/" ]; then
+		cp "${BACKEND}/bin/Release/"*.dll "${UPDATE_SOURCE}"
+	fi
+done
+
+# Install the assembly redirects for all Duplicati .exe files
+find "${UPDATE_SOURCE}" -maxdepth 1 -type f -name Duplicati.*.exe -exec cp Installer/AssemblyRedirects.xml {}.config \;
+
+# Clean some unwanted build files
+for FILE in "control_dir" "Duplicati-server.sqlite" "Duplicati.debug.log" "updates"; do
+	if [ -e "${UPDATE_SOURCE}/${FILE}" ]; then rm -rf "${UPDATE_SOURCE}/${FILE}"; fi
+done
+
+# Clean the localization spam from Azure
+for FILE in "de" "es" "fr" "it" "ja" "ko" "ru" "zh-Hans" "zh-Hant"; do
+	if [ -e "${UPDATE_SOURCE}/${FILE}" ]; then rm -rf "${UPDATE_SOURCE}/${FILE}"; fi
+done
+
+# Clean debug files, if any
+rm -rf "${UPDATE_SOURCE}/"*.mdb;
+rm -rf "${UPDATE_SOURCE}/"*.pdb;
+
+# Remove all library docs files
+rm -rf "${UPDATE_SOURCE}/"*.xml;
+
+# Remove all .DS_Store and Thumbs.db files
+find  . -type f -name ".DS_Store" | xargs rm -rf
+find  . -type f -name "Thumbs.db" | xargs rm -rf
+
+# bundle everything info a zip file
+pushd "${UPDATE_SOURCE}"
+7z a -tzip -r "${ZIPRESULT}"
+popd
+rm "${UPDATE_SOURCE}" -rf
