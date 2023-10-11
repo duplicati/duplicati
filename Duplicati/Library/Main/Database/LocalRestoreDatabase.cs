c30d28318867ae8d5597b38bd9beb4b31f7df3cc
Jojo-1000 <33495614+Jojo-1000@users.noreply.github.com>
2023-10-11 23:30:02 +0200

    Fix restore with blocks from different files.
    Closes #4593
    
    The logic for finding existing source blocks requires that the same target files and hashes are adjacent in query results, but this was not the case.

--- Duplicati/Library/Main/Database/LocalRestoreDatabase.cs
+++ Duplicati/Library/Main/Database/LocalRestoreDatabase.cs
@@ -680,8 +680,9 @@ namespace Duplicati.Library.Main.Database
                 using(var cmd = connection.CreateCommand())
                 {
                     // TODO: Skip metadata as required
-                    cmd.CommandText = string.Format(@"SELECT DISTINCT ""A"".""TargetPath"", ""A"".""ID"", ""B"".""Hash"", (""B"".""Index"" * {2}), ""B"".""Index"", ""B"".""Size"", ""C"".""Path"", (""D"".""Index"" * {2}), ""E"".""Size"", ""B"".""Metadata"" FROM ""{0}"" ""A"", ""{1}"" ""B"", ""File"" ""C"", ""BlocksetEntry"" ""D"", ""Block"" E WHERE ""A"".""ID"" = ""B"".""FileID"" AND ""C"".""BlocksetID"" = ""D"".""BlocksetID"" AND ""D"".""BlockID"" = ""E"".""ID"" AND ""B"".""Hash"" = ""E"".""Hash"" AND ""B"".""Size"" = ""E"".""Size"" AND ""B"".""Restored"" = 0", filetablename, blocktablename, blocksize);
-                    using(var rd = cmd.ExecuteReader())
+                    // Have to order by target path and hash, to ensure BlockDescriptor and BlockSource match adjacent rows
+                    cmd.CommandText = string.Format(@"SELECT DISTINCT ""A"".""TargetPath"", ""A"".""ID"", ""B"".""Hash"", (""B"".""Index"" * {2}), ""B"".""Index"", ""B"".""Size"", ""C"".""Path"", (""D"".""Index"" * {2}), ""E"".""Size"", ""B"".""Metadata"" FROM ""{0}"" ""A"", ""{1}"" ""B"", ""File"" ""C"", ""BlocksetEntry"" ""D"", ""Block"" E WHERE ""A"".""ID"" = ""B"".""FileID"" AND ""C"".""BlocksetID"" = ""D"".""BlocksetID"" AND ""D"".""BlockID"" = ""E"".""ID"" AND ""B"".""Hash"" = ""E"".""Hash"" AND ""B"".""Size"" = ""E"".""Size"" AND ""B"".""Restored"" = 0 ORDER BY ""A"".""TargetPath"", ""B"".""Index""", filetablename, blocktablename, blocksize);
+                    using (var rd = cmd.ExecuteReader())
                     {
                         if (rd.Read())
                         {
