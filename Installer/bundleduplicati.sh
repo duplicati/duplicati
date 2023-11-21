237cc26f9e76a70ba9435b39829d0224edca7cd7
Konstantinos Kanellakis <kanellakhskostas@gmail.com>
2023-11-21 13:53:27 +0200

    test

--- Installer/bundleduplicati.sh
+++ Installer/bundleduplicati.sh
@@ -54,7 +54,7 @@ find  . -type f -name "Thumbs.db" | xargs rm -rf
 bash Installer/test.sh "${UPDATE_SOURCE}"
 # bundle everything info a zip file
 pushd "${UPDATE_SOURCE}"
-rm "Build" -rf
+rm -rf Build*
 7z a -tzip -r "${ZIPRESULT}"
 popd
-rm "${UPDATE_SOURCE}" -rf
+rm -rf "${UPDATE_SOURCE}"
