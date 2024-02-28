212d1f7bbeb711cd686192f4bed9bcb5c72f1cc9
Kenneth Skovhede <kenneth@hexad.dk>
2024-02-28 14:39:59 +0100

    Added tool to update the license

---
+++ BuildTools/LicenseUpdater/Program.cs
@@ -0,0 +1,58 @@
+﻿using LicenseUpdater;
+
+var cmdargs = Environment.GetCommandLineArgs();
+if (cmdargs.Length != 2)
+    throw new Exception($"Usage: dotnet run <path-to-source-folder>");
+
+var startpath = Path.GetFullPath(Environment.GetCommandLineArgs().Skip(1).First());
+if (!Directory.Exists(startpath))
+    throw new Exception($"Start path not found: {startpath}");
+
+var target_extensions = new[] {
+    ".cs"
+    // ".html",
+    // ".js",
+    // ".css"
+}.ToHashSet(StringComparer.OrdinalIgnoreCase);
+
+File.WriteAllText(Path.Combine(startpath, "LICENSE.txt"), Fragments.GetLicenseTextWithPrefixedLines(string.Empty));
+
+var candidates = Directory.EnumerateFiles(startpath, "*", SearchOption.AllDirectories)
+    .Where(x => target_extensions.Contains(Path.GetExtension(x) ?? string.Empty));
+
+var blacklistSubFolders = new[] { "bin", "obj", "packages" };
+
+foreach (var file in candidates)
+{
+    if (string.Equals("AssemblyInfo.cs", Path.GetFileName(file), StringComparison.OrdinalIgnoreCase))
+        continue;
+    if (blacklistSubFolders.Any(x => file.Contains($"{Path.DirectorySeparatorChar}{x}{Path.DirectorySeparatorChar}", StringComparison.OrdinalIgnoreCase)))
+        continue;
+
+    if (new FileInfo(file).Length > 10 * 1024 * 1024)
+    {
+        Console.WriteLine($"{file} skipped due to size");
+        continue;
+    }
+
+    var data = File.ReadAllText(file);
+    if (Fragments.MatchAndReplace(ref data))
+    {
+        File.WriteAllText(file, data);
+        // Console.WriteLine($"{file} updated!");
+        continue;
+    }
+
+    if (string.Equals(Path.GetExtension(file), ".cs", StringComparison.OrdinalIgnoreCase) && !data.StartsWith("// Copyright", StringComparison.OrdinalIgnoreCase))
+    {
+        data = Fragments.GetLicenseTextWithPrefixedLines() + data;
+        File.WriteAllText(file, data);
+        // Console.WriteLine($"{file} updated!");
+        continue;
+    }
+
+    Console.WriteLine($"{file} skipped, no match");
+}
+
+
+
---
+++ BuildTools/LicenseUpdater/README.md
@@ -0,0 +1,9 @@
+# License Updater
+
+This folder contains a small tool that can update the license in all files, using the current year as the copyright year. It is intended to be run once a year, using the root repository folder as input:
+
+```
+dotnet run ../../
+```
+
+The resulting change can then be reviewed and committed.
