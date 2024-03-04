52265395c5cabf68a69d6c5891371c84b901c687
Kenneth Skovhede <kenneth@hexad.dk>
2024-03-04 14:34:38 +0100

    Using only Avalonia for TrayIcon

--- Duplicati/GUI/Duplicati.GUI.TrayIcon/Program.cs
+++ Duplicati/GUI/Duplicati.GUI.TrayIcon/Program.cs
@@ -37,13 +37,6 @@ namespace Duplicati.GUI.TrayIcon
 
         public static HttpServerConnection Connection;
     
-        private const string TOOLKIT_OPTION = "toolkit";
-        private const string TOOLKIT_WINDOWS_FORMS = "winforms";
-        private const string TOOLKIT_GTK = "gtk";
-        private const string TOOLKIT_COCOA = "cocoa";
-        private const string TOOLKIT_RUMPS = "rumps";
-        private const string TOOLKIT_AVALONIA = "avalonia";
-
         private const string HOSTURL_OPTION = "hosturl";
         private const string NOHOSTEDSERVER_OPTION = "no-hosted-server";
         private const string READCONFIGFROMDB_OPTION = "read-config-from-db";
@@ -61,28 +54,6 @@ namespace Duplicati.GUI.TrayIcon
 
         public static string BrowserCommand { get { return _browser_command; } }
         public static Server.Database.Connection databaseConnection = null;
-
-        private static string GetDefaultToolKit()
-        {
-#if !__LEGACY_TOOLKITS__
-            return TOOLKIT_AVALONIA;
-#endif
-            // No longer using Cocoa directly as it fails on 32bit as well            
-            if (Platform.IsClientOSX)
-                return TOOLKIT_RUMPS;
-
-#if __WindowsGTK__ || ENABLE_GTK
-            if (Platform.IsClientPosix)
-            {
-                    return TOOLKIT_GTK;
-            }
-            else
-#endif
-            {
-                //Windows users expect a WinForms element
-                return TOOLKIT_WINDOWS_FORMS;
-            }
-        }
 
         /// <summary>
         /// The main entry point for the application.
@@ -118,8 +89,6 @@ namespace Duplicati.GUI.TrayIcon
                 foreach (Library.Interface.ICommandLineArgument arg in SupportedCommands)
                 {
                     Console.WriteLine("--{0}: {1}", arg.Name, arg.LongDescription);
-                    if (arg.Name == TOOLKIT_OPTION)
-                        Console.WriteLine("    Supported toolkits: {0}{1}", string.Join(", ", arg.ValidValues), Environment.NewLine);
                 }
 
                 Console.WriteLine("Additionally, these server options are also supported:");
@@ -132,31 +101,6 @@ namespace Duplicati.GUI.TrayIcon
             }
 
             options.TryGetValue(BROWSER_COMMAND_OPTION, out _browser_command);
-
-            string toolkit;
-            if (!options.TryGetValue(TOOLKIT_OPTION, out toolkit))
-            {
-#if !(__WindowsGTK__ || ENABLE_GTK)
-                if (Platform.IsClientPosix && !Platform.IsClientOSX)
-                    Console.WriteLine("Warning: this build does not support GTK, rebuild with ENABLE_GTK defined");
-#endif
-                toolkit = GetDefaultToolKit();
-            }
-            else
-            {
-                if (TOOLKIT_WINDOWS_FORMS.Equals(toolkit, StringComparison.OrdinalIgnoreCase))
-                    toolkit = TOOLKIT_WINDOWS_FORMS;
-#if __WindowsGTK__ || ENABLE_GTK
-                else if (TOOLKIT_GTK.Equals(toolkit, StringComparison.OrdinalIgnoreCase))
-                    toolkit = TOOLKIT_GTK;
-#endif
-                else if (TOOLKIT_COCOA.Equals(toolkit, StringComparison.OrdinalIgnoreCase))
-                    toolkit = TOOLKIT_COCOA;
-                else if (TOOLKIT_RUMPS.Equals(toolkit, StringComparison.OrdinalIgnoreCase))
-                    toolkit = TOOLKIT_RUMPS;
-                else
-                    toolkit = GetDefaultToolKit();
-            }
 
             HostedInstanceKeeper hosted = null;
 
@@ -231,10 +175,10 @@ namespace Duplicati.GUI.TrayIcon
             if (options.TryGetValue(HOSTURL_OPTION, out url))
                 serverURL = new Uri(url);
 
-            StartTray(_args, options, toolkit, hosted, password, saltedpassword);
+            StartTray(_args, options, hosted, password, saltedpassword);
         }
 
-        private static void StartTray(string[] _args, Dictionary<string, string> options, string toolkit, HostedInstanceKeeper hosted, string password, bool saltedpassword)
+        private static void StartTray(string[] _args, Dictionary<string, string> options, HostedInstanceKeeper hosted, string password, bool saltedpassword)
         {
             using (hosted)
             {
@@ -257,7 +201,7 @@ namespace Duplicati.GUI.TrayIcon
 
                         using (Connection = new HttpServerConnection(serverURL, password, saltedpassword, databaseConnection != null ? PasswordSource.Database : PasswordSource.HostedServer, disableTrayIconLogin, options))
                         {
-                            using (var tk = RunTrayIcon(toolkit))
+                            using (var tk = RunTrayIcon())
                             {
                                 if (hosted != null && Server.Program.ApplicationInstance != null)
                                     Server.Program.ApplicationInstance.SecondInstanceDetected +=
@@ -320,150 +264,15 @@ namespace Duplicati.GUI.TrayIcon
             }
         }
 
-        private static TrayIconBase RunTrayIcon(string toolkit)
-        {
-            //https://medium.com/@k.l.mueller/cross-platform-tray-bar-applications-with-xamarin-forms-bbd6c1b7f17a
-            //https://docs.microsoft.com/en-us/xamarin/xamarin-forms/platform/other/gtk?tabs=windows
-
-            if (toolkit == TOOLKIT_WINDOWS_FORMS)
-                return GetWinformsInstance();
-#if __WindowsGTK__ || ENABLE_GTK
-            else if (toolkit == TOOLKIT_GTK)
-                return GetGtkInstance();
-#endif
-            else if (toolkit == TOOLKIT_COCOA)
-                return GetCocoaRunnerInstance();
-            else if (toolkit == TOOLKIT_RUMPS)
-                return GetRumpsRunnerInstance();
-            else if (toolkit == TOOLKIT_AVALONIA)
-                return GetAvaloniaRunnerInstance();
-            else
-                throw new UserInformationException(string.Format("The selected toolkit '{0}' is invalid", toolkit), "TrayIconInvalidToolKit");
-        }
-        
-        //We keep these in functions to avoid attempting to load the instance,
-        // because the required assemblies may not exist on the machine 
-        //
-        //They must be non-inlined even if they are prime candidates,
-        // as the inlining will pollute the type system and possibly
-        // attempt to load non-existing assemblies
-
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
-        private static TrayIconBase GetWinformsInstance() { return new Win32Runner(); }
-#if __MonoCS__ || __WindowsGTK__ || ENABLE_GTK
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
-        private static TrayIconBase GetGtkInstance() { return new GtkRunner(); }
-#endif
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
-        private static TrayIconBase GetCocoaRunnerInstance() { 
-#if XAMARIN_MAC_ENABLE && __LEGACY_TOOLKITS__
-            return new CocoaRunner();
-#else
-            throw new UserInformationException("Xamarin.Mac framework not found", "TrayIconMissingXamarinMac");
-#endif
-         } 
-
-        private static TrayIconBase GetRumpsRunnerInstance() { return new RumpsRunner(); }
-        private static TrayIconBase GetAvaloniaRunnerInstance() { return new AvaloniaRunner(); }
-
-
-        //The functions below simply load the requested type,
-        // and if the type is not present, calling the function will result in an exception.
-        //This seems to be more reliable than attempting to load the assembly,
-        // as there are too many complex rules for when an updated assembly is also
-        // acceptable. This is fairly error proof, as it is just asks the runtime
-        // to load the required types
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
-        private static bool TryGetGtk()
-        {
-#if __WindowsGTK__ || ENABLE_GTK
-            return typeof(Gtk.StatusIcon) != null;
-#else
-            return false;
-#endif
-        }
-
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
-        private static bool TryGetWinforms()
-        {
-            return Platform.IsClientWindows;
-        }
-        
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
-        private static bool TryGetXamarinMac()
-        {
-#if XAMARIN_MAC_ENABLE
-            return typeof(AppKit.NSApplication) != null;
-#else
-            return false;
-#endif
-        }
-
-        //The functions below here, simply wrap the call to the above functions,
-        // converting the exception to a simple boolean value, so the calling
-        // code can be kept free of error handling
-        private static bool SupportsGtk
-        {
-            get 
-            {
-                try { return TryGetGtk(); }
-                catch {}
-                
-                return false;
-            }
-        }
-
-        private static bool SupportsCocoa
-        {
-            get 
-            {
-                try { return TryGetXamarinMac(); }
-                catch {}
-                
-                return false;
-            }
-        }
-        
-        private static bool SupportsRumps
-        {
-            get 
-            {
-                try { return RumpsRunner.CanRun(); }
-                catch {}
-
-                return false;
-            }
-        }
-
-
-        private static bool SupportsWinForms
-        {
-            get 
-            {
-                try { return TryGetWinforms(); }
-                catch {}
-                
-                return false;
-            }
-        }
+        private static TrayIconBase RunTrayIcon()
+            => new AvaloniaRunner();
 
         public static Duplicati.Library.Interface.ICommandLineArgument[] SupportedCommands
         {
             get
             {
-                var toolkits = new List<string>();
-                if (SupportsWinForms)
-                    toolkits.Add(TOOLKIT_WINDOWS_FORMS);
-                if (SupportsGtk)
-                    toolkits.Add(TOOLKIT_GTK);
-                if (SupportsCocoa)
-                    toolkits.Add(TOOLKIT_COCOA);
-                if (SupportsRumps)
-                    toolkits.Add(TOOLKIT_RUMPS);
-                
                 var args = new List<Duplicati.Library.Interface.ICommandLineArgument>()
                 {
-                    new Duplicati.Library.Interface.CommandLineArgument(TOOLKIT_OPTION, CommandLineArgument.ArgumentType.Enumeration, "Selects the toolkit to use", "Choose the toolkit used to generate the TrayIcon, note that it will fail if the selected toolkit is not supported on this machine", GetDefaultToolKit(), null, toolkits.ToArray()),
                     new Duplicati.Library.Interface.CommandLineArgument(HOSTURL_OPTION, CommandLineArgument.ArgumentType.String, "Selects the url to connect to", "Supply the url that the TrayIcon will connect to and show status for", DEFAULT_HOSTURL),
                     new Duplicati.Library.Interface.CommandLineArgument(NOHOSTEDSERVER_OPTION, CommandLineArgument.ArgumentType.String, "Disables local server", "Set this option to not spawn a local service, use if the TrayIcon should connect to a running service"),
                     new Duplicati.Library.Interface.CommandLineArgument(READCONFIGFROMDB_OPTION, CommandLineArgument.ArgumentType.String, "Read server connection info from DB", $"Set this option to read server connection info for running service from its database (only together with {NOHOSTEDSERVER_OPTION})"),               
