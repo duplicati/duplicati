31b5e4df3784c2d819b864e7db36bdfdbd464028
Kenneth Skovhede <kenneth@hexad.dk>
2024-03-07 14:51:12 +0100

    Fixed test tool not applying global options

--- Duplicati/Server/WebServer/RESTMethods/RemoteOperation.cs
+++ Duplicati/Server/WebServer/RESTMethods/RemoteOperation.cs
@@ -23,6 +23,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using Duplicati.Library.Interface;
 
 namespace Duplicati.Server.WebServer.RESTMethods
 {
@@ -35,6 +36,62 @@ namespace Duplicati.Server.WebServer.RESTMethods
                 Exists = !string.IsNullOrWhiteSpace(path),
                 Path = path
             });
+        }
+
+        private Dictionary<string, string> ParseUrlOptions(Library.Utility.Uri uri)
+        {
+            var qp = uri.QueryParameters;
+
+            var opts = Runner.GetCommonOptions();
+            foreach (var k in qp.Keys.Cast<string>())
+                opts[k] = qp[k];
+
+            return opts;
+        }
+
+        private IEnumerable<IGenericModule> ConfigureModules(IDictionary<string, string> opts)
+        {
+            // TODO: This works because the generic modules are implemented
+            // with pre .NetCore logic, using static methods
+            // The modules are created to allow multipe dispose,
+            // which violates the .Net patterns
+            var modules = (from n in Library.DynamicLoader.GenericLoader.Modules
+                           where n is Library.Interface.IConnectionModule
+                           select n).ToArray();
+
+            foreach (var n in modules)
+                n.Configure(opts);
+
+            return modules;
+        }
+
+
+        private class TupleDisposeWrapper : IDisposable
+        {
+            public IBackend Backend { get; set; }
+            public IEnumerable<IGenericModule> Modules { get; set; }
+
+            public void Dispose()
+            {
+                Backend.Dispose();
+                DisposeModules();
+            }
+
+            public void DisposeModules()
+            {
+                foreach (var n in Modules)
+                    if (n is IDisposable disposable)
+                        disposable.Dispose();
+            }
+        }
+
+        private TupleDisposeWrapper GetBackend(string url)
+        {
+            var uri = new Library.Utility.Uri(url);
+            var opts = ParseUrlOptions(uri);
+            var modules = ConfigureModules(opts);
+            var backend = Duplicati.Library.DynamicLoader.BackendLoader.GetBackend(url, new Dictionary<string, string>());
+            return new TupleDisposeWrapper() { Backend = backend, Modules = modules };
         }
 
         private void CreateFolder(string uri, RequestInfo info)
@@ -51,12 +108,12 @@ namespace Duplicati.Server.WebServer.RESTMethods
             var remotename = info.Request.QueryString["filename"].Value;
 
             using(var ms = new System.IO.MemoryStream())   
-            using(var b = Library.DynamicLoader.BackendLoader.GetBackend(uri, new Dictionary<string, string>()))
+            using(var b = GetBackend(uri))
             {
                 using(var tf = new Library.Utility.TempFile())
                 {
                     System.IO.File.WriteAllText(tf, data);
-                    b.PutAsync(remotename, tf, CancellationToken.None).Wait();
+                    b.Backend.PutAsync(remotename, tf, CancellationToken.None).Wait();
                 }
             }
 
@@ -65,37 +122,43 @@ namespace Duplicati.Server.WebServer.RESTMethods
 
         private void ListFolder(string uri, RequestInfo info)
         {
-            using(var b = Duplicati.Library.DynamicLoader.BackendLoader.GetBackend(uri, new Dictionary<string, string>()))
-                info.OutputOK(b.List());
+            using(var b = GetBackend(uri))
+                info.OutputOK(b.Backend.List());
         }
 
         private void TestConnection(string url, RequestInfo info)
         {
-            
-            var modules = (from n in Library.DynamicLoader.GenericLoader.Modules
-                where n is Library.Interface.IConnectionModule
-                select n).ToArray();
+            bool autoCreate = info.Request.Param.Contains("autocreate")
+                ? Library.Utility.Utility.ParseBool(info.Request.Param["autocreate"].Value, false)
+                : false;
+
+            TupleDisposeWrapper wrapper = null;
 
             try
             {
-                var uri = new Library.Utility.Uri(url);
-                var qp = uri.QueryParameters;
+                wrapper = GetBackend(url);
 
-                var opts = new Dictionary<string, string>();
-                foreach (var k in qp.Keys.Cast<string>())
-                    opts[k] = qp[k];
+                using (var b = wrapper.Backend)
+                {
+                    try { b.Test(); }
+                    catch (FolderMissingException)
+                    {
+                        if (!autoCreate)
+                            throw;
 
-                foreach (var n in modules)
-                    n.Configure(opts);
-
-                using (var b = Duplicati.Library.DynamicLoader.BackendLoader.GetBackend(url, new Dictionary<string, string>()))
-                    b.Test();
-
-                info.OutputOK();
+                        b.CreateFolder();
+                        b.Test();
+                    }
+                    info.OutputOK();
+                }
             }
             catch (Duplicati.Library.Interface.FolderMissingException)
             {
-                info.ReportServerError("missing-folder");
+                if (!autoCreate) {
+                    info.ReportServerError("missing-folder");
+                } else {
+                    info.ReportServerError("error-creating-folder");
+                }
             }
             catch (Duplicati.Library.Utility.SslCertificateValidator.InvalidCertificateException icex)
             {
@@ -119,9 +182,8 @@ namespace Duplicati.Server.WebServer.RESTMethods
             }
             finally
             {
-                foreach (var n in modules)
-                    if (n is IDisposable disposable)
-                        disposable.Dispose();
+                if (wrapper != null)
+                    wrapper.DisposeModules();
             }
         }
 
