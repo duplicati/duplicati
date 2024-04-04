02d476d16cfac0b98c41a0b66ab43cee1df4fb3d
Kenneth Skovhede <kenneth@hexad.dk>
2024-04-04 15:44:28 +0200

    Added support for writing data to stdin

--- ReleaseBuilder/ProcessHelper.cs
+++ ReleaseBuilder/ProcessHelper.cs
@@ -16,8 +16,9 @@ public static class ProcessHelper
     /// <param name="codeIsError">Callback method that is invoked with the error code from the process; the result indicates if the status code should be interpreted as an error.
     /// Default value is <c>null</c> which will treat anything non-zero as an error</param>
     /// <param name="suppressStdErr">If <c>true</c>, stderr is not forwarded to the console</param>
+    /// <param name="writeStdIn">Function to write to stdin</param>
     /// <returns>An awaitable task</returns>
-    public static async Task Execute(IEnumerable<string> command, string? workingDirectory = null, CancellationToken cancellationToken = default, Func<int, bool>? codeIsError = null, bool suppressStdErr = false)
+    public static async Task Execute(IEnumerable<string> command, string? workingDirectory = null, CancellationToken cancellationToken = default, Func<int, bool>? codeIsError = null, bool suppressStdErr = false, Func<StreamWriter, Task>? writeStdIn = null)
     {
         if (!command.Any())
             throw new ArgumentException("Needs at least one command", nameof(command));
@@ -34,7 +35,7 @@ public static class ProcessHelper
             WorkingDirectory = workingDirectory,
             RedirectStandardError = !suppressStdErr,
             RedirectStandardOutput = false,
-            RedirectStandardInput = false,
+            RedirectStandardInput = writeStdIn != null,
             UseShellExecute = false,
         }) ?? throw new Exception($"Failed to launch process {command.First()}, null returned");
 
@@ -42,6 +43,9 @@ public static class ProcessHelper
         var t = suppressStdErr
             ? Task.CompletedTask
             : p.StandardError.BaseStream.CopyToAsync(Console.OpenStandardError(), cancellationToken);
+
+        if (writeStdIn != null)
+            await writeStdIn(p.StandardInput).ConfigureAwait(false);
 
         await p.WaitForExitAsync(cancellationToken).ConfigureAwait(false);
         if (codeIsError(p.ExitCode))
@@ -75,8 +79,9 @@ public static class ProcessHelper
     /// <param name="codeIsError">Callback method that is invoked with the error code from the process; the result indicates if the status code should be interpreted as an error.
     /// Default value is <c>null</c> which will treat anything non-zero as an error</param>
     /// <param name="suppressStdErr">If <c>true</c>, stderr is not forwarded to the console</param>
+    /// <param name="writeStdIn">Function to write to stdin</param>
     /// <returns>The output from stdout</returns>
-    public static async Task<string> ExecuteWithOutput(IEnumerable<string> command, string? workingDirectory = null, CancellationToken cancellationToken = default, Func<int, bool>? codeIsError = null, bool suppressStdErr = false)
+    public static async Task<string> ExecuteWithOutput(IEnumerable<string> command, string? workingDirectory = null, CancellationToken cancellationToken = default, Func<int, bool>? codeIsError = null, bool suppressStdErr = false, Func<StreamWriter, Task>? writeStdIn = null)
     {
         if (!command.Any())
             throw new ArgumentException("Needs at least one command", nameof(command));
@@ -93,7 +98,7 @@ public static class ProcessHelper
             WorkingDirectory = workingDirectory,
             RedirectStandardError = !suppressStdErr,
             RedirectStandardOutput = true,
-            RedirectStandardInput = false,
+            RedirectStandardInput = writeStdIn != null,
             UseShellExecute = false,
         }) ?? throw new Exception($"Failed to launch process {command.First()}, null returned");
 
@@ -101,6 +106,9 @@ public static class ProcessHelper
         var tstderr = suppressStdErr
             ? Task.CompletedTask
             : p.StandardError.BaseStream.CopyToAsync(Console.OpenStandardError(), cancellationToken);
+
+        if (writeStdIn != null)
+            await writeStdIn(p.StandardInput).ConfigureAwait(false);
 
         await p.WaitForExitAsync(cancellationToken).ConfigureAwait(false);
         if (codeIsError(p.ExitCode))
@@ -121,8 +129,9 @@ public static class ProcessHelper
     /// <param name="codeIsError">Callback method that is invoked with the error code from the process; the result indicates if the status code should be interpreted as an error.
     /// Default value is <c>null</c> which will treat anything non-zero as an error</param>
     /// <param name="suppressStdErr">If <c>true</c>, stderr is not forwarded to the console</param>
+    /// <param name="writeStdIn">Function to write to stdin</param>
     /// <returns>The output from stdout</returns>
-    public static async Task ExecuteWithOutput(IEnumerable<string> command, Stream stdout, string? workingDirectory = null, CancellationToken cancellationToken = default, Func<int, bool>? codeIsError = null, bool suppressStdErr = false)
+    public static async Task ExecuteWithOutput(IEnumerable<string> command, Stream stdout, string? workingDirectory = null, CancellationToken cancellationToken = default, Func<int, bool>? codeIsError = null, bool suppressStdErr = false, Func<StreamWriter, Task>? writeStdIn = null)
     {
         if (!command.Any())
             throw new ArgumentException("Needs at least one command", nameof(command));
@@ -139,7 +148,7 @@ public static class ProcessHelper
             WorkingDirectory = workingDirectory,
             RedirectStandardError = !suppressStdErr,
             RedirectStandardOutput = true,
-            RedirectStandardInput = false,
+            RedirectStandardInput = writeStdIn != null,
             UseShellExecute = false,
         }) ?? throw new Exception($"Failed to launch process {command.First()}, null returned");
 
@@ -147,6 +156,9 @@ public static class ProcessHelper
         var tstderr = suppressStdErr
             ? Task.CompletedTask
             : p.StandardError.BaseStream.CopyToAsync(Console.OpenStandardError(), cancellationToken);
+
+        if (writeStdIn != null)
+            await writeStdIn(p.StandardInput).ConfigureAwait(false);
 
         await p.WaitForExitAsync(cancellationToken).ConfigureAwait(false);
         if (codeIsError(p.ExitCode))
@@ -166,8 +178,9 @@ public static class ProcessHelper
     /// <param name="logFolder"/>The folder where the log files are written</param>
     /// <param name="logFilename">Function to create custom filenames for the log files</param>
     /// Default value is <c>null</c> which will treat anything non-zero as an error</param>
+    /// <param name="writeStdIn">Function to write to stdin</param>
     /// <returns>The output from stdout</returns>
-    public static async Task ExecuteWithLog(IEnumerable<string> command, string? workingDirectory = null, CancellationToken cancellationToken = default, Func<int, bool>? codeIsError = null, string? logFolder = null, Func<int, bool, string>? logFilename = null)
+    public static async Task ExecuteWithLog(IEnumerable<string> command, string? workingDirectory = null, CancellationToken cancellationToken = default, Func<int, bool>? codeIsError = null, string? logFolder = null, Func<int, bool, string>? logFilename = null, Func<StreamWriter, Task>? writeStdIn = null)
     {
         if (!command.Any())
             throw new ArgumentException("Needs at least one command", nameof(command));
@@ -186,7 +199,7 @@ public static class ProcessHelper
             WorkingDirectory = workingDirectory,
             RedirectStandardError = true,
             RedirectStandardOutput = true,
-            RedirectStandardInput = false,
+            RedirectStandardInput = writeStdIn != null,
             UseShellExecute = false,
         }) ?? throw new Exception($"Failed to launch process {command.First()}, null returned");
 
@@ -197,6 +210,9 @@ public static class ProcessHelper
 
         var t1 = p.StandardOutput.BaseStream.CopyToAsync(logstdout, cancellationToken);
         var t2 = p.StandardError.BaseStream.CopyToAsync(logstderr, cancellationToken);
+
+        if (writeStdIn != null)
+            await writeStdIn(p.StandardInput).ConfigureAwait(false);
 
         await p.WaitForExitAsync(cancellationToken).ConfigureAwait(false);
         if (codeIsError(p.ExitCode))
