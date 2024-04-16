1f6e1d238d67b13774215fee41f4105063032e0d
Kenneth Skovhede <kenneth@hexad.dk>
2024-04-16 07:22:54 +0200

    Fixed MSI program files on x64. Remove unused installer things.

--- Installer/Windows/MSI/templates/choco/chocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-$packageName = "{{.Choco.ID}}";
-$fileType = 'msi';
-$scriptPath =  $(Split-Path $MyInvocation.MyCommand.Path);
-$fileFullPath = Join-Path $scriptPath '{{.Choco.MsiFile}}';
-
-Uninstall-ChocolateyPackage $packageName $fileType "$fileFullPath /q"
