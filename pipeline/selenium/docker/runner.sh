502de4933f934d0d55c71c71af6873cfcbe8885f
Kenneth Skovhede <kenneth@hexad.dk>
2024-04-18 16:07:57 +0200

    Ported selenium fixes from main branch

--- pipeline/selenium/docker/runner.sh
+++ pipeline/selenium/docker/runner.sh
@@ -6,4 +6,4 @@ sudo dotnet publish -o /home/seluser/published/ /sources/Duplicati.sln
 sudo /home/seluser/published/Duplicati.Server &
 timeout 240 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; echo Checking if server started...; done' 127.0.0.1 8200
 echo Running Tests...
-python3 /sources/guiTests/guiTest.py
+python3 /sources/guiTests/guiTest.py --use-chrome
