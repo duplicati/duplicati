54237b1cbb5c4a6856e7b3990d529f4fe0391f87
Kenneth Skovhede <kenneth@hexad.dk>
2024-06-28 16:03:54 +0200

    Updated gui test, works locally, not in Docker

--- guiTests/guiTest.py
+++ guiTests/guiTest.py
@@ -20,6 +20,10 @@ parser.add_argument(
 parser.add_argument(
     "--use-chrome", action='store_true'
 )
+parser.add_argument(
+    "--chrome-path"
+)
+
 
 parser.set_defaults(headless=True)
 cmdopt = parser.parse_args()
@@ -45,9 +49,14 @@ if "TRAVIS_BUILD_NUMBER" in os.environ:
 elif cmdopt.use_chrome:
     print("using LOCAL Chrome webdriver")
     from selenium.webdriver.chrome.options import Options
-    import chromedriver_autoinstaller
-    chromedriver_autoinstaller.install()
     chr_opt = Options()
+
+    if cmdopt.chrome_path is None:
+        import chromedriver_autoinstaller
+        chromedriver_autoinstaller.install()
+    else:
+        chr_opt.binary_location = cmdopt.chrome_path
+
     opt = ["--ignore-certificate-errors", "--window-size=1280,800" ]
     if cmdopt.headless: opt += ["--headless"]
     for o in opt: chr_opt.add_argument(o)
@@ -96,15 +105,24 @@ def sha1_folder(folder):
     return sha1_dict
 
 
-def wait_for_text(time, xpath, text):
-    WebDriverWait(driver, time).until(expected_conditions.text_to_be_present_in_element((By.XPATH, xpath), text))
+def wait_for_text(xpath, text, timeout=10):
+    WebDriverWait(driver, timeout).until(expected_conditions.text_to_be_present_in_element((By.XPATH, xpath), text))
 
-def wait_for_load(time, by, target):
-    return WebDriverWait(driver, time).until(expected_conditions.presence_of_element_located((by, target)))
+def wait_for_load(by, target, timeout=10):
+    return WebDriverWait(driver, timeout).until(expected_conditions.presence_of_element_located((by, target)))
+
+def wait_for_clickable(by, target, timeout=10):
+    WebDriverWait(driver, timeout).until(expected_conditions.presence_of_element_located((by, target)))
+    return WebDriverWait(driver, timeout).until(expected_conditions.element_to_be_clickable((by, target)))
 
 def wait_for_redirect(expected_url, timeout=10):
     WebDriverWait(driver, timeout).until(lambda driver: driver.current_url == expected_url)
 
+def wait_for_title(title, timeout=10):
+    WebDriverWait(driver, timeout).until(lambda driver: title in driver.title)
+
+HOME_URL = "http://localhost:8200/ngax/index.html"
+LOGIN_URL = "http://localhost:8200/login.html"
 WEBSERVICE_PASSWORD = "easy1234"
 BACKUP_NAME = "BackupName"
 PASSWORD = "the_backup_password_is_really_long_and_safe"
@@ -114,80 +132,73 @@ DESTINATION_FOLDER_DIRECT_RESTORE = os.path.abspath("duplicati_gui_test_destinat
 RESTORE_FOLDER = os.path.abspath("duplicati_gui_test_restore")
 DIRECT_RESTORE_FOLDER = os.path.abspath("duplicati_gui_test_direct_restore")
 
-# wait 5 seconds for duplicati server to start
-time.sleep(5)
-
-driver.implicitly_wait(10)
 driver.maximize_window()
-driver.get("http://localhost:8200/login.html")
-wait_for_load(10, By.ID, "login-password").send_keys(WEBSERVICE_PASSWORD)
-wait_for_load(10, By.ID, "login-button").click()
+driver.get(LOGIN_URL)
+wait_for_load(By.ID, "login-password").send_keys(WEBSERVICE_PASSWORD)
+wait_for_load(By.ID, "login-button").click()
 
-wait_for_redirect("http://localhost:8200/ngax/index.html")
-if "Duplicati" not in driver.title:
-    raise Exception("Unable to load duplicati GUI! Got: " + driver.title)
+wait_for_redirect(HOME_URL)
+wait_for_title("Duplicati")
 
 # Create and hash random files in the source folder
 write_random_file(1024 * 1024, SOURCE_FOLDER + os.sep + "1MB.test")
 write_random_file(100 * 1024, SOURCE_FOLDER + os.sep + "subfolder" + os.sep + "100KB.test")
 sha1_source = sha1_folder(SOURCE_FOLDER)
 
-# Dismiss the password request
-wait_for_load(10, By.LINK_TEXT, "No, my machine has only a single account").click()
-
 # Add new backup
-wait_for_load(10, By.LINK_TEXT, "Add backup").click()
+wait_for_load(By.LINK_TEXT, "Add backup")
+wait_for_clickable(By.LINK_TEXT, "Add backup").click()
 
 # Choose the "add new" option
-wait_for_load(10, By.ID, "blank").click()
-wait_for_load(10, By.XPATH, "//input[@class='submit next']").click()
+wait_for_clickable(By.ID, "blank").click()
+wait_for_load(By.XPATH, "//input[@class='submit next']").click()
 
 # Add new backup - General page
-wait_for_load(10, By.ID, "name").send_keys(BACKUP_NAME)
-wait_for_load(10, By.ID, "passphrase").send_keys(PASSWORD)
-wait_for_load(10, By.ID, "repeat-passphrase").send_keys(PASSWORD)
-wait_for_load(10, By.ID, "nextStep1").click()
+wait_for_load(By.ID, "name").send_keys(BACKUP_NAME)
+wait_for_load(By.ID, "passphrase").send_keys(PASSWORD)
+wait_for_load(By.ID, "repeat-passphrase").send_keys(PASSWORD)
+wait_for_load(By.ID, "nextStep1").click()
 
 # Add new backup - Destination page
-wait_for_load(10, By.LINK_TEXT, "Manually type path").click()
-wait_for_load(10, By.ID, "file_path").send_keys(DESTINATION_FOLDER)
-wait_for_load(10, By.ID, "nextStep2").click()
+wait_for_load(By.LINK_TEXT, "Manually type path").click()
+wait_for_load(By.ID, "file_path").send_keys(DESTINATION_FOLDER)
+wait_for_load(By.ID, "nextStep2").click()
 
 # Add new backup - Source Data page
-wait_for_load(10, By.ID, "sourcePath").send_keys(os.path.abspath(SOURCE_FOLDER) + os.sep)
-wait_for_load(10, By.ID, "sourceFolderPathAdd").click()
-wait_for_load(10, By.ID, "nextStep3").click()
+wait_for_load(By.ID, "sourcePath").send_keys(os.path.abspath(SOURCE_FOLDER) + os.sep)
+wait_for_load(By.ID, "sourceFolderPathAdd").click()
+wait_for_load(By.ID, "nextStep3").click()
 
 # Add new backup - Schedule page
-useScheduleRun = wait_for_load(10, By.ID, "useScheduleRun")
+useScheduleRun = wait_for_load(By.ID, "useScheduleRun")
 if useScheduleRun.is_selected():
     useScheduleRun.click()
-wait_for_load(10, By.ID, "nextStep4").click()
+wait_for_load(By.ID, "nextStep4").click()
 
 # Add new backup - Options page
-wait_for_load(10, By.ID, "save").click()
+wait_for_load(By.ID, "save").click()
 
 # Run the backup job and wait for finish
-wait_for_load(10, By.LINK_TEXT, BACKUP_NAME).click()
+wait_for_clickable(By.LINK_TEXT, BACKUP_NAME).click()
 [n for n in driver.find_elements("xpath", "//dl[@class='taskmenu']/dd/p/span[contains(text(),'Run now')]") if n.is_displayed()][0].click()
-wait_for_text(60, "//div[@class='task ng-scope']/dl[2]/dd[1]", "(took ")
+wait_for_text("//div[@class='task ng-scope']/dl[2]/dd[1]", "(took ", 60)
 
 # Restore
 if len([n for n in driver.find_elements("xpath", u"//span[contains(text(),'Restore files \u2026')]") if n.is_displayed()]) == 0:
-    wait_for_load(10, By.LINK_TEXT, BACKUP_NAME).click()
+    wait_for_clickable(By.LINK_TEXT, BACKUP_NAME).click()
 
 [n for n in driver.find_elements("xpath", u"//span[contains(text(),'Restore files \u2026')]") if n.is_displayed()][0].click()
-wait_for_load(10, By.XPATH, "//span[contains(text(),'" + SOURCE_FOLDER + "')]")  # wait for filelist
+wait_for_load(By.XPATH, "//span[contains(text(),'" + SOURCE_FOLDER + "')]")  # wait for filelist
 time.sleep(1) # Delay so page has time to load
-wait_for_load(10, By.XPATH, "//restore-file-picker/ul/li/div/a[2]").click()  # select root folder checkbox
+wait_for_load(By.XPATH, "//restore-file-picker/ul/li/div/a[2]").click()  # select root folder checkbox
 
-wait_for_load(10, By.XPATH, "//form[@id='restore']/div[1]/div[@class='buttons']/a/span[contains(text(), 'Continue')]").click()
-wait_for_load(10, By.ID, "restoretonewpath").click()
-wait_for_load(10, By.ID, "restore_path").send_keys(RESTORE_FOLDER)
-wait_for_load(10, By.XPATH, "//form[@id='restore']/div/div[@class='buttons']/a/span[contains(text(),'Restore')]").click()
+wait_for_load(By.XPATH, "//form[@id='restore']/div[1]/div[@class='buttons']/a/span[contains(text(), 'Continue')]").click()
+wait_for_load(By.ID, "restoretonewpath").click()
+wait_for_load(By.ID, "restore_path").send_keys(RESTORE_FOLDER)
+wait_for_load(By.XPATH, "//form[@id='restore']/div/div[@class='buttons']/a/span[contains(text(),'Restore')]").click()
 
 # wait for restore to finish
-wait_for_text(60, "//form[@id='restore']/div[3]/h3/div[1]", "Your files and folders have been restored successfully.")
+wait_for_text("//form[@id='restore']/div[3]/h3/div[1]", "Your files and folders have been restored successfully.", 60)
 
 # hash restored files
 sha1_restore = sha1_folder(RESTORE_FOLDER)
@@ -200,29 +211,29 @@ if os.path.exists(RESTORE_FOLDER):
 os.rename(DESTINATION_FOLDER, DESTINATION_FOLDER_DIRECT_RESTORE)
 
 # direct restore
-wait_for_load(10, By.LINK_TEXT, "Restore").click()
+wait_for_load(By.LINK_TEXT, "Restore").click()
 
 # Choose the "restore direct" option
-wait_for_load(10, By.ID, "direct").click()
-wait_for_load(10, By.XPATH, "//input[@class='submit next']").click()
+wait_for_load(By.ID, "direct").click()
+wait_for_load(By.XPATH, "//input[@class='submit next']").click()
 
-wait_for_load(10, By.LINK_TEXT, "Manually type path").click()
-wait_for_load(10, By.ID, "file_path").send_keys(DESTINATION_FOLDER_DIRECT_RESTORE)
-wait_for_load(10, By.ID, "nextStep1").click()
+wait_for_load(By.LINK_TEXT, "Manually type path").click()
+wait_for_load(By.ID, "file_path").send_keys(DESTINATION_FOLDER_DIRECT_RESTORE)
+wait_for_load(By.ID, "nextStep1").click()
 
-wait_for_load(10, By.ID, "password").send_keys(PASSWORD)
-wait_for_load(10, By.ID, "connect").click()
-wait_for_load(10, By.XPATH, "//span[contains(text(),'" + SOURCE_FOLDER + "')]")  # wait for filelist
+wait_for_load(By.ID, "password").send_keys(PASSWORD)
+wait_for_load(By.ID, "connect").click()
+wait_for_load(By.XPATH, "//span[contains(text(),'" + SOURCE_FOLDER + "')]")  # wait for filelist
 time.sleep(1) # Delay so page has time to load
-wait_for_load(10, By.XPATH, "//restore-file-picker/ul/li/div/a[2]").click()  # select root folder checkbox
-wait_for_load(10, By.XPATH, "//form[@id='restore']/div[1]/div[@class='buttons']/a/span[contains(text(), 'Continue')]").click()
+wait_for_load(By.XPATH, "//restore-file-picker/ul/li/div/a[2]").click()  # select root folder checkbox
+wait_for_load(By.XPATH, "//form[@id='restore']/div[1]/div[@class='buttons']/a/span[contains(text(), 'Continue')]").click()
 
-wait_for_load(10, By.ID, "restoretonewpath").click()
-wait_for_load(10, By.ID, "restore_path").send_keys(DIRECT_RESTORE_FOLDER)
-wait_for_load(10, By.XPATH, "//form[@id='restore']/div/div[@class='buttons']/a/span[contains(text(),'Restore')]").click()
+wait_for_load(By.ID, "restoretonewpath").click()
+wait_for_load(By.ID, "restore_path").send_keys(DIRECT_RESTORE_FOLDER)
+wait_for_load(By.XPATH, "//form[@id='restore']/div/div[@class='buttons']/a/span[contains(text(),'Restore')]").click()
 
 # wait for restore to finish
-wait_for_text(60, "//form[@id='restore']/div[3]/h3/div[1]", "Your files and folders have been restored successfully.")
+wait_for_text("//form[@id='restore']/div[3]/h3/div[1]", "Your files and folders have been restored successfully.", 60)
 
 # hash direct restore files
 sha1_direct_restore = sha1_folder(DIRECT_RESTORE_FOLDER)
