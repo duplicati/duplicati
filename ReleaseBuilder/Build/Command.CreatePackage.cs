38ac29be13f0fef21c0a8b7c33fdc7e51f039dee
Kenneth Skovhede <kenneth@hexad.dk>
2024-07-01 14:25:06 +0200

    Making docker version tags lowercase

--- ReleaseBuilder/Build/Command.CreatePackage.cs
+++ ReleaseBuilder/Build/Command.CreatePackage.cs
@@ -837,7 +837,7 @@ public static partial class Command
 
         // Build the images
         var args = new List<string> { Program.Configuration.Commands.Docker!, "buildx", "build" };
-        args.AddRange(tags.SelectMany(x => new[] { "-t", $"{rtcfg.DockerRepo}:{x}" }));
+        args.AddRange(tags.SelectMany(x => new[] { "-t", $"{rtcfg.DockerRepo}:{x.ToLowerInvariant()}" }));
         args.AddRange([
             "--platform", string.Join(",", dockerArchs),
             "--build-arg", $"VERSION={rtcfg.ReleaseInfo.Version}",
