dba91b2a17d7670dda50c0ab5ba1264482da2c86
Suguru Hirahara <luixxiul@users.noreply.github.com>
2024-07-24 13:37:36 -0400

    Sort DescriptionDisableUploadVerifyShort on FTP/Strings.cs

--- Duplicati/Library/Backend/FTP/Strings.cs
+++ Duplicati/Library/Backend/FTP/Strings.cs
@@ -39,7 +39,7 @@ namespace Duplicati.Library.Backend.Strings {
         public static string MissingFolderError(string foldername, string message) { return LC.L(@"The folder {0} was not found, message: {1}", foldername, message); }
         public static string ListVerifyFailure(string filename, IEnumerable<string> files) { return LC.L(@"The file {0} was uploaded but not found afterwards, the file listing returned {1}", filename, string.Join(Environment.NewLine, files)); }
         public static string ListVerifySizeFailure(string filename, long actualsize, long expectedsize) { return LC.L(@"The file {0} was uploaded but the returned size was {1} and it was expected to be {2}", filename, actualsize, expectedsize); }
-        public static string DescriptionDisableUploadVerifyShort { get { return LC.L(@"Disable upload verification"); } }
         public static string DescriptionDisableUploadVerifyLong { get { return LC.L(@"To protect against network failures, every upload will be attempted verified. Use this option to disable this verification to make the upload faster but less reliable."); } }
+        public static string DescriptionDisableUploadVerifyShort { get { return LC.L(@"Disable upload verification"); } }
     }
 }
