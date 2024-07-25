2d22b8cdd6a2e71f7be17f29d108eb69fac5ca3c
Suguru Hirahara <luixxiul@users.noreply.github.com>
2024-07-25 00:49:26 -0400

    Sort strings on Builtin/Strings.cs - part 2

--- Duplicati/Library/Modules/Builtin/Strings.cs
+++ Duplicati/Library/Modules/Builtin/Strings.cs
@@ -55,12 +55,12 @@ namespace Duplicati.Library.Modules.Builtin.Strings
         public static string DisplayName { get { return LC.L(@"Configure http requests"); } }
         public static string OauthurlLong { get { return LC.L(@"Duplicati uses an external server to support the OAuth authentication flow. If you have set up your own Duplicati OAuth server, you can supply the refresh url."); } }
         public static string OauthurlShort { get { return LC.L(@"Alternate OAuth URL"); } }
-        public static string SslversionsShort { get { return LC.L(@"Sets allowed SSL versions"); } }
         public static string SslversionsLong { get { return LC.L(@"This option changes the default SSL versions allowed. This is an advanced option and should only be used if you want to enhance security or work around an issue with a particular SSL protocol."); } }
+        public static string SslversionsShort { get { return LC.L(@"Sets allowed SSL versions"); } }
         public static string OperationtimeoutLong { get { return LC.L(@"This option changes the default timeout for any HTTP request, the time covers the entire operation from initial packet to shutdown"); } }
         public static string OperationtimeoutShort { get { return LC.L(@"Sets the default operation timeout"); } }
-        public static string ReadwritetimeoutShort { get { return LC.L(@"Sets readwrite"); } }
         public static string ReadwritetimeoutLong { get { return LC.L(@"This option changes the default read-write timeout. Read-write timeouts are used to detect a stalled requests, and this option configures the maximum time between activity on a connection."); } }
+        public static string ReadwritetimeoutShort { get { return LC.L(@"Sets readwrite"); } }
         public static string BufferrequestsLong { get { return LC.L(@"This option sets the HTTP buffering. Setting this to ""{0}"" can cause memory leaks, but can also improve performance in some cases.", "true"); } }
         public static string BufferrequestsShort { get { return LC.L(@"Sets HTTP buffering"); } }
     }
@@ -145,7 +145,6 @@ To enable SMTP over SSL, use the format smtps://example.com. To enable SMTP STAR
     {
         public static string SendxmpptoLong { get { return LC.L(@"The users who should have the messages sent, specify multiple users separated with commas"); } }
         public static string SendxmpptoShort { get { return LC.L(@"XMPP recipient email"); } }
-        public static string SendxmppmessageShort { get { return LC.L(@"The message template"); } }
         public static string SendxmppmessageLong { get { return LC.L(@"This value can be a filename. If the file exists, the file contents will be used as the message.
 
 In the message, certain tokens are replaced:
@@ -155,15 +154,16 @@ In the message, certain tokens are replaced:
 %PARSEDRESULT% - The parsed result, if the operation is a backup. Possible values are: Error, Warning, Success
 
 All command line options are also reported within %value%, e.g. %volsize%. Any unknown/unset value is removed."); } }
+        public static string SendxmppmessageShort { get { return LC.L(@"The message template"); } }
         public static string SendxmppusernameLong { get { return LC.L(@"The username for the account that will send the message, including the hostname. I.e. ""account@jabber.org/Home"""); } }
         public static string SendxmppusernameShort { get { return LC.L(@"The XMPP username"); } }
-        public static string SendxmpppasswordShort { get { return LC.L(@"The XMPP password"); } }
         public static string SendxmpppasswordLong { get { return LC.L(@"The password for the account that will send the message"); } }
+        public static string SendxmpppasswordShort { get { return LC.L(@"The XMPP password"); } }
         public static string SendxmpplevelLong(string success, string warning, string error, string fatal, string all) { return LC.L(@"You can specify one of ""{0}"", ""{1}"", ""{2}"", ""{3}"". 
 You can supply multiple options with a comma separator, e.g. ""{0},{1}"". The special value ""{4}"" is a shorthand for ""{0},{1},{2},{3}"" and will cause all backup operations to send a message.", success, warning, error, fatal, all); }
         public static string SendxmpplevelShort { get { return LC.L(@"The messages to send"); } }
-        public static string SendxmppanyoperationShort { get { return LC.L(@"Send messages for all operations"); } }
         public static string SendxmppanyoperationLong { get { return LC.L(@"By default, messages will only be sent after a Backup operation. Use this option to send messages for all operations"); } }
+        public static string SendxmppanyoperationShort { get { return LC.L(@"Send messages for all operations"); } }
         public static string DisplayName { get { return LC.L(@"XMPP report module"); } }
         public static string Description { get { return LC.L(@"This module provides support for sending status reports via XMPP messages"); } }
         public static string LoginTimeoutError { get { return LC.L(@"Timeout occurred while logging in to jabber server"); } }
@@ -175,7 +175,6 @@ You can supply multiple options with a comma separator, e.g. ""{0},{1}"". The sp
         public static string Description { get { return LC.L(@"This module provides support for sending status reports via HTTP messages"); } }
         public static string SendhttpurlLong { get { return LC.L(@"HTTP report url"); } }
         public static string SendhttpurlShort { get { return LC.L(@"HTTP report url"); } }
-        public static string SendhttpmessageShort { get { return LC.L(@"The message template"); } }
         public static string SendhttpmessageLong { get { return LC.L(@"This value can be a filename. If the file exists, the file contents will be used as the message.
 
 In the message, certain tokens are replaced:
@@ -185,19 +184,20 @@ In the message, certain tokens are replaced:
 %PARSEDRESULT% - The parsed result, if the operation is a backup. Possible values are: Error, Warning, Success
 
 All command line options are also reported within %value%, e.g. %volsize%. Any unknown/unset value is removed."); } }
+        public static string SendhttpmessageShort { get { return LC.L(@"The message template"); } }
         public static string SendhttpmessageparameternameLong { get { return LC.L(@"The name of the parameter to send the message as."); } }
         public static string SendhttpmessageparameternameShort { get { return LC.L(@"The name of the parameter to send the message as"); } }
-        public static string SendhttpextraparametersShort { get { return LC.L(@"Extra parameters to add to the http message"); } }
         public static string SendhttpextraparametersLong { get { return LC.L(@"Extra parameters to add to the http message. I.e. ""parameter1=value1&parameter2=value2"""); } }
+        public static string SendhttpextraparametersShort { get { return LC.L(@"Extra parameters to add to the http message"); } }
         public static string SendhttplevelLong(string success, string warning, string error, string fatal, string all) { return LC.L(@"You can specify one of ""{0}"", ""{1}"", ""{2}"", ""{3}"". 
 You can supply multiple options with a comma separator, e.g. ""{0},{1}"". The special value ""{4}"" is a shorthand for ""{0},{1},{2},{3}"" and will cause all backup operations to send a message.", success, warning, error, fatal, all); }
         public static string SendhttplevelShort { get { return LC.L(@"The messages to send"); } }
-        public static string SendhttpanyoperationShort { get { return LC.L(@"Send messages for all operations"); } }
         public static string SendhttpanyoperationLong { get { return LC.L(@"By default, messages will only be sent after a Backup operation. Use this option to send messages for all operations"); } }
+        public static string SendhttpanyoperationShort { get { return LC.L(@"Send messages for all operations"); } }
         public static string HttpverbLong { get { return LC.L(@"Use this option to change the default HTTP verb used to submit a report"); } }
         public static string HttpverbShort { get { return LC.L(@"Sets the HTTP verb to use"); } }
-        public static string SendhttpurlsformShort { get { return LC.L(@"HTTP report urls for sending form data"); } }
         public static string SendhttpurlsformLong { get { return LC.L(@"HTTP report urls for sending form-encoded data. This property accepts multiple urls, seperated by a semi-colon. All urls will receive the same data. Note that this option ignores the format and verb settings"); } }
+        public static string SendhttpurlsformShort { get { return LC.L(@"HTTP report urls for sending form data"); } }
         public static string SendhttpurlsjsonLong { get { return LC.L(@"HTTP report urls for sending JSON data. This property accepts multiple urls, seperated by a semi-colon. All urls will receive the same data. Note that this option ignores the format and verb settings"); } }
         public static string SendhttpurlsjsonShort { get { return LC.L(@"HTTP report urls for sending JSON data"); } }
     }
@@ -207,11 +207,11 @@ You can supply multiple options with a comma separator, e.g. ""{0},{1}"". The sp
         public static string SendMessageFailedError(string message) { return LC.L(@"Failed to send message: {0}", message); }
         public static string OptionLoglevelLong { get { return LC.L("Use this option to set the log level for messages to include in the report"); } }
         public static string OptionLoglevelShort { get { return LC.L("Defines a log level for messages"); } }
-        public static string OptionLogfilterShort { get { return LC.L("Log message filter"); } }
         public static string OptionLogfilterLong { get { return LC.L("Use this option to set a filter expression that defines what options are included in the report"); } }
+        public static string OptionLogfilterShort { get { return LC.L("Log message filter"); } }
         public static string OptionmaxloglinesLong { get { return LC.L("Use this option to set the maximum number of log lines to include in the report. Zero or negative values means unlimited."); } }
         public static string OptionmaxloglinesShort { get { return LC.L("Limits log lines"); } }
-        public static string ResultFormatShort { get { return LC.L(@"Selects the output format for results"); } }
         public static string ResultFormatLong(IEnumerable<string> options) { return LC.L(@"Selects the output format for results. Available formats: {0}", string.Join(", ", options)); }
+        public static string ResultFormatShort { get { return LC.L(@"Selects the output format for results"); } }
     }
 }
