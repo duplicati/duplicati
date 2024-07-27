37b9d15b29f68bbb7a9a3f123f266c4f43d3a6b4
Suguru Hirahara <luixxiul@users.noreply.github.com>
2024-07-27 07:45:37 -0400

    Remove 'The "' from Idrivee2/Strings.cs

--- Duplicati/Library/Backend/Idrivee2/Strings.cs
+++ Duplicati/Library/Backend/Idrivee2/Strings.cs
@@ -21,10 +21,10 @@
 using Duplicati.Library.Localization.Short;
 namespace Duplicati.Library.Backend.Strings {
     internal static class Idrivee2Backend {
-        public static string KeySecretDescriptionLong { get { return LC.L(@"The ""Access Key Secret "" can be obtained after logging into your IDrive e2 account, this can also be supplied through the ""auth-password"" property."); } }
-        public static string KeySecretDescriptionShort { get { return LC.L(@"The ""Access Key Secret"""); } }
-        public static string KeyIDDescriptionLong { get { return LC.L(@"The ""Access Key ID"" can be obtained after logging into your IDrive e2 account., this can also be supplied through the ""auth-username"" property."); } }
-        public static string KeyIDDescriptionShort { get { return LC.L(@"The ""Access Key ID"""); } }
+        public static string KeySecretDescriptionLong { get { return LC.L(@"Access Key Secret can be obtained after logging into your IDrive e2 account, this can also be supplied through the ""auth-password"" property."); } }
+        public static string KeySecretDescriptionShort { get { return LC.L(@"Access Key Secret"); } }
+        public static string KeyIDDescriptionLong { get { return LC.L(@"Access Key ID can be obtained after logging into your IDrive e2 account., this can also be supplied through the ""auth-username"" property."); } }
+        public static string KeyIDDescriptionShort { get { return LC.L(@"Access Key ID"); } }
 
         public static string BucketNameOrPathDescriptionLong { get { return LC.L(@"The ""Bucket Name or Complete Path"" is name of target bucket or complete of a folder inside the bucket."); } }
         public static string BucketNameOrPathDescriptionShort { get { return LC.L(@"The ""Bucket Name or Complete Path"""); } }
