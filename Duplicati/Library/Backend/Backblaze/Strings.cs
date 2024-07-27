42a83beeaac4bb7b31972e04b4042dd7078bf87e
Suguru Hirahara <luixxiul@users.noreply.github.com>
2024-07-27 07:48:03 -0400

    Split sentences correctly on Backblaze/Strings.cs, Idrivee2/Strings.cs, and S3/Strings.cs

--- Duplicati/Library/Backend/Backblaze/Strings.cs
+++ Duplicati/Library/Backend/Backblaze/Strings.cs
@@ -22,9 +22,9 @@
 using Duplicati.Library.Localization.Short;
 namespace Duplicati.Library.Backend.Strings {
     internal static class B2 {
-        public static string B2applicationkeyDescriptionLong { get { return LC.L(@"B2 Cloud Storage Application Key can be obtained after logging into your Backblaze account, this can also be supplied through the ""auth-password"" property"); } }
+        public static string B2applicationkeyDescriptionLong { get { return LC.L(@"B2 Cloud Storage Application Key can be obtained after logging into your Backblaze account. This can also be supplied through the ""auth-password"" property"); } }
         public static string B2applicationkeyDescriptionShort { get { return LC.L(@"B2 Cloud Storage Application Key"); } }
-        public static string B2accountidDescriptionLong { get { return LC.L(@"B2 Cloud Storage Account ID can be obtained after logging into your Backblaze account, this can also be supplied through the ""auth-username"" property"); } }
+        public static string B2accountidDescriptionLong { get { return LC.L(@"B2 Cloud Storage Account ID can be obtained after logging into your Backblaze account. This can also be supplied through the ""auth-username"" property"); } }
         public static string B2accountidDescriptionShort { get { return LC.L(@"B2 Cloud Storage Account ID"); } }
         public static string DisplayName { get { return LC.L(@"B2 Cloud Storage"); } }
         public static string AuthPasswordDescriptionLong { get { return LC.L(@"The password used to connect to the server. This may also be supplied as the environment variable ""AUTH_PASSWORD""."); } }
--- Duplicati/Library/Backend/Idrivee2/Strings.cs
+++ Duplicati/Library/Backend/Idrivee2/Strings.cs
@@ -21,9 +21,9 @@
 using Duplicati.Library.Localization.Short;
 namespace Duplicati.Library.Backend.Strings {
     internal static class Idrivee2Backend {
-        public static string KeySecretDescriptionLong { get { return LC.L(@"Access Key Secret can be obtained after logging into your IDrive e2 account, this can also be supplied through the ""auth-password"" property."); } }
+        public static string KeySecretDescriptionLong { get { return LC.L(@"Access Key Secret can be obtained after logging into your IDrive e2 account. This can also be supplied through the ""auth-password"" property."); } }
         public static string KeySecretDescriptionShort { get { return LC.L(@"Access Key Secret"); } }
-        public static string KeyIDDescriptionLong { get { return LC.L(@"Access Key ID can be obtained after logging into your IDrive e2 account., this can also be supplied through the ""auth-username"" property."); } }
+        public static string KeyIDDescriptionLong { get { return LC.L(@"Access Key ID can be obtained after logging into your IDrive e2 account. This can also be supplied through the ""auth-username"" property."); } }
         public static string KeyIDDescriptionShort { get { return LC.L(@"Access Key ID"); } }
 
         public static string BucketNameOrPathDescriptionLong { get { return LC.L(@"The ""Bucket Name or Complete Path"" is name of target bucket or complete of a folder inside the bucket."); } }
