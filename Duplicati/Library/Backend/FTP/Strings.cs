c348e51048d412c4744bf4052763cde349d5f254
Suguru Hirahara <luixxiul@users.noreply.github.com>
2024-07-26 22:37:53 -0400

    Move DescriptionDisableUploadVerifyLong and DescriptionDisableUploadVerifyShort up

--- Duplicati/Library/Backend/FTP/Strings.cs
+++ Duplicati/Library/Backend/FTP/Strings.cs
@@ -36,10 +36,10 @@ namespace Duplicati.Library.Backend.Strings {
         public static string DescriptionAuthUsernameShort { get { return LC.L(@"Supplies the username used to connect to the server"); } }
         public static string DescriptionUseSSLLong { get { return LC.L(@"Use this flag to communicate using Secure Socket Layer (SSL) over ftp (ftps)."); } }
         public static string DescriptionUseSSLShort { get { return LC.L(@"Instructs Duplicati to use an SSL (ftps) connection"); } }
+        public static string DescriptionDisableUploadVerifyLong { get { return LC.L(@"To protect against network failures, every upload will be attempted verified. Use this option to disable this verification to make the upload faster but less reliable."); } }
+        public static string DescriptionDisableUploadVerifyShort { get { return LC.L(@"Disable upload verification"); } }
         public static string MissingFolderError(string foldername, string message) { return LC.L(@"The folder {0} was not found, message: {1}", foldername, message); }
         public static string ListVerifyFailure(string filename, IEnumerable<string> files) { return LC.L(@"The file {0} was uploaded but not found afterwards, the file listing returned {1}", filename, string.Join(Environment.NewLine, files)); }
         public static string ListVerifySizeFailure(string filename, long actualsize, long expectedsize) { return LC.L(@"The file {0} was uploaded but the returned size was {1} and it was expected to be {2}", filename, actualsize, expectedsize); }
-        public static string DescriptionDisableUploadVerifyLong { get { return LC.L(@"To protect against network failures, every upload will be attempted verified. Use this option to disable this verification to make the upload faster but less reliable."); } }
-        public static string DescriptionDisableUploadVerifyShort { get { return LC.L(@"Disable upload verification"); } }
     }
 }
