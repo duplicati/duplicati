f1f0ef81fc0e95cf1a78f1da5f85a637cd926f2d
Suguru Hirahara <luixxiul@users.noreply.github.com>
2024-07-29 22:10:12 -0400

    Add periods to strings on SSHv2/Strings.cs

--- Duplicati/Library/Backend/SSHv2/Strings.cs
+++ Duplicati/Library/Backend/SSHv2/Strings.cs
@@ -25,20 +25,20 @@ namespace Duplicati.Library.Backend.Strings
     {
         public static string Description { get { return LC.L(@"Module for generating SSH private/public keys"); } }
         public static string DisplayName { get { return LC.L(@"SSH Key Generator"); } }
-        public static string KeyUsernameLong { get { return LC.L(@"A username to append to the public key"); } }
+        public static string KeyUsernameLong { get { return LC.L(@"A username to append to the public key."); } }
         public static string KeyUsernameShort { get { return LC.L(@"Public key username"); } }
-        public static string KeyTypeLong { get { return LC.L(@"Determines the type of key to generate"); } }
+        public static string KeyTypeLong { get { return LC.L(@"Determines the type of key to generate."); } }
         public static string KeyTypeShort { get { return LC.L(@"The key type"); } }
-        public static string KeyLenLong { get { return LC.L(@"The length of the key in bits"); } }
+        public static string KeyLenLong { get { return LC.L(@"The length of the key in bits."); } }
         public static string KeyLenShort { get { return LC.L(@"The key length"); } }
     }
     internal static class KeyUploader
     {
         public static string Description { get { return LC.L(@"Module for uploading SSH public keys"); } }
         public static string DisplayName { get { return LC.L(@"SSH Key Uploader"); } }
-        public static string UrlLong { get { return LC.L(@"The SSH connection URL used to establish the connection"); } }
+        public static string UrlLong { get { return LC.L(@"The SSH connection URL used to establish the connection."); } }
         public static string UrlShort { get { return LC.L(@"The SSH connection URL"); } }
-        public static string PubkeyLong { get { return LC.L(@"The SSH public key must be a valid SSH string, which is appended to the .ssh/authorized_keys file"); } }
+        public static string PubkeyLong { get { return LC.L(@"The SSH public key must be a valid SSH string, which is appended to the .ssh/authorized_keys file."); } }
         public static string PubkeyShort { get { return LC.L(@"The SSH public key to append"); } }
     }
     internal static class SSHv2Backend
@@ -57,7 +57,7 @@ namespace Duplicati.Library.Backend.Strings
         public static string DescriptionSshkeyfileShort { get { return LC.L(@"Uses a SSH private key to authenticate"); } }
         public static string DescriptionSshkeyLong(string urlprefix) { return LC.L(@"An url-encoded SSH private key. The private key must be prefixed with {0}. If the file is encrypted, the password supplied is used to decrypt the keyfile. If this option is supplied, the password is not used to authenticate.", urlprefix); }
         public static string DescriptionSshkeyShort { get { return LC.L(@"Uses a SSH private key to authenticate"); } }
-        public static string DescriptionSshtimeoutLong { get { return LC.L(@"Use this option to manage the internal timeout for SSH operations. If this options is set to zero, the operations will not time out"); } }
+        public static string DescriptionSshtimeoutLong { get { return LC.L(@"Use this option to manage the internal timeout for SSH operations. If this options is set to zero, the operations will not time out."); } }
         public static string DescriptionSshtimeoutShort { get { return LC.L(@"Sets the operation timeout value"); } }
         public static string DescriptionSshkeepaliveLong { get { return LC.L(@"This option can be used to enable the keep-alive interval for the SSH connection. If the connection is idle, aggressive firewalls might close the connection. Using keep-alive will keep the connection open in this scenario. If this value is set to zero, the keep-alive is disabled."); } }
         public static string DescriptionSshkeepaliveShort { get { return LC.L(@"Sets a keepalive value"); } }
