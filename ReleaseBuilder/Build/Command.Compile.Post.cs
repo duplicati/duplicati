2bbf0207e3de3965d308f1030e2c6747fdc7b393
Kenneth Skovhede <kenneth@hexad.dk>
2024-08-02 23:12:32 +0200

    Added newly built `libMono.Unix.so` with large file support for 32bit Arm. This fixes #5352

--- ReleaseBuilder/Build/Command.Compile.Post.cs
+++ ReleaseBuilder/Build/Command.Compile.Post.cs
@@ -38,6 +38,7 @@ public static partial class Command
                     break;
 
                 case OSType.Linux:
+                    await ReplaceLibMonoUnix(baseDir, buildDir, arch);
                     break;
 
                 default:
@@ -259,6 +260,28 @@ public static partial class Command
         }
 
         /// <summary>
+        /// Replaces the library libMono.Unix.so with a version that has large file support for ARM7
+        /// </summary>
+        /// <param name="baseDir">The base directory</param>
+        /// <param name="buildDir">The build directory</param>
+        /// <param name="arch">The architecture to build for</param>
+        /// <returns>An awaitable task</returns>
+        static Task ReplaceLibMonoUnix(string baseDir, string buildDir, ArchType arch)
+        {
+            if (arch != ArchType.Arm7)
+                return Task.CompletedTask;
+
+            var sourceFile = Path.Combine(baseDir, "ReleaseBuilder", "Resources", "linux-arm-binary", "libMono.Unix.so");
+            var targetFile = Path.Combine(buildDir, "libMono.Unix.so");
+            if (!File.Exists(targetFile))
+                throw new Exception($"Expected file \"{targetFile}\" not found, has build changed?");
+
+            File.Copy(sourceFile, targetFile, overwrite: true);
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
         /// Signs all .exe and .dll files with Authenticode
         /// </summary>
         /// <param name="buildDir">The folder to sign files in</param>
@@ -279,27 +302,5 @@ public static partial class Command
             foreach (var file in filenames)
                 await rtcfg.AuthenticodeSign(file);
         }
-    }
-
-    /// <summary>
-    /// Downloads a file from a URL and saves it to a destination path
-    /// </summary>
-    /// <param name="url">The URL to download from</param>
-    /// <param name="destinationPath">The path to save the file to</param>
-    /// <returns>An awaitable task</returns>
-    static async Task DownloadFileAsync(string url, string destinationPath)
-    {
-        using var httpClient = new HttpClient(new HttpClientHandler
-        {
-            CookieContainer = new CookieContainer()
-        });
-
-        using var response = await httpClient.GetAsync(url);
-        response.EnsureSuccessStatusCode();
-        using var tf = new TempFile();
-        using var fileStream = File.Create(tf);
-        await response.Content.CopyToAsync(fileStream);
-
-        File.Move(tf, destinationPath);
     }
 }
