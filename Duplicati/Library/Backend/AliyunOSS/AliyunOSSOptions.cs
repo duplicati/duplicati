bc6553135e2e76830b8154c0d7d1f7eaceac7320
Suguru Hirahara <luixxiul@users.noreply.github.com>
2024-08-06 23:27:29 -0400

    Add my name to the copyright notice as well

--- Duplicati/Library/Backend/AliyunOSS/AliyunOSSOptions.cs
+++ Duplicati/Library/Backend/AliyunOSS/AliyunOSSOptions.cs
@@ -1,4 +1,5 @@
 // Copyright (C) 2024, trueai-org
+// Copyright (C) 2024, Suguru Hirahara
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
