28c53ab9f62656df000f4f4fb6e07e03b80d92a7
Kenneth Skovhede <kenneth@hexad.dk>
2024-08-15 14:50:31 +0200

    Fixed error message reading update version. This fixes #5434

--- Duplicati/WebserverCore/Abstractions/UpdateInfo.cs
+++ Duplicati/WebserverCore/Abstractions/UpdateInfo.cs
@@ -1,17 +1,39 @@
+using Duplicati.Library.AutoUpdater;
+
 namespace Duplicati.WebserverCore.Abstractions;
 
 public class UpdateInfo
 {
-    public string Displayname { get; set; } = "";
-    public string Version { get; set; } = "";
-    public DateTime? ReleaseTime { get; set; }
-    public string ReleaseType { get; set; } = "";
-    public string UpdateSeverity { get; set; } = "";
-    public string ChangeInfo { get; set; } = "";
-    public long CompressedSize { get; set; }
-    public long UncompressedSize { get; set; }
-    public string SHA256 { get; set; } = "";
-    public string MD5 { get; set; } = "";
-    public string[] RemoteURLS { get; set; } = [];
-    public FileEntry[] Files { get; set; } = [];
+    public required int MinimumCompatibleVersion { get; init; }
+    public required string? IncompatibleUpdateUrl { get; init; }
+    public required string? Displayname { get; init; }
+    public required string? Version { get; init; }
+    public required DateTime ReleaseTime { get; init; }
+    public required string? ReleaseType { get; init; }
+    public required string? UpdateSeverity { get; init; }
+    public required string? ChangeInfo { get; init; }
+    public required int PackageUpdaterVersion { get; init; }
+    public required PackageEntry[]? Packages { get; init; }
+    public required string GenericUpdatePageUrl { get; init; }
+
+    public static UpdateInfo? FromSrc(Library.AutoUpdater.UpdateInfo? src)
+    {
+        if (src == null)
+            return null;
+
+        return new UpdateInfo
+        {
+            MinimumCompatibleVersion = src.MinimumCompatibleVersion,
+            IncompatibleUpdateUrl = src.IncompatibleUpdateUrl,
+            Displayname = src.Displayname,
+            Version = src.Version,
+            ReleaseTime = src.ReleaseTime,
+            ReleaseType = src.ReleaseType,
+            UpdateSeverity = src.UpdateSeverity,
+            ChangeInfo = src.ChangeInfo,
+            PackageUpdaterVersion = src.PackageUpdaterVersion,
+            Packages = src.Packages,
+            GenericUpdatePageUrl = src.GenericUpdatePageUrl
+        };
+    }
 }
