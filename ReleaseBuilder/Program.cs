ecd22ac26446fd91396f39c4bdba2fc7fbe28908
Kenneth Skovhede <kenneth@hexad.dk>
2024-09-18 15:29:52 +0200

    Added support for building agent packages. Still lacking support for Windows MSI + MacOS pkg as they are currently tied to the TrayIcon being the base.

--- ReleaseBuilder/Program.cs
+++ ReleaseBuilder/Program.cs
@@ -18,29 +18,49 @@ class Program
         "win-x86-gui.msi",
         "win-arm64-gui.zip",
         "win-arm64-gui.msi",
+        // "win-x64-agent.msi", // Need to add support for building without trayicon + window service support
+        // "win-arm64-agent.msi", // Need to add support for building without trayicon + window service support
+        "win-x64-agent.zip",
+        "win-arm64-agent.zip",
+
         "linux-x64-gui.zip",
         "linux-x64-gui.deb",
         "linux-x64-gui.rpm",
-        "linux-arm7-gui.deb",
-        "linux-arm7-cli.deb",
         "linux-x64-cli.zip",
         "linux-x64-cli.deb",
         "linux-x64-cli.rpm",
         "linux-x64-cli.docker",
         // "linux-x64-cli.spk", // Need to add new integration with DSM WebUI
-        "linux-arm64-cli.docker",
+        "linux-x64-agent.zip",
+        "linux-x64-agent.deb",
+        "linux-x64-agent.rpm",
+        "linux-x64-agent.docker",
+
+        "linux-arm7-gui.zip",
+        "linux-arm7-gui.deb",
+        "linux-arm7-cli.zip",
+        "linux-arm7-cli.deb",
+        "linux-arm7-cli.docker",
+
         "linux-arm64-gui.zip",
         "linux-arm64-gui.deb",
         "linux-arm64-gui.rpm",
         "linux-arm64-cli.zip",
         "linux-arm64-cli.deb",
         "linux-arm64-cli.rpm",
+        "linux-arm64-cli.docker",
         // "linux-arm64-cli.spk",  // Need to add new integration with DSM WebUI
-        "linux-arm7-cli.docker",
+        "linux-arm64-agent.zip",
+        "linux-arm64-agent.deb",
+        "linux-arm64-agent.rpm",
+        "linux-arm64-agent.docker",
+
         "osx-x64-gui.dmg",
         "osx-x64-gui.pkg",
+        // "osx-x64-agent.pkg", // Need to support building DMG without trayicon
         "osx-arm64-gui.dmg",
         "osx-arm64-gui.pkg",
+        // "osx-arm64-agent.pkg", // Need to support building DMG without trayicon
     }
     .Select(x => PackageTarget.ParsePackageId(x))
     .Distinct()
