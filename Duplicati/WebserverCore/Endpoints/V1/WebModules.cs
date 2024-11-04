d98186e1ebecdd1b60749e57568fc92deee32bfe
Kenneth Skovhede <kenneth@hexad.dk>
2024-11-04 13:20:23 +0100

    Implemented secret provider on the remoteoperation and webmodules calls

--- Duplicati/WebserverCore/Endpoints/V1/WebModules.cs
+++ Duplicati/WebserverCore/Endpoints/V1/WebModules.cs
@@ -1,4 +1,7 @@
 using Duplicati.Library.Interface;
+using Duplicati.Library.Main;
+using Duplicati.Library.RestAPI;
+using Duplicati.Server;
 using Duplicati.WebserverCore.Abstractions;
 using Duplicati.WebserverCore.Exceptions;
 using Microsoft.AspNetCore.Mvc;
@@ -10,16 +13,22 @@ public record WebModules : IEndpointV1
     public static void Map(RouteGroupBuilder group)
     {
         group.MapGet("/webmodules", ExecuteGet).RequireAuthorization();
-        group.MapPost("/webmodule/{modulekey}", ([FromRoute] string modulekey, [FromBody] Dictionary<string, string> options) => ExecutePost(modulekey, options)).RequireAuthorization();
+        group.MapPost("/webmodule/{modulekey}", ([FromRoute] string modulekey, [FromBody] Dictionary<string, string> options, CancellationToken cancellationToken) => ExecutePost(modulekey, options, cancellationToken)).RequireAuthorization();
     }
 
     private static IEnumerable<IWebModule> ExecuteGet()
         => Library.DynamicLoader.WebLoader.Modules;
 
-    private static Dto.WebModuleOutputDto ExecutePost(string modulekey, Dictionary<string, string> options)
+    private static async Task<Dto.WebModuleOutputDto> ExecutePost(string modulekey, Dictionary<string, string> inputOptions, CancellationToken cancellationToken)
     {
         var m = Library.DynamicLoader.WebLoader.Modules.FirstOrDefault(x => x.Key.Equals(modulekey, StringComparison.OrdinalIgnoreCase))
             ?? throw new NotFoundException("No such module found");
+
+        var options = Runner.GetCommonOptions();
+        foreach (var k in inputOptions.Keys)
+            options[k] = inputOptions[k];
+
+        await SecretProviderHelper.ApplySecretProviderAsync([], options, Library.Utility.TempFolder.SystemTempPath, FIXMEGlobal.SecretProvider, cancellationToken);
 
         return new Dto.WebModuleOutputDto(
             Status: "OK",
