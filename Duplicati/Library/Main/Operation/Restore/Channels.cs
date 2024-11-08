459664d33bd6d2d9b5ca3df0d63a90d33f099a1f
Carl Johnsen <carl-johannes@di.ku.dk>
2024-11-08 14:38:24 +0100

    Added restore channel buffer sizes as a variable

--- Duplicati/Library/Main/Operation/Restore/Channels.cs
+++ Duplicati/Library/Main/Operation/Restore/Channels.cs
@@ -5,10 +5,13 @@ namespace Duplicati.Library.Main.Operation.Restore
 {
     internal static class Channels
     {
-        public static readonly ChannelMarkerWrapper<(long, Database.IRemoteVolume)> downloadRequest = new(new ChannelNameAttribute("downloadRequest"));
-        public static readonly ChannelMarkerWrapper<(long, TempFile)> downloadedVolume = new(new ChannelNameAttribute("downloadResponse"));
-        public static readonly ChannelMarkerWrapper<Database.LocalRestoreDatabase.IFileToRestore> filesToRestore = new(new ChannelNameAttribute("filesToRestore"));
-        public static readonly ChannelMarkerWrapper<(long, byte[])> decompressedVolumes = new(new ChannelNameAttribute("decompressedVolumes"));
-        public static readonly ChannelMarkerWrapper<(long, TempFile)> decryptedVolume = new(new ChannelNameAttribute("decrytedVolume"));
+        // TODO Should maybe come from Options, or at least some global configuration file?
+        private static int bufferSize = 0;
+
+        public static readonly ChannelMarkerWrapper<(long, Database.IRemoteVolume)> downloadRequest = new(new ChannelNameAttribute("downloadRequest", bufferSize));
+        public static readonly ChannelMarkerWrapper<(long, TempFile)> downloadedVolume = new(new ChannelNameAttribute("downloadResponse", bufferSize));
+        public static readonly ChannelMarkerWrapper<Database.LocalRestoreDatabase.IFileToRestore> filesToRestore = new(new ChannelNameAttribute("filesToRestore", bufferSize));
+        public static readonly ChannelMarkerWrapper<(long, byte[])> decompressedVolumes = new(new ChannelNameAttribute("decompressedVolumes", bufferSize));
+        public static readonly ChannelMarkerWrapper<(long, TempFile)> decryptedVolume = new(new ChannelNameAttribute("decrytedVolume", bufferSize));
     }
 }
