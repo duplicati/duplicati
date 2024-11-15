9a8e3ab90c7972f13da3754587f5988113ceeb4c
Carl Johnsen <carl-johannes@di.ku.dk>
2024-11-15 13:50:13 +0100

    Added on-the-fly block hash verification

--- Duplicati/Library/Main/Operation/Restore/VolumeDecompressor.cs
+++ Duplicati/Library/Main/Operation/Restore/VolumeDecompressor.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using CoCoL;
 using Duplicati.Library.Main.Volumes;
+using Duplicati.Library.Utility;
 
 namespace Duplicati.Library.Main.Operation.Restore
 {
@@ -17,12 +19,21 @@ namespace Duplicati.Library.Main.Operation.Restore
             async self =>
             {
                 try {
+                    using var block_hasher = HashFactory.CreateHasher(options.BlockHashAlgorithm);
+
                     while (true)
                     {
                         var (block_request, volume) = await self.Input.ReadAsync();
 
                         byte[] buffer = new byte[block_request.BlockSize];
                         new BlockVolumeReader(options.CompressionModule, volume, options).ReadBlock(block_request.BlockHash, buffer);
+                        var hash = Convert.ToBase64String(block_hasher.ComputeHash(buffer, 0, (int)block_request.BlockSize));
+
+                        if (hash != block_request.BlockHash)
+                        {
+                            Console.WriteLine($"Block hash mismatch: {hash} != {block_request.BlockHash}");
+                            throw new Exception("Block hash mismatch");
+                        }
 
                         await self.Output.WriteAsync((block_request, buffer));
                     }
