fc90868c17c91726dbb285130d6d088da6f0377f
Kenneth Skovhede <kenneth@hexad.dk>
2024-11-21 13:13:44 +0100

    Inverted the flag for relative paths, as that is more compatible with the previous library.
    (cherry picked from commit 9dadd514bf7c78fcab3e081fd9986d4e3f5e9320)

--- Duplicati/Library/Backend/FTP/Strings.cs
+++ Duplicati/Library/Backend/FTP/Strings.cs
@@ -65,8 +65,8 @@ namespace Duplicati.Library.Backend
         public static string FtpActiveDeprecated { get { return LC.L(@"ftp-regular is deprecated, use ftp-data-connection-type instead."); } }
         public static string UseSslDeprecated { get { return LC.L(@"use-ssl is deprecated, use ftp-ssl-protocols instead."); } }
         public static string FileMissingError(string filename, string message) { return LC.L(@"The file {0} was not found. Message: {1}", filename, message); }
-        public static string DescriptionRelativePathsShort { get { return LC.L(@"Use relative paths for FTP connections"); } }
-        public static string DescriptionRelativePathsLong { get { return LC.L(@"Use this option to use relative paths for FTP connections. This option only has an effect if the initial starting folder in the FTP server is not the root folder."); } }
+        public static string DescriptionAbsolutePathShort { get { return LC.L(@"Treat the url path as absolute"); } }
+        public static string DescriptionAbsolutePathLong { get { return LC.L(@"Use this option to interpret the url path as an absolute path. This option only has an effect if the initial starting folder in the FTP server is not the (virtual) root folder. If not set, the path in the url is treated as relative to the initial login folder."); } }
         public static string DescriptionUseCwdNamesShort { get { return LC.L(@"Use CWD instead of absolute paths"); } }
         public static string DescriptionUseCwdNamesLong { get { return LC.L(@"Use this option to start the connection with a CWD command instead of an absolute path. This can be useful if the FTP server does not support absolute paths."); } }
         public static string ErrorCreateFolder(string targetFolderName, string resultingFolder) { return LC.L(@"Error creating folder {0}, gave folder: {1}", targetFolderName, resultingFolder); }
