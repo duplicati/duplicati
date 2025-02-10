f94100d283edb8ef8b021fa8e234005c1c554504
Kenneth Skovhede <kenneth@duplicati.com>
2025-02-10 21:06:39 +0100

    Merge pull request #5950 from duplicati/feature/fix-zip-fallback
    Handle automatic fallback when reading zip files
    
    (cherry picked from commit e76bbc478f789bc30555ed8ecc8b1e91d1b12b22)

--- Duplicati/Library/Main/Operation/RecreateDatabaseHandler.cs
+++ Duplicati/Library/Main/Operation/RecreateDatabaseHandler.cs
@@ -20,6 +20,7 @@
 // DEALINGS IN THE SOFTWARE.
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Duplicati.Library.Interface;
 using Duplicati.Library.Main.Database;
@@ -236,9 +237,14 @@ namespace Duplicati.Library.Main.Operation
 
                                 var parsed = VolumeBase.ParseFilename(entry.Name);
 
+                                using var stream = new FileStream(tmpfile, FileMode.Open, FileAccess.Read, FileShare.Read);
+                                using var compressor = DynamicLoader.CompressionLoader.GetModule(parsed.CompressionModule, stream, ArchiveMode.Read, m_options.RawOptions);
+                                if (compressor == null)
+                                    throw new UserInformationException(string.Format("Failed to load compression module: {0}", parsed.CompressionModule), "FailedToLoadCompressionModule");
+
                                 if (!hasUpdatedOptions)
                                 {
-                                    VolumeReaderBase.UpdateOptionsFromManifest(parsed.CompressionModule, tmpfile, m_options);
+                                    VolumeReaderBase.UpdateOptionsFromManifest(compressor, m_options);
                                     hasUpdatedOptions = true;
                                     // Recompute the cached sizes
                                     blocksize = m_options.Blocksize;
@@ -249,12 +255,12 @@ namespace Duplicati.Library.Main.Operation
                                 var filesetid = restoredb.CreateFileset(volumeIds[entry.Name], parsed.Time, tr);
 
                                 // retrieve fileset data from dlist
-                                var filesetData = VolumeReaderBase.GetFilesetData(parsed.CompressionModule, tmpfile, m_options);
+                                var filesetData = VolumeReaderBase.GetFilesetData(compressor, m_options);
 
                                 // update fileset using filesetData
                                 restoredb.UpdateFullBackupStateInFileset(filesetid, filesetData.IsFullBackup);
 
-                                using (var filelistreader = new FilesetVolumeReader(parsed.CompressionModule, tmpfile, m_options))
+                                using (var filelistreader = new FilesetVolumeReader(compressor, m_options))
                                     foreach (var fe in filelistreader.Files.Where(x => Library.Utility.FilterExpression.Matches(filter, x.Path)))
                                     {
                                         try
