9501470d0d4a35ef2a031d3e77a9527ce4add983
Kenneth Skovhede <kenneth@hexad.dk>
2025-02-14 11:28:22 +0100

    Added listen interface to startup message

--- Duplicati/WebserverCore/DuplicatiWebserver.cs
+++ Duplicati/WebserverCore/DuplicatiWebserver.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2025, The Duplicati Team
+﻿// Copyright (C) 2025, The Duplicati Team
 // https://duplicati.com, hello@duplicati.com
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a 
@@ -72,6 +72,11 @@ public class DuplicatiWebserver
     /// The port the server is listening on
     /// </summary>
     public required int Port { get; init; }
+
+    /// <summary>
+    /// The interfaces being listened to
+    /// </summary>
+    public required string Interface { get; init; }
 
     /// <summary>
     /// The port the server is listening on
@@ -335,11 +340,19 @@ public class DuplicatiWebserver
         // Preload static system info, for better first-load experience
         var _ = Task.Run(() => app.Services.GetRequiredService<ISystemInfoProvider>().GetSystemInfo(null));
 
+        // Get a string description of the listen interface used
+        var listenInterface = settings.Interface == System.Net.IPAddress.Any
+            ? "*"
+            : settings.Interface == System.Net.IPAddress.Loopback
+                ? "localhost"
+                : settings.Interface.ToString();
+
         return new DuplicatiWebserver()
         {
             Configuration = builder.Configuration,
             App = app,
             Port = settings.Port,
+            Interface = listenInterface,
             CorsEnabled = useCors
         };
 
