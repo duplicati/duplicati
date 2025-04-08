d528ef6278ad39d95e6c228d04585815e22f45da
Marcelo C. <marcelo@duplicati.com>
2025-04-08 11:21:38 -0300

    Fix Resource Leak in BackendManager Connection Handling
    While investigating issue [#6103](https://github.com/duplicati/duplicati/issues/6103), found that connections were not disposed due to the backend pool in BackendManager's handler. Set up a FileZilla server to test the issue before and after the change, confirming that connections now close as expected with this fix.

--- Duplicati/Library/Main/Backend/BackendManager.cs
+++ Duplicati/Library/Main/Backend/BackendManager.cs
@@ -388,7 +388,9 @@ internal partial class BackendManager : IBackendManager
                 Logging.Log.WriteWarningMessage(LOGTAG, "BackendManagerShutdown", null, "Backend manager queue runner did not stop");
             if (queueRunner.IsFaulted)
                 Logging.Log.WriteWarningMessage(LOGTAG, "BackendManagerShutdown", queueRunner.Exception, "Backend manager queue runner crashed");
-
         }
+
+        if (queueRunner.IsCompleted)
+            queueRunner.Dispose();
     }
 }
