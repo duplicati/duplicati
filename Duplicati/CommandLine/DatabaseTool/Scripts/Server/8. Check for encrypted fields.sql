c8f8fb1a37dc4e4e7e3e91faa90bee141a0df5c3
Kenneth Skovhede <kenneth@hexad.dk>
2025-04-24 16:42:57 +0200

    Less cryptic script comment

--- Duplicati/CommandLine/DatabaseTool/Scripts/Server/8. Check for encrypted fields.sql
+++ Duplicati/CommandLine/DatabaseTool/Scripts/Server/8. Check for encrypted fields.sql
@@ -1,4 +1,4 @@
-﻿/* RETURN-ZERO-RESULTS */
+﻿/* This script returns an error message if any encrypted values are found */
 SELECT "Database is encrypted and must be decrypted before it can be downgraded" AS "Message"
 FROM Option WHERE Value LIKE 'enc-v1:%'
 UNION ALL
