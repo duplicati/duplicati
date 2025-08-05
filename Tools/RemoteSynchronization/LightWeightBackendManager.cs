542186228dd949446d59586eb61744c29a138a1b
Carl Johnsen <carl-johannes@di.ku.dk>
2025-08-05 13:05:10 +0200

    Added error handling to the lightweight backend manager; copied from BackendManager.Handler

--- Tools/RemoteSynchronization/LightWeightBackendManager.cs
+++ Tools/RemoteSynchronization/LightWeightBackendManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Duplicati.Library.Interface;
@@ -8,23 +9,27 @@ using Duplicati.Library.Utility;
 
 namespace RemoteSynchronization
 {
-    public class LightWeightBackendManager(string backendUrl, Dictionary<string, string> options, int maxRetries = 3, int retryDelay = 1000) : IDisposable
+    public class LightWeightBackendManager(string backendUrl, Dictionary<string, string> options, int maxRetries = 3, int retryDelay = 1000, bool autoCreateFolders = false, bool retryWithExponentialBackoff = false) : IDisposable
     {
         private static readonly string LOGTAG = Duplicati.Library.Logging.Log.LogTagFromType<Program>();
 
         public IBackend? _backend = null;
+
+        private bool _anyDownloaded = false;
+        private bool _anyUploaded = false;
         private readonly string _backendUrl = backendUrl;
         private int _instantiations = 0;
         private readonly int _maxRetries = maxRetries;
         private Dictionary<string, string> _options = options;
-        private readonly int _retryDelay = retryDelay;
+        private int _retryDelay = retryDelay;
         private IStreamingBackend? _streamingBackend = null;
 
         public Task DeleteAsync(string remotename, CancellationToken token)
         {
             return RetryWithDelay(
                 $"Delete {remotename}",
-                () => _streamingBackend.DeleteAsync(remotename, token)
+                () => _streamingBackend.DeleteAsync(remotename, token),
+                token
             );
         }
 
@@ -32,10 +37,7 @@ namespace RemoteSynchronization
         {
             get
             {
-                if (_streamingBackend == null)
-                {
-                    Instantiate();
-                }
+                Instantiate();
 
                 return _streamingBackend.DisplayName;
             }
@@ -60,12 +62,21 @@ namespace RemoteSynchronization
         {
             return RetryWithDelay(
                 $"Get {remotename}",
-                () => _streamingBackend.GetAsync(remotename, stream, token)
+                () => _streamingBackend
+                    .GetAsync(remotename, stream, token)
+                    .ContinueWith(_ => _anyDownloaded = true, token),
+                token
             );
         }
 
         private void Instantiate()
         {
+            if (_backend != null)
+            {
+                // If we already have a backend, we can just return.
+                return;
+            }
+
             if (_instantiations < _maxRetries)
             {
                 _backend = Duplicati.Library.DynamicLoader.BackendLoader.GetBackend(_backendUrl, _options);
@@ -82,10 +93,7 @@ namespace RemoteSynchronization
         {
             while (true)
             {
-                if (_streamingBackend == null)
-                {
-                    Instantiate();
-                }
+                Instantiate();
 
                 try
                 {
@@ -95,7 +103,8 @@ namespace RemoteSynchronization
                 {
                     Duplicati.Library.Logging.Log.WriteErrorMessage(LOGTAG, "rsync", ex, "Error during operation: List", null);
                     Dispose(); // Dispose current backend and streaming backend
-                    Task.Delay(_retryDelay, token).Await();
+
+                    TryRecoverFromException(ex, token).Await();
                 }
             }
 
@@ -105,16 +114,16 @@ namespace RemoteSynchronization
         {
             return RetryWithDelay(
                 $"Put {remotename}",
-                () => _streamingBackend.PutAsync(remotename, stream, token)
+                () => _streamingBackend
+                    .PutAsync(remotename, stream, token)
+                    .ContinueWith(_ => _anyUploaded = true, token),
+                token
             );
         }
 
         public Task RenameAsync(string oldname, string newname, CancellationToken token)
         {
-            if (_streamingBackend == null)
-            {
-                Instantiate();
-            }
+            Instantiate();
 
             return _backend switch
             {
@@ -125,40 +134,119 @@ namespace RemoteSynchronization
                         {
                             // Download the file, rename it, and delete the old one
                             using var downloaded = new MemoryStream();
-                            await sb.GetAsync(oldname, downloaded, token);
-                            await sb.PutAsync(newname, downloaded, token);
-                            await sb.DeleteAsync(oldname, token);
-                        }
+                            await sb.GetAsync(oldname, downloaded, token).ConfigureAwait(false);
+                            await sb.PutAsync(newname, downloaded, token).ConfigureAwait(false);
+                            await sb.DeleteAsync(oldname, token).ConfigureAwait(false);
+                            _anyUploaded = true;
+                            _anyDownloaded = true;
+                        },
+                        token
                     ),
                 IRenameEnabledBackend ireb =>
                     RetryWithDelay(
                         $"Rename {oldname} to {newname}",
-                        () => ireb.RenameAsync(oldname, newname, token)
+                        () => ireb
+                            .RenameAsync(oldname, newname, token)
+                            .ContinueWith(
+                                _ =>
+                                {
+                                    _anyUploaded = true;
+                                    _anyDownloaded = true;
+                                },
+                                token
+                            ),
+                        token
                     ),
                 _ => throw new InvalidOperationException("Backend does not support renaming."),
             };
         }
 
-        private async Task RetryWithDelay(string operationName, Func<Task> action)
+        private async Task RetryWithDelay(string operationName, Func<Task> action, CancellationToken token)
         {
             while (true)
             {
-                if (_streamingBackend == null)
-                {
-                    Instantiate();
-                }
+                // This will throw an exception if we've reached the max
+                // number of retries.
+                Instantiate();
 
                 try
                 {
-                    await action();
-                    return;
+                    await action().ConfigureAwait(false);
+                    return; // Exit the loop if the action succeeds.
                 }
                 catch (Exception ex)
                 {
                     Duplicati.Library.Logging.Log.WriteErrorMessage(LOGTAG, "rsync", ex, "Error during operation: {0}", operationName);
-                    Dispose(); // Dispose current backend and streaming backend
-                    await Task.Delay(_retryDelay);
+                    Dispose(); // Dispose current backend and streaming backend.
+
+                    // Try to see if we can recover from the error.
+                    await TryRecoverFromException(ex, token).ConfigureAwait(false);
                 }
+            }
+        }
+
+        private async Task<bool> TryCreateFolder(CancellationToken token)
+        {
+            try
+            {
+                Instantiate();
+
+                // Attempt to create the folder
+                await _backend.CreateFolderAsync(token).ConfigureAwait(false);
+
+                return true; // Folder creation succeeded
+            }
+            catch (Exception ex)
+            {
+                Dispose();
+
+                Duplicati.Library.Logging.Log.WriteErrorMessage(LOGTAG, "rsync", ex, "Failed to create folder", null);
+
+                return false; // Folder creation failed
+            }
+        }
+
+        private async Task TryRecoverFromException(Exception ex, CancellationToken token)
+        {
+            // Copied from Duplicati.Library.Main.Backend.BackendManager.Handler.
+
+            // Refresh DNS name if we fail to connect in order to prevent issues with incorrect DNS entries.
+            var dnsFailure = ExceptionExtensions.FlattenException(ex)
+            .Any(x =>
+                (x is System.Net.WebException wex && wex.Status == System.Net.WebExceptionStatus.NameResolutionFailure)
+                ||
+                (x is System.Net.Sockets.SocketException sockEx && sockEx.SocketErrorCode == System.Net.Sockets.SocketError.HostNotFound)
+            );
+
+            if (dnsFailure)
+            {
+                try
+                {
+                    Instantiate();
+
+                    foreach (var name in await _backend.GetDNSNamesAsync(token).ConfigureAwait(false) ?? [])
+                        if (!string.IsNullOrWhiteSpace(name))
+                            System.Net.Dns.GetHostEntry(name);
+                }
+                catch { }
+            }
+
+            var recovered = false;
+
+            // Check if this was a folder missing exception and we are allowed to autocreate folders
+            if (!(_anyDownloaded || _anyUploaded) && autoCreateFolders && ExceptionExtensions.FlattenException(ex).Any(x => x is FolderMissingException))
+            {
+                if (await TryCreateFolder(token).ConfigureAwait(false))
+                    recovered = true;
+            }
+
+            // Finally, if we did not recover, wait the specified delay before retrying.
+            if (!recovered && _retryDelay > 0)
+            {
+                if (retryWithExponentialBackoff)
+                    _retryDelay <<= 1; // Double the delay for exponential backoff
+
+                await Task.Delay(_retryDelay, token).ConfigureAwait(false);
             }
         }
 
