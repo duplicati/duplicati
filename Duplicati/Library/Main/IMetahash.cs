6e53aeebb75b62e213b46053d0fe314de735a07e
Kenneth Skovhede <kenneth@duplicati.com>
2025-08-08 20:10:03 +0200

    Minor cleanup

--- Duplicati/Library/Main/IMetahash.cs
+++ Duplicati/Library/Main/IMetahash.cs
@@ -19,7 +19,6 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 // DEALINGS IN THE SOFTWARE.
 
-using System;
 using System.Collections.Generic;
 
 namespace Duplicati.Library.Main
