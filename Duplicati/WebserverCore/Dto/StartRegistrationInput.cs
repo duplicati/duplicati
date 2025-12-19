9c6d80434d7a8c399a70d0d3b33aa1807e65027e
Kenneth Skovhede <kenneth@duplicati.com>
2025-12-19 12:58:33 +0100

    Merge pull request #6687 from duplicati/feature/improve-contract-for-register
    Improve API for getting registration status.

--- Duplicati/WebserverCore/Dto/StartRegistrationInput.cs
+++ Duplicati/WebserverCore/Dto/StartRegistrationInput.cs
@@ -24,4 +24,4 @@ namespace Duplicati.WebserverCore.Dto;
 /// The required data for starting a registration
 /// </summary>
 /// <param name="RegistrationUrl">The URL to register the machine with</param>
-public sealed record StartRegistrationInput(string RegistrationUrl);
+public sealed record StartRegistrationInput(string? RegistrationUrl);
