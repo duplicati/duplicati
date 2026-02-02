4083a28a2906a5afed48ea33e5994ea549efab8c
Carl Johnsen <carl-johannes@di.ku.dk>
2026-02-02 16:10:56 +0100

    Added debugging of the full disk parsing

--- proprietary/FullDiskRunner/Program.cs
+++ proprietary/FullDiskRunner/Program.cs
@@ -2,6 +2,7 @@
 
 // TODO this project is just a test project to test the disk image library. Remove later, or elevate to a proper tool.
 
+/*
 using var disk = new Duplicati.Proprietary.DiskImage.Raw.Windows(@"\\.\PhysicalDrive0");
 var initialized = await disk.InitializeAsync(CancellationToken.None);
 if (!initialized)
@@ -14,3 +15,13 @@ byte[] buffer = new byte[4096];
 await partition.ReadExactlyAsync(buffer, CancellationToken.None);
 
 await File.WriteAllBytesAsync("partition0.bin", buffer);
+*/
+
+var bytes = await File.ReadAllBytesAsync("partition1.bin");
+var table = new Duplicati.Proprietary.DiskImage.Partition.GPT();
+var parsed = await table.ParseAsync(bytes, 512, CancellationToken.None);
+if (!parsed)
+{
+    Console.WriteLine("Failed to parse partition table.");
+    return;
+}
