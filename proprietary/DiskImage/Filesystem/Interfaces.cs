f2028b46000fb9e5a2d199ce60fe1a25d0febc69
Carl Johnsen <carl-johannes@di.ku.dk>
2026-02-04 10:41:07 +0100

    Added filesystem interface

--- proprietary/DiskImage/Filesystem/Interfaces.cs
+++ proprietary/DiskImage/Filesystem/Interfaces.cs
@@ -1,14 +1,67 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using Duplicati.Library.Interface;
 using Duplicati.Proprietary.DiskImage.Partition;
 
 namespace Duplicati.Proprietary.DiskImage.Filesystem;
 
 public interface IFilesystem : IDisposable
 {
+    /// <summary>
+    /// Gets the parent partition.
+    /// </summary>
+    IPartition Partition { get; }
 
+    /// <summary>
+    /// Gets the filesystem type.
+    /// </summary>
+    FileSystemType Type { get; }
+
+    /// <summary>
+    /// Gets filesystem-specific metadata. E.g. for journal filesystems, this could be the journal file.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A task that resolves to filesystem-specific metadata.</returns>
+    Task<object?> GetFilesystemMetadataAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Lists top-level files in the filesystem.
+    /// </summary>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A task that resolves to a collection of top-level files.</returns>
+    Task<IEnumerable<IFile>> ListFilesAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Lists files in a directory.
+    /// </summary>
+    /// <param name="directory">The directory to list files from.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A task that resolves to a collection of files in the specified directory.</returns>
+    /// <throws cref="ArgumentNullException">Thrown if <paramref name="directory"/> is null.</throws>
+    /// <throws cref="ArgumentException">Thrown if <paramref name="directory"/> is not a directory.</throws>
+    Task<IEnumerable<IFile>> ListFilesAsync(IFile directory, CancellationToken cancellationToken);
+
+    Task<Stream> OpenFileAsync(IFile file, CancellationToken cancellationToken);
+}
+
+public interface IFile
+{
+    string? Path { get; }
+    long? Address { get; }
+    long Size { get; }
+    bool IsDirectory { get; }
+}
+
+public interface IFileMetadata
+{
+    byte[]? RawMetadata { get; }
+
+    DateTimeOffset? Created { get; }
+
+    DateTimeOffset? Modified { get; }
+
+    DateTimeOffset? Accessed { get; }
 }
