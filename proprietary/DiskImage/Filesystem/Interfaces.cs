b2ffa91aa26a3586eca4cf307dfd802ab31f634c
Carl Johnsen <carl-johannes@di.ku.dk>
2026-02-04 09:42:38 +0100

    Restructured the interfaces

---
+++ proprietary/DiskImage/Filesystem/Interfaces.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Duplicati.Library.Interface;
+using Duplicati.Proprietary.DiskImage.Partition;
+
+namespace Duplicati.Proprietary.DiskImage.Filesystem;
+
+public interface IFilesystem : IDisposable
+{
+
+}
